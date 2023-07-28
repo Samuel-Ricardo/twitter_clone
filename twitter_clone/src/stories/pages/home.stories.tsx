1c7b9b32fb2c1e7a31c300b9df1561a4d63f97e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-07-27 23:48:07 -0300

    [:memo:] | create: docs - Home (page)

---
+++ twitter_clone/src/stories/pages/home.stories.tsx
@@ -0,0 +1,10 @@
+//import type { Meta, StoryObj } from '@storybook/react';
+
+import Home from '../../app/page';
+
+export default {
+  title: 'Pages/Home',
+  component: Home,
+};
+
+export const HomePage = () => <Home />;
