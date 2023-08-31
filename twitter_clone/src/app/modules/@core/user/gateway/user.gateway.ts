93d8885d48c44716f8541e467de21d3a39c8a8be
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 14:56:39 -0300

    [:sparkles:] | create: gateway - user (module)

---
+++ twitter_clone/src/app/modules/@core/user/gateway/user.gateway.ts
@@ -0,0 +1,15 @@
+import {
+  ICreateUserDTO,
+  IDeleteuserDTO,
+  ISelectUserByIdDTO,
+  IUpdateUserDTO,
+} from '../DTO';
+import { User } from '../entity/user.entity';
+
+export interface UserGateway {
+  create(user: ICreateUserDTO): Promise<User>;
+  listAll(): Promise<User[]>;
+  selectById(props: ISelectUserByIdDTO): Promise<User>;
+  update(props: IUpdateUserDTO): Promise<User>;
+  delete(props: IDeleteuserDTO): Promise<boolean>;
+}
