6c7ecb75a5dbd04f53480f067c896b7566faea11
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 17:41:11 -0300

    [:sparkles:] | implements:  gateway - user > update (axios)

--- twitter_clone/src/app/modules/gateway/axios/user/user.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/user/user.gateway.ts
@@ -4,32 +4,45 @@ import { injectable } from 'inversify';
 import {
   ICreateUserDTO,
   ISelectUserByIdDTO,
+  IUpdateUserDTO,
   IUserDTO,
 } from '@/app/modules/@core/user/DTO';
 import { User } from '@/app/modules/@core/user/entity/user.entity';
 import {
   CreateUserSchema,
   SelectUserByIdSchema,
+  UpdateUserSchema,
 } from '@/app/modules/@core/user/validator';
 
 @injectable()
 export class AxiosUserGateway extends AxiosHTTPGateway implements IUserGateway {
+  readonly prefix = '/users';
+
   async create(user: ICreateUserDTO) {
     if (!CreateUserSchema.parse(user)) throw Error('Invalid user');
 
-    const response = await this.post<{ user: IUserDTO }>('/users', user);
+    const response = await this.post<{ user: IUserDTO }>(this.prefix, user);
     return User.create(response.data.user);
   }
 
   async listAll() {
-    const response = await this.get<{ users: IUserDTO[] }>('/users');
+    const response = await this.get<{ users: IUserDTO[] }>(this.prefix);
     return User.createArray(response.data.users);
   }
 
   async selectById(data: ISelectUserByIdDTO) {
     if (!SelectUserByIdSchema.parse(data)) throw Error('Invalid user');
 
-    const response = await this.get<{ user: IUserDTO }>(`/users/${data.id}`);
+    const response = await this.get<{ user: IUserDTO }>(
+      `${this.prefix}/${data.id}`,
+    );
+    return User.create(response.data.user);
+  }
+
+  async update(data: IUpdateUserDTO) {
+    UpdateUserSchema.parse(data);
+
+    const response = await this.put<{ user: IUserDTO }>(this.prefix, data);
     return User.create(response.data.user);
   }
 }
