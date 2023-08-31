94451f0539365068b8846ea49955db140cc669bc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 14:45:45 -0300

    [:sparkles:] | create: DTO - update > user (module)

---
+++ twitter_clone/src/app/modules/@core/user/DTO/update.dto.ts
@@ -0,0 +1,16 @@
+export interface UpdateUserDTO {
+  id: string;
+  name?: string;
+  username?: string;
+  bio?: string;
+  email?: string;
+  password?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  sessionToken?: string;
+  hasNotifications?: boolean;
+  emailVerified?: Date;
+  image?: string;
+  coverImage?: string;
+  profileImage?: string;
+}
