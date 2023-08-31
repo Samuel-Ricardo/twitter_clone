ccba7d10ad6ae058779e5b826aa5b447df3c5b36
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 11:00:31 -0300

    [:sparkles:] | create: getters - entity > user (module)

--- twitter_clone/src/app/modules/@core/user/entity/user.entity.ts
+++ twitter_clone/src/app/modules/@core/user/entity/user.entity.ts
@@ -14,12 +14,12 @@ export class User {
     private _updatedAt: Date,
     private _hasNotifications: boolean,
 
-    private sessionToken?: string | null,
-    private bio?: string | null,
-    private emailVerified?: Date | null,
-    private image?: string | null,
-    private coverImage?: string | null,
-    private profileImage?: string | null,
+    private _sessionToken?: string | null,
+    private _bio?: string | null,
+    private _emailVerified?: Date | null,
+    private _image?: string | null,
+    private _coverImage?: string | null,
+    private _profileImage?: string | null,
   ) {}
 
   static create(data: IUserDTO): User {
@@ -39,5 +39,61 @@ export class User {
       data.coverImage,
       data.profileImage,
     );
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get username(): string {
+    return this._username;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  get hasNotifications(): boolean {
+    return this._hasNotifications;
+  }
+
+  get sessionToken(): string | null | undefined {
+    return this._sessionToken;
+  }
+
+  get bio(): string | null | undefined {
+    return this._bio;
+  }
+
+  get emailVerified(): Date | null | undefined {
+    return this._emailVerified;
+  }
+
+  get image(): string | null | undefined {
+    return this._image;
+  }
+
+  get coverImage(): string | null | undefined {
+    return this._coverImage;
+  }
+
+  get profileImage(): string | null | undefined {
+    return this._profileImage;
   }
 }
