4dc3205edf4bcb36a02b2ce53473cf7a3729a1cb
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 17:43:49 -0300

    [:sparkles:] | update: gateway - user (module)

--- twitter_clone/src/app/modules/@core/user/gateway/user.gateway.ts
+++ twitter_clone/src/app/modules/@core/user/gateway/user.gateway.ts
@@ -8,9 +8,11 @@ import { User } from '../entity/user.entity';
 import { ISWRSupport } from './support/swr.support';
 
 export interface IUserGateway extends ISWRSupport {
+  readonly prefix: '/users';
+
   create(user: ICreateUserDTO): Promise<User>;
   listAll(): Promise<User[]>;
   selectById(props: ISelectUserByIdDTO): Promise<User>;
   update(props: IUpdateUserDTO): Promise<User>;
-  delete(props: IDeleteuserDTO): Promise<boolean>;
+  deleteUser(props: IDeleteuserDTO): Promise<void | any>;
 }
