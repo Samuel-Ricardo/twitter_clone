bd01b60b64a7202e2225d891718305580958145e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 14:35:55 -0300

    [:safety_vest:] | create: validator - user (module)

---
+++ twitter_clone/src/app/modules/@core/user/validator/user.schema.ts
@@ -0,0 +1,24 @@
+import { catchZod } from '@/app/modules/util';
+import { z } from 'zod';
+
+export const UserSchema = z
+  .object({
+    id: z.string(),
+
+    name: z.string(),
+    username: z.string(),
+    email: z.string(),
+    password: z.string(),
+
+    createdAt: z.date(),
+    updatedAt: z.date(),
+    hasNotifications: z.boolean(),
+
+    sessionToken: z.string().nullish(),
+    bio: z.string().nullish(),
+    emailVerified: z.date().nullish(),
+    image: z.string().nullish(),
+    coverImage: z.string().nullish(),
+    profileImage: z.string().nullish(),
+  })
+  .catch(catchZod);
