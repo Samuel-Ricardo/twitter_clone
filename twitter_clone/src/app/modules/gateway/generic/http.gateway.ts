8585519214226f5e4045f74fac86e4997b71f3d2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-08-31 17:17:32 -0300

    [:sparkles:] | update: interface - gateway > http (module)

--- twitter_clone/src/app/modules/gateway/generic/http.gateway.ts
+++ twitter_clone/src/app/modules/gateway/generic/http.gateway.ts
@@ -2,13 +2,13 @@ export interface IHTTPGateway {
   readonly URL: string;
   readonly client: any;
 
-  get(path: string, config?: any): Promise<any>;
+  get<T>(path: string, config?: any): Promise<any>;
 
-  post(path: string, body: any, config?: any): Promise<any>;
+  post<T>(path: string, body: any, config?: any): Promise<any>;
 
-  put(path: string, body: any, config?: any): Promise<any>;
+  put<T>(path: string, body: any, config?: any): Promise<any>;
 
-  patch(path: string, body: any, config?: any): Promise<any>;
+  patch<T>(path: string, body: any, config?: any): Promise<any>;
 
-  delete(path: string, config?: any): Promise<any>;
+  delete<T>(path: string, config?: any): Promise<any>;
 }
