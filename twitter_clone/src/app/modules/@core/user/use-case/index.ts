b6dac5d09510be7e589b338fa1fbc3cd41bd2ee7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 09:59:21 -0300

    [:sparkles:] | create: index - use-case (user)

---
+++ twitter_clone/src/app/modules/@core/user/use-case/index.ts
@@ -0,0 +1,5 @@
+export * from './list.use-case';
+export * from './select_by_id.use-case';
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './update.use-case';
