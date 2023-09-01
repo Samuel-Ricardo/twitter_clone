893a230ff484b597b33b8a90863d0219f25d22b0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 10:12:27 -0300

    [:sparkles:] | update: registry - app > user (module)

--- twitter_clone/src/app/modules/app.factory.ts
+++ twitter_clone/src/app/modules/app.factory.ts
@@ -1,7 +1,9 @@
 import { AxiosFactory } from './axios/axios.factory';
 import { GatewayFactory as GATEWAY } from './gateway/gateway.factory';
+import { UserFactory as USER } from './@core/user/user.factory';
 
 export const MODULES = {
   ...AxiosFactory,
   GATEWAY,
+  USER,
 };
