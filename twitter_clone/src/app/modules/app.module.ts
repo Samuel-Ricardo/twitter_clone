68bb872dd564542ed4605402a29c0338cca7ecd7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 09:34:22 -0300

    [:sparkles:] | update: module - app > gateway (module)

--- twitter_clone/src/app/modules/app.module.ts
+++ twitter_clone/src/app/modules/app.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
-import { AxiosModule } from './axios/axios.module';
+import { AxiosModule as AXIOS } from './axios/axios.module';
+import { GatewayModule as GATEWAY } from './gateway/gateway.module';
 
 const Module = new Container({ autoBindInjectable: true });
 
-export const AppModule = Container.merge(Module, AxiosModule);
+export const AppModule = Container.merge(Module, AXIOS, GATEWAY);
