c21442bb4d443355d294e02624a1da35ce7171cf
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 10:11:16 -0300

    [:sparkles:] | update: factory - app > user (module)

--- twitter_clone/src/app/modules/app.registry.ts
+++ twitter_clone/src/app/modules/app.registry.ts
@@ -1,7 +1,9 @@
 import { AxiosRegistry } from './axios/axios.registry';
 import { GatewayRegistry as GATEWAY } from './gateway/gateway.registry';
+import { UserRegistry as USER } from './@core/user/user.registry';
 
 export const MODULE = {
   ...AxiosRegistry,
   GATEWAY,
+  USER,
 };
