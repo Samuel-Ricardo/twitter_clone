62275fd7a5b416e3a75fb21481416ef403f27c9e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 08:56:16 -0300

    [:recycle:] | refact

--- twitter_clone/src/app/modules/@core/user/validator/user.validator.ts
+++ twitter_clone/src/app/modules/@core/user/validator/user.validator.ts
@@ -1,21 +1,8 @@
-import { z } from 'zod';
+import { User } from '../entity/user.entity';
+import { ICreateUserDTO } from '../DTO/create.dto';
 
-export const UserValidator = z.object({
-  id: z.string(),
-
-  name: z.string(),
-  username: z.string(),
-  email: z.string(),
-  password: z.string(),
-
-  createdAt: z.date(),
-  updatedAt: z.date(),
-  hasNotifications: z.boolean(),
-
-  sessionToken: z.string().nullish(),
-  bio: z.string().nullish(),
-  emailVerified: z.date().nullish(),
-  image: z.string().nullish(),
-  coverImage: z.string().nullish(),
-  profileImage: z.string().nullish(),
-});
+export interface IUserValidator {
+  validateEntity(user: User): boolean;
+  validateDTO<T>(dto: T, schema: any): boolean;
+  validateCreateDTO(dto: ICreateUserDTO): boolean;
+}
