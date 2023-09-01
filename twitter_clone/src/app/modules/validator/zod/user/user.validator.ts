a03cfd5fc4fa4c3c3bf39662329ad2a261d79fd3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 09:05:20 -0300

    [:safety_vest:] | create: validator - user (zod)

---
+++ twitter_clone/src/app/modules/validator/zod/user/user.validator.ts
@@ -0,0 +1,37 @@
+import { User } from '@/app/modules/@core/user/entity/user.entity';
+import { IUserValidator } from '../../../@core/user/validator/user.validator';
+import { injectable } from 'inversify';
+import {
+  UserSchema,
+  CreateUserSchema,
+  DeleteUserScheme,
+  UpdateUserSchema,
+  SelectUserByIdSchema,
+} from '../../../@core/user/validator';
+import { z } from 'zod';
+import {
+  ICreateUserDTO,
+  IUserDTO,
+  IUpdateUserDTO,
+  IDeleteuserDTO,
+  ISelectUserByIdDTO,
+} from '@/app/modules/@core/user/DTO';
+
+@injectable()
+export class ZodUserValidator implements IUserValidator {
+  validateEntity(user: User): boolean {
+    return UserSchema.parse(user.toStruct()) ? true : false;
+  }
+
+  validateDTO<T>(DTO: T, schema: z.ZodAny): boolean {
+    return schema.parse(DTO) ? true : false;
+  }
+
+  validateCreateDTO(DTO: ICreateUserDTO): boolean {
+    return CreateUserSchema.parse(DTO) ? true : false;
+  }
+
+  validateUpdateDTO(DTO: IUpdateUserDTO) {
+    return UpdateUserSchema.parse(DTO) ? true : false;
+  }
+}
