b468d3ec1728eeae1748154455a32a5556dce055
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 09:32:55 -0300

    [:sparkles:] | update: registry - app (module)

--- twitter_clone/src/app/modules/app.registry.ts
+++ twitter_clone/src/app/modules/app.registry.ts
@@ -1,5 +1,7 @@
 import { AxiosRegistry } from './axios/axios.registry';
+import { GatewayRegistry as GATEWAY } from './gateway/gateway.registry';
 
 export const MODULE = {
   ...AxiosRegistry,
+  GATEWAY,
 };
