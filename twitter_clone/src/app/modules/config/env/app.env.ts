368bfc735cf13c9fa5f382ebffeca748619f33a2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 23:55:10 -0300

    [:closed_lock_with_key:] | update: config - ENV (module)

--- twitter_clone/src/app/modules/config/env/app.env.ts
+++ twitter_clone/src/app/modules/config/env/app.env.ts
@@ -1,17 +1,13 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export const ENV = {
   ...process.env,
 
   API: {
-    URL: process.env.API_URL || 'http://localhost:6000',
+    URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3004',
   },
 
   NEXT: {
     API: {
-      URL: process.env.NEXT_API_URL || 'http://localhost:3000',
+      URL: process.env.NEXT_PUBLIC_NEXT_API_URL || 'http://localhost:3000',
     },
   },
 };
