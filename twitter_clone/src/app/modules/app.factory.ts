8f041dcbae6eb9d181cfdd7d75ec752eef201c8c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 23:53:22 -0300

    [:gear:] | update: factory - app > config (module)

--- twitter_clone/src/app/modules/app.factory.ts
+++ twitter_clone/src/app/modules/app.factory.ts
@@ -1,9 +1,11 @@
 import { AxiosFactory } from './axios/axios.factory';
 import { GatewayFactory as GATEWAY } from './gateway/gateway.factory';
 import { UserFactory as USER } from './@core/user/user.factory';
+import { ConfigFactory as CONFIG } from './config/config.factory';
 
 export const MODULES = {
   ...AxiosFactory,
   GATEWAY,
   USER,
+  CONFIG,
 };
