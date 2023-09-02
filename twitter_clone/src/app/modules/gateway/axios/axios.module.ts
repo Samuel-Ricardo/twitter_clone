cd8402806664861b72eaba9d4c773b12237f6099
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-02 00:05:19 -0300

    [:sparkles:] | update: module - axios (module/gateway)

--- twitter_clone/src/app/modules/gateway/axios/axios.module.ts
+++ twitter_clone/src/app/modules/gateway/axios/axios.module.ts
@@ -3,10 +3,15 @@ import { AxiosModule } from '../../axios/axios.module';
 import { AxiosGatewayRegistry } from './axios.registry';
 import { AxiosHTTPGateway } from './generic/http.gateway';
 import { AxiosUserGateway } from './user/user.gateway';
+import { ConfigModule } from '../../config/config.module';
 
 const Module = new Container({ autoBindInjectable: true });
 
-export const AxiosGatewayModule = Container.merge(Module, AxiosModule);
+export const AxiosGatewayModule = Container.merge(
+  Module,
+  AxiosModule,
+  ConfigModule,
+);
 
 AxiosGatewayModule.bind(AxiosGatewayRegistry.GENERIC.HTTP).to(AxiosHTTPGateway);
 AxiosGatewayModule.bind(AxiosGatewayRegistry.USER).to(AxiosUserGateway);
