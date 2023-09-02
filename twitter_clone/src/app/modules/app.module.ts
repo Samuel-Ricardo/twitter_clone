482667be355832a08552c487c8c9db93d6be4ce4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-01 23:51:55 -0300

    [:gear:] | update: module - app > config (module)

--- twitter_clone/src/app/modules/app.module.ts
+++ twitter_clone/src/app/modules/app.module.ts
@@ -1,8 +1,11 @@
+import 'reflect-metadata';
+
 import { Container } from 'inversify';
 import { AxiosModule as AXIOS } from './axios/axios.module';
 import { GatewayModule as GATEWAY } from './gateway/gateway.module';
 import { UserModule as USER } from './@core/user/user.module';
+import { ConfigModule as CONFIG } from './config/config.module';
 
 const Module = new Container({ autoBindInjectable: true });
 
-export const AppModule = Container.merge(Module, AXIOS, GATEWAY, USER);
+export const AppModule = Container.merge(Module, AXIOS, GATEWAY, USER, CONFIG);
