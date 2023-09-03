ca9586846adbd9be48e01c07c2a23598067065a6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 13:40:12 -0300

    [:clown_face:] | simulate: controller - user (mock/module)

--- twitter_clone/test/mock/module/@core/user/controller/user.controller.ts
+++ twitter_clone/test/mock/module/@core/user/controller/user.controller.ts
@@ -1,4 +1,19 @@
 import { UserController } from '@/app/modules/@core/user/controller';
+import { ISimulatedUserController } from '@test/@types/simulate/user/controller';
 import { mockDeep } from 'jest-mock-extended';
+import { interfaces } from 'inversify';
+import { USER_REGISTRY_MOCK } from '../user.registry';
 
 export const mockUserController = () => mockDeep<UserController>();
+
+export const simulateUserController = ({
+  container,
+}: interfaces.Context): ISimulatedUserController => {
+  const service = container.get<any>(USER_REGISTRY_MOCK.SERVICE.MOCK);
+  const controller = new UserController(service);
+
+  return {
+    controller,
+    service,
+  };
+};
