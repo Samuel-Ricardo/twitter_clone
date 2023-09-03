7896407ded20da9971e4a94d3a5f5eed34fdc40c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 15:54:30 -0300

    [:clown_face:] | update: factory - app (mock/module)

--- twitter_clone/test/mock/module/app.factory.ts
+++ twitter_clone/test/mock/module/app.factory.ts
@@ -1,7 +1,9 @@
 import { USER_FACTORY_MOCK as USER } from './@core/user/user.factory';
 import { AXIOS_FACTORY_MOCK as AXIOS } from './axios/axios.factory';
+import { GATEWAY_FACTORY_MOCK as GATEWAY } from './gateway/gateway.factory';
 
 export const MODULES_MOCK = {
   USER,
   AXIOS,
+  GATEWAY,
 };
