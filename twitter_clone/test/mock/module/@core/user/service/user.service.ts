7b3003b02953946585a4fa77d03fdec1fede29bc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 13:23:11 -0300

    [:clown_face:] | simulate: service - user (mock/module)

--- twitter_clone/test/mock/module/@core/user/service/user.service.ts
+++ twitter_clone/test/mock/module/@core/user/service/user.service.ts
@@ -1,4 +1,32 @@
 import { UserService } from '@/app/modules/@core/user/service';
+import { ISimulatedUserService } from '@test/@types/simulate';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
+import { USER_REGISTRY_MOCK } from '../user.registry';
 
 export const mockUserService = () => mockDeep<UserService>();
+
+export const simulateUserService = ({
+  container,
+}: interfaces.Context): ISimulatedUserService => {
+  const create = container.get<any>(USER_REGISTRY_MOCK.USE_CASE.CREATE);
+  const update = container.get<any>(USER_REGISTRY_MOCK.USE_CASE.UPDATE);
+  const delete_user = container.get<any>(USER_REGISTRY_MOCK.USE_CASE.DELETE);
+  const all = container.get<any>(USER_REGISTRY_MOCK.USE_CASE.GET.ALL);
+  const id = container.get<any>(USER_REGISTRY_MOCK.USE_CASE.GET.BY.ID);
+
+  const service = new UserService(create, update, id, all, delete_user);
+
+  return {
+    servcie: service,
+    use_case: {
+      create: create,
+      update: update,
+      delete: delete_user,
+      get: {
+        all: all,
+        by: id,
+      },
+    },
+  };
+};
