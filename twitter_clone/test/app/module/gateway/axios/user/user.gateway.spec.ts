f861e7cc4d777f0d2d88ed04340089a89b955f69
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 16:56:46 -0300

    [:white_check_mark:] | create: [unit] test - create > user (gateway/axios)

--- twitter_clone/test/app/module/gateway/axios/user/user.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/user/user.gateway.spec.ts
@@ -1,3 +1,7 @@
+/*
+ * @jest-environment node
+ */
+
 import 'reflect-metadata';
 
 import { AxiosUserGateway } from '@/app/modules/gateway/axios/user/user.gateway';
@@ -5,6 +9,7 @@ import { ISimulatedUserGateweay } from '@test/@types/simulate/user/gateway';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import axios from 'axios';
 import { expect } from '@jest/globals';
+import { CREATE_USER_DATA, VALID_USER } from '@test/mock/data/user';
 
 describe('[GATEWAY] | AXIOS => [USER]', () => {
   let MODULE: ISimulatedUserGateweay<AxiosUserGateway, typeof axios>;
@@ -19,7 +24,20 @@ describe('[GATEWAY] | AXIOS => [USER]', () => {
     expect(MODULE.client).toBeDefined();
   });
 
-  it('should be defined', () => {
-    expect(MODULE).toBeDefined();
+  it('[UNIT] | Should: create => [USER]', async () => {
+    MODULE.client.post.mockResolvedValue({
+      data: { user: VALID_USER.toStruct() },
+    });
+
+    const result = await MODULE.gateway.create(CREATE_USER_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(VALID_USER);
+    expect(MODULE.client.post).toHaveBeenCalledTimes(1);
+    expect(MODULE.client.post).toHaveBeenCalledWith(
+      MODULE.gateway.fullURL,
+      CREATE_USER_DATA,
+      undefined,
+    );
   });
 });
