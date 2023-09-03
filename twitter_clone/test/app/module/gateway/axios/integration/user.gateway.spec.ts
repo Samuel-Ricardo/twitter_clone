59205d2e587b02cbeb99b9843d4b1fdebd9ec757
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 17:38:02 -0300

    [:white_check_mark:] | create: test suit - gateway > user (module)

--- twitter_clone/test/app/module/gateway/axios/integration/user.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/integration/user.gateway.spec.ts
@@ -4,105 +4,19 @@
 
 import 'reflect-metadata';
 
+import { MODULES } from '@/app/modules';
 import { AxiosUserGateway } from '@/app/modules/gateway/axios/user/user.gateway';
-import { ISimulatedUserGateweay } from '@test/@types/simulate/user/gateway';
-import { MODULES_MOCK } from '@test/mock/module/app.factory';
-import axios from 'axios';
 import { expect } from '@jest/globals';
-import {
-  CREATE_USER_DATA,
-  UPDATE_USER_DATA,
-  VALID_UPDATE_USER as VALID_UPDATED_USER,
-  VALID_USER,
-} from '@test/mock/data/user';
+import { User } from '@/app/modules/@core/user/entity/user.entity';
 
-describe('[GATEWAY] | AXIOS => [USER]', () => {
-  let MODULE: ISimulatedUserGateweay<AxiosUserGateway, typeof axios>;
+describe('[GATEWAY] | AXIOS => [USER] ', () => {
+  let gateway: AxiosUserGateway;
 
-  beforeEach(() => {
-    jest.clearAllMocks();
+  beforeAll(() => {
+    gateway = MODULES.GATEWAY.AXIOS.USER();
 
-    MODULE = MODULES_MOCK.GATEWAY.AXIOS.USER.SIMULATE();
-
-    expect(MODULE).toBeDefined();
-    expect(MODULE.gateway).toBeInstanceOf(AxiosUserGateway);
-    expect(MODULE.client).toBeDefined();
+    expect(gateway).toBeInstanceOf(AxiosUserGateway);
   });
 
-  it('[UNIT] | Should: create => [USER]', async () => {
-    MODULE.client.post.mockResolvedValue({
-      data: { user: VALID_USER.toStruct() },
-    });
-
-    const result = await MODULE.gateway.create(CREATE_USER_DATA);
-
-    expect(result).toBeDefined();
-    expect(result).toEqual(VALID_USER);
-    expect(MODULE.client.post).toHaveBeenCalledTimes(1);
-    expect(MODULE.client.post).toHaveBeenCalledWith(
-      MODULE.gateway.fullURL,
-      CREATE_USER_DATA,
-      undefined,
-    );
-  });
-
-  it('[UNIT] | Should: list [all] => [USER]', async () => {
-    MODULE.client.get.mockResolvedValue({
-      data: { users: [VALID_USER.toStruct()] },
-    });
-
-    const result = await MODULE.gateway.listAll();
-
-    expect(result).toBeDefined();
-    expect(result).toEqual([VALID_USER]);
-    expect(MODULE.client.get).toHaveBeenCalledTimes(1);
-    expect(MODULE.client.get).toHaveBeenCalledWith(
-      MODULE.gateway.fullURL,
-      undefined,
-    );
-  });
-
-  it('[UNIT] | Should: select [by] => [USER]', async () => {
-    MODULE.client.get.mockResolvedValue({
-      data: { user: VALID_USER.toStruct() },
-    });
-
-    const result = await MODULE.gateway.selectById({ id: VALID_USER.id });
-
-    expect(result).toBeDefined();
-    expect(result).toEqual(VALID_USER);
-    expect(MODULE.client.get).toHaveBeenCalledTimes(1);
-    expect(MODULE.client.get).toHaveBeenCalledWith(
-      `${MODULE.gateway.fullURL}/${VALID_USER.id}`,
-      undefined,
-    );
-  });
-
-  it('[UNIT] | Should: update => [USER]', async () => {
-    MODULE.client.put.mockResolvedValue({
-      data: { user: VALID_UPDATED_USER.toStruct() },
-    });
-
-    const result = await MODULE.gateway.update(UPDATE_USER_DATA);
-
-    expect(result).toBeDefined();
-    expect(result).toEqual(VALID_UPDATED_USER);
-    expect(result.id).toEqual(VALID_USER.id);
-    expect(result.bio).not.toEqual(VALID_USER.bio);
-
-    expect(MODULE.client.put).toHaveBeenCalledTimes(1);
-    expect(MODULE.client.put).toHaveBeenCalledWith(
-      MODULE.gateway.fullURL,
-      UPDATE_USER_DATA,
-      undefined,
-    );
-  });
-
-  it('[UNIT] | Should: delete => [USER]', async () => {
-    MODULE.client.delete.mockResolvedValue({ status: 204 });
-
-    expect(
-      MODULE.gateway.deleteUser({ id: VALID_USER.id }),
-    ).resolves.not.toThrow();
-  });
+  it('[INTEGRATION] | Should: list [all] => [USER]', async () => {});
 });
