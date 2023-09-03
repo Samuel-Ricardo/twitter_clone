b2d4559ba89b15b38a8957a9e4e60e5629c96439
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 15:10:25 -0300

    [:clown_face:] | update: module - app (mock/module)

--- twitter_clone/test/mock/module/app.module.ts
+++ twitter_clone/test/mock/module/app.module.ts
@@ -1,6 +1,11 @@
 import { Container } from 'inversify';
 import { USER_MODULE_MOCK } from './@core/user/user.module';
+import { AXIOS_MODULE_MOCK } from './axios/axios.module';
 
 const MODULE = new Container();
 
-export const APP_MODULE_MOCK = Container.merge(MODULE, USER_MODULE_MOCK);
+export const APP_MODULE_MOCK = Container.merge(
+  MODULE,
+  USER_MODULE_MOCK,
+  AXIOS_MODULE_MOCK,
+);
