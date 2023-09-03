dfda3e9e2b24d86dc23335daad134023a299ca15
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 15:11:06 -0300

    [:clown_face:] | update: factory - app (mock/module)

--- twitter_clone/test/mock/module/app.factory.ts
+++ twitter_clone/test/mock/module/app.factory.ts
@@ -1,5 +1,7 @@
 import { USER_FACTORY_MOCK as USER } from './@core/user/user.factory';
+import { AXIOS_FACTORY_MOCK as AXIOS } from './axios/axios.factory';
 
 export const MODULES_MOCK = {
   USER,
+  AXIOS,
 };
