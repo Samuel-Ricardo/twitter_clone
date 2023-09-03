1cbab325f6d4b869f7f8a508fda0b9baab260c04
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-03 13:07:37 -0300

    [:clown_face:] | create: module - user (mock/module)

---
+++ twitter_clone/test/mock/module/@core/user/user.module.ts
@@ -0,0 +1,33 @@
+import { Container } from 'inversify';
+import { USER_REGISTRY_MOCK } from './user.registry';
+import {
+  mockCreatUserUseCase,
+  mockDeletUserUseCase,
+  mockUpdateUserUseCase,
+  mockListAllUsersUseCase,
+  mockSelectUserByIdUseCase,
+} from './use-case';
+
+const MODULE = new Container();
+
+export const USER_MODULE_MOCK = MODULE;
+
+USER_MODULE_MOCK.bind(USER_REGISTRY_MOCK.USE_CASE.CREATE).toDynamicValue(
+  mockCreatUserUseCase,
+);
+
+USER_MODULE_MOCK.bind(USER_REGISTRY_MOCK.USE_CASE.UPDATE).toDynamicValue(
+  mockUpdateUserUseCase,
+);
+
+USER_MODULE_MOCK.bind(USER_REGISTRY_MOCK.USE_CASE.DELETE).toDynamicValue(
+  mockDeletUserUseCase,
+);
+
+USER_MODULE_MOCK.bind(USER_REGISTRY_MOCK.USE_CASE.GET.ALL).toDynamicValue(
+  mockListAllUsersUseCase,
+);
+
+USER_MODULE_MOCK.bind(USER_REGISTRY_MOCK.USE_CASE.GET.BY.ID).toDynamicValue(
+  mockSelectUserByIdUseCase,
+);
