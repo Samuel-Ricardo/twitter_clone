8994d9d6831e24bc9cf4c4360b9098c94a645882
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-04 10:32:40 -0300

    [:safety_vest:] | update: schema - user (module)

--- twitter_clone/src/app/modules/@core/user/validator/user.schema.ts
+++ twitter_clone/src/app/modules/@core/user/validator/user.schema.ts
@@ -10,13 +10,13 @@ export const UserSchema = z
     email: z.string(),
     password: z.string(),
 
-    createdAt: z.string(),
-    updatedAt: z.string(),
+    createdAt: z.string().or(z.date()),
+    updatedAt: z.string().or(z.date()),
     hasNotifications: z.boolean(),
 
     sessionToken: z.string().nullish(),
     bio: z.string().nullish(),
-    emailVerified: z.string().nullish(),
+    emailVerified: z.string().or(z.date()).nullish(),
     image: z.string().nullish(),
     coverImage: z.string().nullish(),
     profileImage: z.string().nullish(),
