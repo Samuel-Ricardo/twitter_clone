8a9f3f47815e162006ec1cb2529957f3f66336e3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-04 15:08:00 -0300

    [:white_check_mark:] | create: [unit] test - create > user (controller)

--- twitter_clone/test/app/module/core/user/user.controller.spec.ts
+++ twitter_clone/test/app/module/core/user/user.controller.spec.ts
@@ -7,7 +7,12 @@ import 'reflect-metadata';
 import { ISimulatedUserController } from '@test/@types/simulate/user/controller';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { expect } from '@jest/globals';
-import { CREATE_USER_DATA, VALID_USER } from '@test/mock/data/user';
+import {
+  CREATE_USER_DATA,
+  UPDATE_USER_DATA,
+  VALID_UPDATE_USER,
+  VALID_USER,
+} from '@test/mock/data/user';
 
 describe('[CONTROLLER] | @CORE => [USER]', () => {
   let MODULE: ISimulatedUserController;
@@ -32,5 +37,17 @@ describe('[CONTROLLER] | @CORE => [USER]', () => {
 
     expect(MODULE.service.create).toHaveBeenCalledTimes(1);
     expect(MODULE.service.create).toHaveBeenCalledWith(CREATE_USER_DATA);
+  });
+
+  it('[UNIT] | Should: update => [USER]', async () => {
+    MODULE.service.update.mockResolvedValue(VALID_UPDATE_USER);
+
+    const result = await MODULE.controller.update(UPDATE_USER_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual({ user: VALID_UPDATE_USER.toStruct() });
+
+    expect(MODULE.service.update).toHaveBeenCalledTimes(1);
+    expect(MODULE.service.update).toHaveBeenCalledWith(UPDATE_USER_DATA);
   });
 });
