ec3746e6eb4391134b1de796830f1004e9a7899b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-04 10:37:08 -0300

    [:clown_face:] | update: module - axios (mock/module)

--- twitter_clone/test/mock/module/axios/axios.module.ts
+++ twitter_clone/test/mock/module/axios/axios.module.ts
@@ -2,6 +2,9 @@ import { Container } from 'inversify';
 import { AXIOS_REGISTRY_MOCK } from './axios.registry';
 import { mockAxios } from './axios';
 
-export const AXIOS_MODULE_MOCK = new Container();
+export const AXIOS_MODULE_MOCK = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
 
 AXIOS_MODULE_MOCK.bind(AXIOS_REGISTRY_MOCK.MOCK).toDynamicValue(mockAxios);
