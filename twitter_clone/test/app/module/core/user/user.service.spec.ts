11286d2be8e46fe428fdb6e61eccb6c39529eb32
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-04 11:03:48 -0300

    [:white_check_mark:] | create: [unit] test - service > create (user)

--- twitter_clone/test/app/module/core/user/user.service.spec.ts
+++ twitter_clone/test/app/module/core/user/user.service.spec.ts
@@ -7,6 +7,7 @@ import 'reflect-metadata';
 import { expect, describe, it } from '@jest/globals';
 import { ISimulatedUserService } from '@test/@types';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
+import { CREATE_USER_DATA, VALID_USER } from '@test/mock/data/user';
 
 describe('[SERVICE] | @CORE => [USER]', () => {
   let MODULE: ISimulatedUserService;
@@ -24,6 +25,15 @@ describe('[SERVICE] | @CORE => [USER]', () => {
   });
 
   it('[UNIT] | Should: create => [USER]', async () => {
-    expect(true).toBeTruthy();
+    MODULE.use_case.create.execute.mockResolvedValue(VALID_USER);
+
+    const result = await MODULE.service.create(CREATE_USER_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual(VALID_USER);
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledTimes(1);
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledWith(
+      CREATE_USER_DATA,
+    );
   });
 });
