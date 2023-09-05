f407a942dd62433e7e433f469edeadbd62903a3a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 14:50:35 -0300

    [:sparkles:] | create: service - post (module)

---
+++ twitter_clone/src/app/modules/@core/post/service/post.service.ts
@@ -0,0 +1,28 @@
+import { MODULE } from '@/app/modules';
+import { inject, injectable } from 'inversify';
+import {
+  CreatePostUseCase,
+  FindAllPostUseCase,
+  FindPostByIdUseCase,
+  DeletePostUseCase,
+  UpdatePostUseCase,
+  FindPostsByAuthorUseCase,
+} from '../use-case';
+
+@injectable()
+export class PostService {
+  constructor(
+    @inject(MODULE.POST.USE_CASE.CREATE)
+    private readonly createPost: CreatePostUseCase,
+    @inject(MODULE.POST.USE_CASE.FIND.ALL)
+    private readonly findAllPost: FindAllPostUseCase,
+    @inject(MODULE.POST.USE_CASE.FIND.BY.ID)
+    private readonly findPostById: FindPostByIdUseCase,
+    @inject(MODULE.POST.USE_CASE.DELETE)
+    private readonly deletePost: DeletePostUseCase,
+    @inject(MODULE.POST.USE_CASE.UPDATE)
+    private readonly updatePost: UpdatePostUseCase,
+    @inject(MODULE.POST.USE_CASE.FIND.BY.AUTHOR.ID)
+    private readonly findPostsByAuthor: FindPostsByAuthorUseCase,
+  ) {}
+}
