48817841a768ae2d4753e82230971d3113385a8b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 10:49:56 -0300

    [:safety_vest:] | create: schema - post (module)

---
+++ twitter_clone/src/app/modules/@core/post/validator/schema/post.schema.ts
@@ -0,0 +1,13 @@
+import { catchZod } from '@/app/modules/util';
+import { z } from 'zod';
+
+export const PostSchema = z
+  .object({
+    id: z.string().catch(catchZod),
+    body: z.string().catch(catchZod),
+    authorId: z.string().catch(catchZod),
+    createdAt: z.date().catch(catchZod),
+    updatedAt: z.date().catch(catchZod),
+    image: z.string().optional().nullish().catch(catchZod),
+  })
+  .catch(catchZod);
