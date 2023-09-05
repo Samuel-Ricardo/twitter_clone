cd98f978fad085ab530a1bb2948c15b44f1c836a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 14:21:11 -0300

    [:sparkles:] | update: use-case - update > post (module)

--- twitter_clone/src/app/modules/@core/post/use-case/update.use-case.ts
+++ twitter_clone/src/app/modules/@core/post/use-case/update.use-case.ts
@@ -1,6 +1,8 @@
+import { injectable } from 'inversify';
 import { IUpdatePostDTO } from '../DTO';
 import { PostGatewayAccess } from '../gateway';
 
+@injectable()
 export class UpdatePostUseCase extends PostGatewayAccess {
   async execute(post: IUpdatePostDTO) {
     return this.gateway.update(post);
