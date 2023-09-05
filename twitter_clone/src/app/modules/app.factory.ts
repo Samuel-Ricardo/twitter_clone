981595ab0933e512518c624181f32db18df69801
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 14:48:55 -0300

    [:sparkles:] | update: factory - app (module)

--- twitter_clone/src/app/modules/app.factory.ts
+++ twitter_clone/src/app/modules/app.factory.ts
@@ -2,10 +2,12 @@ import { AxiosFactory } from './axios/axios.factory';
 import { GatewayFactory as GATEWAY } from './gateway/gateway.factory';
 import { UserFactory as USER } from './@core/user/user.factory';
 import { ConfigFactory as CONFIG } from './config/config.factory';
+import { POST_FACTORY as POST } from './@core/post/post.factory';
 
 export const MODULES = {
   ...AxiosFactory,
-  GATEWAY,
   USER,
+  POST,
   CONFIG,
+  GATEWAY,
 };
