f51465e6d4634d2fd1d4ab62b0a1a7db5d7697ba
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 15:16:32 -0300

    [:sparkles:] | create: controller - create > post (module)

--- twitter_clone/src/app/modules/@core/post/controller/post.controller.ts
+++ twitter_clone/src/app/modules/@core/post/controller/post.controller.ts
@@ -1,6 +1,13 @@
 import { inject, injectable } from 'inversify';
 import { PostService } from '../service';
 import { MODULE } from '../../../app.registry';
+import {
+  ICreatePostDTO,
+  IFindPostByAuthorIdDTO,
+  IDeletePostDTO,
+  IUpdatePostDTO,
+  IFindPostByIdDTO,
+} from '../DTO';
 
 @injectable()
 export class PostController {
@@ -8,4 +15,9 @@ export class PostController {
     @inject(MODULE.POST.SERVICE)
     private readonly service: PostService,
   ) {}
+
+  async create(post: ICreatePostDTO) {
+    const result = await this.service.create(post);
+    return { post: result.toStruct() };
+  }
 }
