d03028ac3d93471f1bd337165a8f6cd5dd88166b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 11:39:47 -0300

    [:sparkles:] | update: index - DTO (post)

--- twitter_clone/src/app/modules/@core/post/DTO/index.ts
+++ twitter_clone/src/app/modules/@core/post/DTO/index.ts
@@ -1,1 +1,6 @@
 export * from './post.dto';
+export * from './create.dto';
+export * from './update.dto';
+export * from './delete.dto';
+export * from './find_by_id.dto';
+export * from './find_by_author.dto';
