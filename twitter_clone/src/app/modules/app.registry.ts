bc2223d43a5a8876bf427a1976b3b5b3770b3e87
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 14:43:34 -0300

    [:sparkles:] | update: registry - app (module)

--- twitter_clone/src/app/modules/app.registry.ts
+++ twitter_clone/src/app/modules/app.registry.ts
@@ -2,10 +2,12 @@ import { AxiosRegistry } from './axios/axios.registry';
 import { GatewayRegistry as GATEWAY } from './gateway/gateway.registry';
 import { UserRegistry as USER } from './@core/user/user.registry';
 import { ConfigRegistry as CONFIG } from './config/config.registry';
+import { POST_REGISTRY as POST } from './@core/post/post.registry';
 
 export const MODULE = {
   ...AxiosRegistry,
-  GATEWAY,
   USER,
+  POST,
   CONFIG,
+  GATEWAY,
 };
