0494928d06b8dd84501d6679f302c56ef748b148
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 12:18:33 -0300

    [:sparkles:] | create: gateway - create > post (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/post/post.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/post/post.gateway.ts
@@ -1,6 +1,16 @@
 import { IPostGateway } from '@/app/modules/@core/post/gateway';
 import { AxiosHTTPGateway } from '../generic/http.gateway';
 import { injectable } from 'inversify';
+import {
+  ICreatePostDTO,
+  IUpdatePostDTO,
+  IDeletePostDTO,
+  IFindPostByAuthorIdDTO,
+  IFindPostByIdDTO,
+  IPostDTO,
+} from '@/app/modules/@core/post/DTO';
+import { CreatePostSchema } from '@/app/modules/@core/post/validator/schema';
+import { Post } from '@/app/modules/@core/post/entity';
 
 @injectable()
 export class AxiosPostGateway extends AxiosHTTPGateway implements IPostGateway {
@@ -8,5 +18,12 @@ export class AxiosPostGateway extends AxiosHTTPGateway implements IPostGateway {
 
   get fullURL() {
     return `${this.URL}/${this.prefix}`;
+  }
+
+  async create(post: ICreatePostDTO) {
+    CreatePostSchema.parse(post);
+
+    const response = await this.post<{ post: IPostDTO }>(this.prefix, post);
+    return Post.create(response.data.post);
   }
 }
