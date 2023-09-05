5f571b75ce2e9af3c86a90cd46592f9f65d324b8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 10:11:29 -0300

    [:clown_face:] | simulate: hook - list [all] > user (mock/module)

--- twitter_clone/test/mock/module/hooks/user/all.hook.ts
+++ twitter_clone/test/mock/module/hooks/user/all.hook.ts
@@ -2,9 +2,22 @@ import { useUsers } from '@/app/hooks/user/all.hook';
 import { mockFn } from 'jest-mock-extended';
 
 import { MODULES } from '@/app/modules';
+import { interfaces } from 'inversify';
+import { ISimulatedUseUsersHook } from '@test/@types/simulate/user/hooks/all';
+import { MODULE_MOCK } from '../../app.registry';
 
 jest.mock('../../../../../src/app/modules/@core/user/user.factory.ts');
 
 export { useUsers, MODULES };
 
 export const mockUseUsers = () => mockFn<typeof useUsers>();
+
+export const simulateUseUsers = ({
+  container,
+}: interfaces.Context): ISimulatedUseUsersHook => {
+  const controller = container.get<any>(MODULE_MOCK.USER.CONTROLLER.MOCK);
+
+  MODULES.USER.MAIN.mockReturnValue(controller);
+
+  return { hook: useUsers, controller };
+};
