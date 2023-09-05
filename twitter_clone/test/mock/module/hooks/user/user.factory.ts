e8991df1fb0aa5bae381d79e19f818b63f05e32f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 10:12:20 -0300

    [:clown_face:] | update: factory - hooks > user (mock/module)

--- twitter_clone/test/mock/module/hooks/user/user.factory.ts
+++ twitter_clone/test/mock/module/hooks/user/user.factory.ts
@@ -2,14 +2,17 @@ import { DeepMockProxy } from 'jest-mock-extended';
 import { USER_HOOKS_MODULE_MOCK } from './user.module';
 import { useUsers } from '@/app/hooks/user/all.hook';
 import { USER_HOOKS_REGISTRY_MOCK } from './user.registry';
+import { ISimulatedUseUsersHook } from '@test/@types/simulate/user/hooks/all';
 
 export const USER_HOOKS_FACTORY_MOCK = {
   ALL: {
-    MOCK: USER_HOOKS_MODULE_MOCK.get<DeepMockProxy<typeof useUsers>>(
-      USER_HOOKS_REGISTRY_MOCK.ALL.MOCK,
-    ),
-    SIMULATE: USER_HOOKS_MODULE_MOCK.get<typeof useUsers>(
-      USER_HOOKS_REGISTRY_MOCK.ALL.SIMULATE,
-    ),
+    MOCK: () =>
+      USER_HOOKS_MODULE_MOCK.get<DeepMockProxy<typeof useUsers>>(
+        USER_HOOKS_REGISTRY_MOCK.ALL.MOCK,
+      ),
+    SIMULATE: () =>
+      USER_HOOKS_MODULE_MOCK.get<ISimulatedUseUsersHook>(
+        USER_HOOKS_REGISTRY_MOCK.ALL.SIMULATE,
+      ),
   },
 };
