76bf51f2d91f0f00004024bad0a344f00b5fd3f8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 11:36:58 -0300

    [:safety_vest:] | update: schema - create > post (module)

--- twitter_clone/src/app/modules/@core/post/validator/schema/index.ts
+++ twitter_clone/src/app/modules/@core/post/validator/schema/index.ts
@@ -1,1 +1,6 @@
 export * from './post.schema';
+export * from './create.schema';
+export * from './delete.schema';
+export * from './update.schema';
+export * from './find_by_id.schema';
+export * from './find_by_author.schema';
