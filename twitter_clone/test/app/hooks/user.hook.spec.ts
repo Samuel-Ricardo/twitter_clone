eca5bc4e56ae53efc2c14f8f6abb1f75e39dc73f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 10:15:40 -0300

    [:white_check_mark:] | create: [unit] test - list [all] > user (module/hooks)

--- twitter_clone/test/app/hooks/user.hook.spec.ts
+++ twitter_clone/test/app/hooks/user.hook.spec.ts
@@ -15,5 +15,65 @@ describe('[HOOK] | USER', () => {
     MODULE = MODULES_MOCK.HOOKS.USER.ALL.SIMULATE();
   });
 
-  it('', () => expect(1).toBe(1));
+  it('[UNIT] | [HOOK] - Should: list [all] => [USER]', async () => {
+    MODULE.controller.listAll.mockReturnValue(SWR_USER([USER_DATA]));
+
+    const { result } = renderHook(() => useUsers());
+    const { data } = result.current;
+
+    expect(MODULE.controller.listAll).toHaveBeenCalledTimes(1);
+    expect(MODULE.controller.listAll).toHaveBeenCalledWith();
+
+    expect(result.current).toBeDefined();
+    expect(result.current.data).toBeDefined();
+
+    expect(data).toBeDefined();
+    expect(data).toStrictEqual([USER_DATA]);
+  });
+
+  it('[UNIT] | [HOOK] - Should: list [all] => [USER]', async () => {
+    MODULES.USER.MAIN.mockReturnValue({
+      listAll: jest.fn(),
+    });
+
+    const USER_DATA = {
+      name: 'pedro',
+      username: 'p3dr0',
+      email: 'pedro@email.com',
+      password: 'h3j2f6',
+      id: '123',
+      bio: 'Hello World! :D',
+      hasNotifications: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    console.log({ MODULES: MODULES.USER.MAIN() });
+
+    MODULES.USER.MAIN().listAll.mockReturnValue({
+      users: {
+        data: [USER_DATA],
+        error: null,
+        mutate: () => ({}) as any,
+        isLoading: false,
+        isValidating: false,
+      },
+    });
+
+    console.log({ MODULES2: MODULES.USER.MAIN().listAll() });
+
+    const { result } = renderHook(() => useUsers());
+    const { data, isLoading, error } = result.current;
+
+    console.log({ data: result.current });
+
+    console.log({ isLoading, error, data });
+
+    expect(MODULES.USER.MAIN().listAll).toHaveBeenCalled();
+
+    expect(result.current).toBeDefined();
+
+    expect(data).toBeDefined();
+    expect(data?.length).toBeGreaterThanOrEqual(0);
+  });
 });
