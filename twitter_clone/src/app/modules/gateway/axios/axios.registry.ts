7f1483418b5ca044390b4a3bfbe1f95f650cbd11
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 13:57:07 -0300

    [:sparkles:] | update: registry - gateway > axios (module)

--- twitter_clone/src/app/modules/gateway/axios/axios.registry.ts
+++ twitter_clone/src/app/modules/gateway/axios/axios.registry.ts
@@ -3,4 +3,5 @@ export const AxiosGatewayRegistry = {
     HTTP: Symbol.for('gateway.axios.generic.http'),
   },
   USER: Symbol.for('gateway.axios.user'),
+  POST: Symbol.for('gateway.axios.post'),
 };
