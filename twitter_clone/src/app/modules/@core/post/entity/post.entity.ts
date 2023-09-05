b739b98d2ddeddf0a8d5692fb91ec233cda78950
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-05 10:54:57 -0300

    [:safety_vest:] | validate: entity - post (module)

---
+++ twitter_clone/src/app/modules/@core/post/entity/post.entity.ts
@@ -0,0 +1,26 @@
+import { IPostDTO } from '../DTO/post.dto';
+import { PostSchema } from '../validator/schema/post.schema';
+
+export class Post {
+  constructor(
+    private _id: string,
+    private _body: string,
+    private _authorId: string,
+    private _createdAt: Date,
+    private _updatedAt: Date,
+    private _image?: string | null,
+  ) {
+    Post.validate({
+      id: _id,
+      body: _body,
+      authorId: _authorId,
+      createdAt: _createdAt,
+      updatedAt: _updatedAt,
+      image: _image,
+    });
+  }
+
+  static validate(post: IPostDTO) {
+    return PostSchema.parse(post) as IPostDTO;
+  }
+}
