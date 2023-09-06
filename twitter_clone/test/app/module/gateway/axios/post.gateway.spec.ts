a778327c04cb766af4391869f340dd45c958f652
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 15:13:40 -0300

    [:white_check_mark:] | create: [unit] test - create > post (module/gateway)

--- twitter_clone/test/app/module/gateway/axios/post.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/post.gateway.spec.ts
@@ -7,6 +7,7 @@ import 'reflect-metadata';
 import { AxiosPostGateway } from '@/app/modules/gateway/axios/post/post.gateway';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { expect } from '@jest/globals';
+import { CREATE_POST_DATA, VALID_POST } from '@test/mock/data/post';
 
 describe('[GATEWAY] | AXIOS => [POST]', () => {
   let MODULE = MODULES_MOCK.GATEWAY.AXIOS.POST.SIMULATE();
@@ -21,6 +22,21 @@ describe('[GATEWAY] | AXIOS => [POST]', () => {
   });
 
   it('[UNIT] | Should: create => [POST]', async () => {
-    expect(1).toBe(1);
+    MODULE.client.post.mockResolvedValue({
+      data: { post: VALID_POST.toStruct() },
+      status: 200,
+    });
+
+    const result = await MODULE.gateway.create(CREATE_POST_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toEqual(VALID_POST);
+
+    expect(MODULE.client.post).toHaveBeenCalledTimes(1);
+    expect(MODULE.client.post).toHaveBeenCalledWith(
+      MODULE.gateway.fullURL,
+      CREATE_POST_DATA,
+      undefined,
+    );
   });
 });
