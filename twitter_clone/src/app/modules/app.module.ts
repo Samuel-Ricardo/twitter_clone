c606135b8f4ffcc4e77027fe6bdbfde58433aa51
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 11:21:17 -0300

    [:sparkles:] | update: module - app (module)

--- twitter_clone/src/app/modules/app.module.ts
+++ twitter_clone/src/app/modules/app.module.ts
@@ -5,7 +5,15 @@ import { AxiosModule as AXIOS } from './axios/axios.module';
 import { GatewayModule as GATEWAY } from './gateway/gateway.module';
 import { UserModule as USER } from './@core/user/user.module';
 import { ConfigModule as CONFIG } from './config/config.module';
+import { POST_MODULE as POST } from './@core/post/post.module';
 
-const Module = new Container({ autoBindInjectable: true });
+const MODULE = new Container({ autoBindInjectable: true });
 
-export const AppModule = Container.merge(Module, AXIOS, GATEWAY, USER, CONFIG);
+export const AppModule = Container.merge(
+  MODULE,
+  USER,
+  POST,
+  CONFIG,
+  AXIOS,
+  GATEWAY,
+);
