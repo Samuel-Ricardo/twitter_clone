1fce1f231be19bfacdeee96c2cff2297ca42d152
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 15:56:47 -0300

    [:white_check_mark:] | create: [unit] test - create > post (module/service)

--- twitter_clone/test/app/module/core/post/post.service.spec.ts
+++ twitter_clone/test/app/module/core/post/post.service.spec.ts
@@ -7,7 +7,12 @@ import 'reflect-metadata';
 import { ISimulatedPostService } from '@test/@types/simulate/post';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { expect } from '@jest/globals';
-import { CREATE_POST_DATA, VALID_POST } from '@test/mock/data/post';
+import {
+  CREATE_POST_DATA,
+  UPDATE_POST_DATA,
+  VALID_POST,
+  VALID_UPDATED_POST,
+} from '@test/mock/data/post';
 
 describe('[SERVICE] | POST', () => {
   let MODULE: ISimulatedPostService;
@@ -29,5 +34,27 @@ describe('[SERVICE] | POST', () => {
 
     expect(result).toBeDefined();
     expect(result).toStrictEqual(VALID_POST);
+
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledTimes(1);
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledWith(
+      CREATE_POST_DATA,
+    );
+  });
+
+  it('[UNIT] | Should: update => [POST]', async () => {
+    MODULE.use_case.update.execute.mockResolvedValue(VALID_UPDATED_POST);
+
+    const result = await MODULE.service.update(UPDATE_POST_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual(VALID_UPDATED_POST);
+
+    expect(result.id).toEqual(VALID_POST.id);
+    expect(result.body).not.toEqual(VALID_POST.body);
+
+    expect(MODULE.use_case.update.execute).toHaveBeenCalledTimes(1);
+    expect(MODULE.use_case.update.execute).toHaveBeenCalledWith(
+      UPDATE_POST_DATA,
+    );
   });
 });
