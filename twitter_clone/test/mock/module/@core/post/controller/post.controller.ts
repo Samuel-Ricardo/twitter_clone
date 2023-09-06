1ddab557d1e1a6c55ba0634128e580d1ad9fa2b5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 10:52:32 -0300

    [:clown_face:] | simulate: controller - post (mock/module)

--- twitter_clone/test/mock/module/@core/post/controller/post.controller.ts
+++ twitter_clone/test/mock/module/@core/post/controller/post.controller.ts
@@ -1,4 +1,15 @@
 import { PostController } from '@/app/modules/@core/post';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
+import { POST_REGISTRY_MOCK } from '../post.registry';
+import { ISimulatedPostController } from '@test/@types/simulate/post';
 
 export const mockPostController = () => mockDeep<PostController>();
+
+export const simulatePostController = ({
+  container,
+}: interfaces.Context): ISimulatedPostController => {
+  const service = container.get<any>(POST_REGISTRY_MOCK.SERVICE.MOCK);
+
+  return { service, controller: new PostController(service) };
+};
