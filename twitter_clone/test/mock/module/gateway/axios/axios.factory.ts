57cd54808da937add104276ccce80e81a9389323
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 11:13:42 -0300

    [:clown_face:] | update: factory - gateway > axios (mock/module)

--- twitter_clone/test/mock/module/gateway/axios/axios.factory.ts
+++ twitter_clone/test/mock/module/gateway/axios/axios.factory.ts
@@ -4,8 +4,20 @@ import { AxiosUserGateway } from '@/app/modules/gateway/axios/user/user.gateway'
 import { AXIOS_GATEWAY_REGISTRY_MOCK } from './axios.registry';
 import { ISimulatedUserGateweay } from '@test/@types/simulate/user/gateway';
 import axios from 'axios';
+import { AxiosPostGateway } from '@/app/modules/gateway/axios/post/post.gateway';
+import { ISimulatedPostGateway } from '@test/@types/simulate/post/gateway';
 
 export const AXIOS_GATEWAY_FACTORY_MOCK = {
+  POST: {
+    MOCK: () =>
+      AXIOS_GATEWAY_MODULE_MOCK.get<DeepMockProxy<AxiosPostGateway>>(
+        AXIOS_GATEWAY_REGISTRY_MOCK.POST.MOCK,
+      ),
+    SIMULATE: () =>
+      AXIOS_GATEWAY_MODULE_MOCK.get<
+        ISimulatedPostGateway<AxiosPostGateway, typeof axios>
+      >(AXIOS_GATEWAY_REGISTRY_MOCK.POST.SIMULATE),
+  },
   USER: {
     MOCK: () =>
       AXIOS_GATEWAY_MODULE_MOCK.get<DeepMockProxy<AxiosUserGateway>>(
