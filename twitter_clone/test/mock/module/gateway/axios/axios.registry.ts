38166cb1700695fc19d8be341e38d9e85ce14d88
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 11:10:53 -0300

    [:clown_face:] | update: registry - gateway > axios (mock/module)

--- twitter_clone/test/mock/module/gateway/axios/axios.registry.ts
+++ twitter_clone/test/mock/module/gateway/axios/axios.registry.ts
@@ -1,4 +1,8 @@
 export const AXIOS_GATEWAY_REGISTRY_MOCK = {
+  POST: {
+    MOCK: Symbol.for('mock:module.gateway.axios.post'),
+    SIMULATE: Symbol.for('mock:module.gateway.axios.post.simulated'),
+  },
   USER: {
     MOCK: Symbol.for('mock:module.gateway.axios.user'),
     SIMULATE: Symbol.for('mock:module.gateway.axios.user.simulated'),
