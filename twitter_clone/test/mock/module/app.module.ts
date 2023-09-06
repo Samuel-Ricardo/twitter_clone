2b162b4b31552bb9a20797a38fadb7345dbe161a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-06 10:57:16 -0300

    [:clown_face:] | update: module - app (mock/module)

--- twitter_clone/test/mock/module/app.module.ts
+++ twitter_clone/test/mock/module/app.module.ts
@@ -5,12 +5,14 @@ import { USER_MODULE_MOCK } from './@core/user/user.module';
 import { AXIOS_MODULE_MOCK } from './axios/axios.module';
 import { GATEWAY_MODULE_MOCK } from './gateway/gateway.module';
 import { HOOKS_MODULE_MOCK } from './hooks/hooks.module';
+import { POST_MODULE_MOCK } from './@core/post/post.module';
 
 const MODULE = new Container();
 
 export const APP_MODULE_MOCK = Container.merge(
   MODULE,
   USER_MODULE_MOCK,
+  POST_MODULE_MOCK,
   AXIOS_MODULE_MOCK,
   GATEWAY_MODULE_MOCK,
   HOOKS_MODULE_MOCK,
