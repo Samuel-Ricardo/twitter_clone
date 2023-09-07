f96358b9d24496a84edc916099f9a7f9e2fa4ebe
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 14:48:22 -0300

    [:clown_face:] | create: mock - data > comment (test)

---
+++ twitter_clone/test/mock/data/comment.ts
@@ -0,0 +1,34 @@
+import { VALID_USER } from './user';
+import { VALID_POST } from './post';
+import { randomID } from '../../util/mongo';
+import {
+  ICommentDTO,
+  ICreateCommentDTO,
+  IUpdateCommentDTO,
+} from '@/app/modules/@core/comment/DTO';
+import { Comment } from '@/app/modules/@core/comment/entity';
+
+export const CREATE_POST_COMMENT_DATA: ICreateCommentDTO = {
+  body: 'Hello World Comment! :D',
+  postId: VALID_POST.id,
+  authorId: VALID_USER.id,
+};
+
+export const VALID_POST_COMMENT_DATA: ICommentDTO = {
+  ...CREATE_POST_COMMENT_DATA,
+  id: randomID(),
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const VALID_POST_COMMENT = Comment.create(VALID_POST_COMMENT_DATA);
+
+export const UPDATE_POST_COMMENT_DATA: IUpdateCommentDTO = {
+  id: VALID_POST_COMMENT.id,
+  body: 'UPDATED Hello World Comment! :D',
+};
+
+const UPDATE_POST_COMMENT = Comment.create(VALID_POST_COMMENT_DATA);
+UPDATE_POST_COMMENT.body = UPDATE_POST_COMMENT_DATA.body;
+
+export { UPDATE_POST_COMMENT };
