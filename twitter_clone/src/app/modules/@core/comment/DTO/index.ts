554e73072869d4a6c13e3480f6ee91f6394322dd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 08:58:04 -0300

    [:sparkles:] | update: index - DTO > comment (module)

--- twitter_clone/src/app/modules/@core/comment/DTO/index.ts
+++ twitter_clone/src/app/modules/@core/comment/DTO/index.ts
@@ -1,1 +1,6 @@
 export * from './comment.dto';
+export * from './create.dto';
+export * from './update.dto';
+export * from './delete.dto';
+export * from './get_by_post.dto';
+export * from './get_by_author.dto';
