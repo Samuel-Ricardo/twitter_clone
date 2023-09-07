57b60b4178e9d33e7cae15340ee7a1b5623b26f4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 10:12:40 -0300

    [:sparkles:] | create: access - gateway > comment (module)

---
+++ twitter_clone/src/app/modules/@core/comment/gateway/gateway.access.ts
@@ -0,0 +1,11 @@
+import { inject, injectable } from 'inversify';
+import { type ICommentGateway } from './comment.gateway';
+import { MODULE } from '@/app/modules';
+
+@injectable()
+export abstract class CommentGatewayAccess {
+  constructor(
+    @inject(MODULE.GATEWAY.AXIOS.COMMENT)
+    protected readonly gateway: ICommentGateway,
+  ) {}
+}
