cca86742775d16c0a089f13850f501af98e7dd2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 15:04:17 -0300

    [:clown_face:] | mock: service - comment (mock/module)

---
+++ twitter_clone/test/mock/module/@core/comment/service/comment.service.ts
@@ -0,0 +1,4 @@
+import { CommentService } from '@/app/modules/@core/comment/service/comment.service';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockCommentService = mockDeep<CommentService>();
