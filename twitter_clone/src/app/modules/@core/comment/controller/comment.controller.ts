1fb0982bb967bb8bea09a54a428c96bea492e67b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 11:06:21 -0300

    [:sparkles:] | create: controller - update > comment (module)

--- twitter_clone/src/app/modules/@core/comment/controller/comment.controller.ts
+++ twitter_clone/src/app/modules/@core/comment/controller/comment.controller.ts
@@ -1,7 +1,11 @@
 import { MODULE } from '@/app/modules/app.registry';
 import { inject, injectable } from 'inversify';
 import { CommentService } from '../service/comment.service';
-import { ICreateCommentDTO } from '../DTO';
+import {
+  ICreateCommentDTO,
+  IDeleteCommentDTO,
+  IUpdateCommentDTO,
+} from '../DTO';
 
 @injectable()
 export class CommentController {
@@ -12,5 +16,13 @@ export class CommentController {
 
   async create(comment: ICreateCommentDTO) {
     return { comment: (await this.service.create(comment)).toStruct() };
+  }
+
+  async update(comment: IUpdateCommentDTO) {
+    return { comment: (await this.service.update(comment)).toStruct() };
+  }
+
+  async delete(comment: IDeleteCommentDTO) {
+    return this.service.delete(comment);
   }
 }
