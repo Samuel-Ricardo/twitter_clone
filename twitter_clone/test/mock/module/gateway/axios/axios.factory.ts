3ae3510b2f7077d716e31f64028fa2b4cff21998
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 14:59:10 -0300

    [:clown_face:] | update: factory - gateway > axios (mock/module)

--- twitter_clone/test/mock/module/gateway/axios/axios.factory.ts
+++ twitter_clone/test/mock/module/gateway/axios/axios.factory.ts
@@ -6,8 +6,20 @@ import { ISimulatedUserGateweay } from '@test/@types/simulate/user/gateway';
 import axios from 'axios';
 import { AxiosPostGateway } from '@/app/modules/gateway/axios/post/post.gateway';
 import { ISimulatedPostGateway } from '@test/@types/simulate/post/gateway';
+import { AxiosCommentGateway } from '@/app/modules/gateway/axios/comment/comment.gateway';
+import { ISimulatedCommentGateway } from '@test/@types/simulate/comment/gateway';
 
 export const AXIOS_GATEWAY_FACTORY_MOCK = {
+  COMMENT: {
+    MOCK: () =>
+      AXIOS_GATEWAY_MODULE_MOCK.get<DeepMockProxy<AxiosCommentGateway>>(
+        AXIOS_GATEWAY_REGISTRY_MOCK.COMMENT.MOCK,
+      ),
+    SIMULATE: () =>
+      AXIOS_GATEWAY_MODULE_MOCK.get<
+        ISimulatedCommentGateway<AxiosCommentGateway, typeof axios>
+      >(AXIOS_GATEWAY_REGISTRY_MOCK.COMMENT.SIMULATE),
+  },
   POST: {
     MOCK: () =>
       AXIOS_GATEWAY_MODULE_MOCK.get<DeepMockProxy<AxiosPostGateway>>(
