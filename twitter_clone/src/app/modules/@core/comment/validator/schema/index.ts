5a3a7d5e7464f870b0e82feb1347126008e247b5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 08:58:58 -0300

    [:safety_vest:] | create: index - schema > comments (module)

--- twitter_clone/src/app/modules/@core/comment/validator/schema/index.ts
+++ twitter_clone/src/app/modules/@core/comment/validator/schema/index.ts
@@ -1,1 +1,6 @@
 export * from './comment.schema';
+export * from './create.schema';
+export * from './update.schema';
+export * from './delete.schema';
+export * from './get_by_post.schema';
+export * from './get_by_author.schema';
