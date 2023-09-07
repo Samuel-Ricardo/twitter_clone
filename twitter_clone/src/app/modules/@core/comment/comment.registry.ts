cb68d55c4d1517724a136fa85572fbef49c9371e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 10:57:18 -0300

    [:sparkles:] | updaate: registry - comment (module)

--- twitter_clone/src/app/modules/@core/comment/comment.registry.ts
+++ twitter_clone/src/app/modules/@core/comment/comment.registry.ts
@@ -1,15 +1,17 @@
 export const COMMENT_REGISTRY = {
+  SERVICE: Symbol.for('MODULE:COMMENT.SERVICE'),
   USE_CASE: {
     CREATE: Symbol.for('MODULE:COMMENT.USE_CASE.CREATE'),
     DELETE: Symbol.for('MODULE:COMMENT.USE_CASE.DELETE'),
     UPDATE: Symbol.for('MODULE:COMMENT.USE_CASE.UPDATE'),
     FIND: {
-      ALL: Symbol.for('MODULE:COMMENT.USE_CASE.FIND.ALL'),
       BY: {
         AUTHOR: {
           ID: Symbol.for('MODULE:COMMENT.USE_CASE.FIND.BY.AUTHOR.ID'),
         },
-        ID: Symbol.for('MODULE:COMMENT.USE_CASE.FIND.BY.ID'),
+        POST: {
+          ID: Symbol.for('MODULE:COMMENT.USE_CASE.FIND.BY.POST.ID'),
+        },
       },
     },
   },
