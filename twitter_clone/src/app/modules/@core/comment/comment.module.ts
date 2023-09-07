35a50156f957cdebb2e11232172550ecaed7f606
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 10:34:41 -0300

    [:sparkles:] | create: module - comment (module)

---
+++ twitter_clone/src/app/modules/@core/comment/comment.module.ts
@@ -0,0 +1,25 @@
+import { Container } from 'inversify';
+import { GatewayModule } from '../../gateway/gateway.module';
+import {
+  CreateCommentUseCase,
+  DeleteCommentUseCase,
+  UpdateCommentUseCase,
+  FindPostCommentsUseCase,
+  FindUserCommentsUseCase,
+} from './use-case';
+import { COMMENT_REGISTRY } from './comment.registry';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const COMMENT_MODULE = Container.merge(MODULE, GatewayModule);
+
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.CREATE).to(CreateCommentUseCase);
+
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.DELETE).to(DeleteCommentUseCase);
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.UPDATE).to(UpdateCommentUseCase);
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.FIND.BY.POST.ID).to(
+  FindPostCommentsUseCase,
+);
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.FIND.BY.AUTHOR.ID).to(
+  FindUserCommentsUseCase,
+);
