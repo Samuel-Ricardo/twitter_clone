60f0a0933f64b8a9c8b2bbdae279dee96c596a01
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 15:32:05 -0300

    [:clown_face:] | update: registry - app (mock/module)

--- twitter_clone/test/mock/module/app.registry.ts
+++ twitter_clone/test/mock/module/app.registry.ts
@@ -3,10 +3,12 @@ import { USER_REGISTRY_MOCK as USER } from './@core/user/user.registry';
 import { AXIOS_REGISTRY_MOCK as AXIOS } from './axios/axios.registry';
 import { GATEWAY_REGISTRY_MOCK as GATEWAY } from './gateway/gateway.registry';
 import { HOOKS_REGISTRY_MOCK as HOOKS } from './hooks/hooks.registry';
+import { COMMENT_REGISTRY_MOCK as COMMENT } from './@core/comment/comment.registry';
 
 export const MODULE_MOCK = {
   USER,
   POST,
+  COMMENT,
   AXIOS,
   GATEWAY,
   HOOKS,
