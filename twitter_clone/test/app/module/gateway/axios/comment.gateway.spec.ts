265a32cea5fac8a4546a8d622cb02fb6a4d86e06
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 16:55:33 -0300

    [:white_check_mark:] | create: [unit] test - find by [author] > comment (module/gateway)

--- twitter_clone/test/app/module/gateway/axios/comment.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/comment.gateway.spec.ts
@@ -15,6 +15,7 @@ import {
   UPDATE_POST_COMMENT_DATA,
 } from '@test/mock/data/comment';
 import { VALID_POST } from '@test/mock/data/post';
+import { VALID_USER } from '@test/mock/data/user';
 
 describe('[GATEWAY] | Axios => [COMMENT]', () => {
   let MODULE = MODULES_MOCK.GATEWAY.AXIOS.COMMENT.SIMULATE();
@@ -87,6 +88,25 @@ describe('[GATEWAY] | Axios => [COMMENT]', () => {
     expect(MODULE.client.get).toBeCalledTimes(1);
     expect(MODULE.client.get).toBeCalledWith(
       `${MODULE.gateway.fullURL}/post/${VALID_POST.id}`,
+      undefined,
+    );
+  });
+
+  it('[UNIT] | Should: find by [USER] => [COMMENT]', async () => {
+    MODULE.client.get.mockResolvedValue({
+      data: { comments: [VALID_POST_COMMENT.toStruct()] },
+    });
+
+    const result = await MODULE.gateway.findByauthor({
+      authorId: VALID_USER.id,
+    });
+
+    expect(result).toStrictEqual([VALID_POST_COMMENT]);
+    expect(result[0].authorId).toBe(VALID_USER.id);
+
+    expect(MODULE.client.get).toBeCalledTimes(1);
+    expect(MODULE.client.get).toBeCalledWith(
+      `${MODULE.gateway.fullURL}/author/${VALID_USER.id}`,
       undefined,
     );
   });
