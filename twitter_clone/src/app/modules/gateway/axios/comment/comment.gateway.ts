aed1a0ef4bc3ebdbc2262e65d1024bb8f55042fc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-07 09:35:32 -0300

    [:sparkles:] | create: gateway - create > comment (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/comment/comment.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/comment/comment.gateway.ts
@@ -1,13 +1,26 @@
 import { ICommentGateway } from '@/app/modules/@core/comment/gateway/comment.gateway';
 import { AxiosHTTPGateway } from '../generic/http.gateway';
-import { ICreateCommentDTO } from '@/app/modules/@core/comment/DTO';
+import {
+  ICommentDTO,
+  ICreateCommentDTO,
+} from '@/app/modules/@core/comment/DTO';
+import { injectable } from 'inversify';
+import { Comment } from '@/app/modules/@core/comment/entity';
 
+@injectable()
 export class AxiosCommentGateway
   extends AxiosHTTPGateway
   implements ICommentGateway
 {
-  create(comment: ICreateCommentDTO) {
-    throw new Error('Method not implemented.');
+  readonly prefix = 'comments';
+
+  async create(comment: ICreateCommentDTO) {
+    const result = await this.post<{ comment: ICommentDTO }>(
+      this.prefix,
+      comment,
+    );
+
+    return Comment.create(result.data.comment);
   }
   update(comment: IUpdateCommentDTO): Promise<Comment> {
     throw new Error('Method not implemented.');
