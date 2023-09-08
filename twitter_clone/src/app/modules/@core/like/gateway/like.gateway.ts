4771e3d4c32a05e36fd4794ef29435737cefd83e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 11:53:46 -0300

    [:electric_plug:] | create: gateway - like (module)

---
+++ twitter_clone/src/app/modules/@core/like/gateway/like.gateway.ts
@@ -0,0 +1,16 @@
+import {
+  ICreateLikeDTO,
+  IFindCommentLikesDTO,
+  IFindPostLikesDTO,
+  IDeleteLikeDTO,
+  IFindUserLikesDTO,
+} from '../DTO';
+import { Like } from '../entity';
+
+export interface ICommentGateway {
+  create(like: ICreateLikeDTO): Promise<Like>;
+  deleteLike(like: IDeleteLikeDTO): Promise<Like>;
+  findCommentLikes(like: IFindCommentLikesDTO): Promise<Like[]>;
+  findPostLikes(like: IFindPostLikesDTO): Promise<Like[]>;
+  findUserLikes(like: IFindUserLikesDTO): Promise<Like[]>;
+}
