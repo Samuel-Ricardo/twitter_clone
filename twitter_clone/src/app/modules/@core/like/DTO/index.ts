5c97bd01fa8e8e278789c0a64f61fe30fe23e93c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 11:39:55 -0300

    [:sparkles:] | create: index - DTO > Like (module)

---
+++ twitter_clone/src/app/modules/@core/like/DTO/index.ts
@@ -0,0 +1,6 @@
+export * from './like.dto';
+export * from './create.dto';
+export * from './delete.dto';
+export * from './find_by_comment.dto';
+export * from './find_by_post.dto';
+export * from './find_by_user.dto';
