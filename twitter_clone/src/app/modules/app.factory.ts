e2e1070a91eca8b3992ea8c58e7a87e714c66ec7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 16:03:42 -0300

    [:sparkles:] | update: factory - app (module)

--- twitter_clone/src/app/modules/app.factory.ts
+++ twitter_clone/src/app/modules/app.factory.ts
@@ -4,12 +4,14 @@ import { UserFactory as USER } from './@core/user/user.factory';
 import { ConfigFactory as CONFIG } from './config/config.factory';
 import { POST_FACTORY as POST } from './@core/post/post.factory';
 import { COMMENT_FACTORY as COMMENT } from './@core/comment/comment.factory';
+import { LIKE_FACTORY as LIKE } from './@core/like/like.factory';
 
 export const MODULES = {
   ...AxiosFactory,
   USER,
   POST,
   COMMENT,
+  LIKE,
   CONFIG,
   GATEWAY,
 };
