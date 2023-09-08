1a8ae0f8a52c9d0e2e5d946f3b508f151ebd5ad4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 12:15:13 -0300

    [:electric_plug:] | create: gateway - create > like (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/like/like.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/like/like.gateway.ts
@@ -1,10 +1,23 @@
 import { ILikeGateway } from '@/app/modules/@core/like/gateway/like.gateway';
 import { AxiosHTTPGateway } from '../generic/http.gateway';
+import {
+  ICreateLikeDTO,
+  IDeleteLikeDTO,
+  IFindPostLikesDTO,
+  ILikeDTO,
+} from '@/app/modules/@core/like/DTO';
+import { Like } from '../../../@core/like/entity';
 
 export class AxiosLikeGateway extends AxiosHTTPGateway implements ILikeGateway {
   readonly prefix = 'likes';
 
   get fullURL() {
     return `${this.URL}/${this.prefix}`;
+  }
+
+  async create(like: ICreateLikeDTO) {
+    const result = await this.post<{ like: ILikeDTO }>(this.fullURL, like);
+
+    return Like.create(result.data.like);
   }
 }
