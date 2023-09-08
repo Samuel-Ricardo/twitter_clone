b4132869cada3cb10de752b0fd5a503916cec020
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 15:50:23 -0300

    [:sparkles:] | create: registry - like (module)

---
+++ twitter_clone/src/app/modules/@core/like/like.registry.ts
@@ -0,0 +1,13 @@
+export const LIKE_REGISTRY = {
+  USE_CASE: {
+    CREATE: Symbol.for('LIKE_USE_CASE_CREATE'),
+    DELETE: Symbol.for('LIKE_USE_CASE_DELETE'),
+    FIND: {
+      BY: {
+        POST: Symbol.for('LIKE_USE_CASE_FIND_BY_POST'),
+        COMMENT: Symbol.for('LIKE_USE_CASE_FIND_BY_COMMENT'),
+        USER: Symbol.for('LIKE_USE_CASE_FIND_BY_USER'),
+      },
+    },
+  },
+};
