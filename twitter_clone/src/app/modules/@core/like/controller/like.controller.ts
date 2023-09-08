4116ebc67052606f0212ea6d7b92b6709533714d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 16:43:09 -0300

    [:sparkles:] | create: controller - deleete > like (module)

--- twitter_clone/src/app/modules/@core/like/controller/like.controller.ts
+++ twitter_clone/src/app/modules/@core/like/controller/like.controller.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { LikeService } from '../service';
 import { MODULE } from '@/app/modules/app.registry';
-import { ICreateLikeDTO } from '../DTO';
+import { ICreateLikeDTO, IDeleteLikeDTO, IFindPostLikesDTO } from '../DTO';
 
 @injectable()
 export class LikeController {
@@ -12,5 +12,13 @@ export class LikeController {
 
   async create(like: ICreateLikeDTO) {
     return { like: (await this.service.create(like)).toStruct() };
+  }
+
+  async dislike(like: IDeleteLikeDTO) {
+    return await this.service.delete(like);
+  }
+
+  getTweetLikes(tweet: IFindPostLikesDTO) {
+    return { like: this.service.findByPost(tweet) };
   }
 }
