05e2b0b4f56d1057afa1a9f5ad626738b20603eb
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 15:24:05 -0300

    [:sparkles:] | create: access - gateway > like (module)

---
+++ twitter_clone/src/app/modules/@core/like/gateway/gateway.access.ts
@@ -0,0 +1,11 @@
+import { inject, injectable } from 'inversify';
+import { type ILikeGateway } from './like.gateway';
+import { MODULE } from '@/app/modules';
+
+@injectable()
+export abstract class LikeGatewayAccess {
+  constructor(
+    @inject(MODULE.GATEWAY.AXIOS.LIKE)
+    private readonly gateway: ILikeGateway,
+  ) {}
+}
