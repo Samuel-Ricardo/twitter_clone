05954a478dd6ae3358013bc158a05c39e98c6566
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 09:21:45 -0300

    [:white_check_mark:] | create: [unit] test - create > comment (module/service)

--- twitter_clone/test/app/module/core/comments/comment.service.spec.ts
+++ twitter_clone/test/app/module/core/comments/comment.service.spec.ts
@@ -7,6 +7,10 @@ import 'reflect-metadata';
 import { expect } from '@jest/globals';
 import { ISimulatedCommentService } from '@test/@types/simulate/comment/service';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
+import {
+  VALID_POST_COMMENT,
+  VALID_POST_COMMENT_DATA,
+} from '@test/mock/data/comment';
 
 describe('[CORE] | SERVICE =:> [COMMENT]', () => {
   let MODULE: ISimulatedCommentService;
@@ -22,6 +26,16 @@ describe('[CORE] | SERVICE =:> [COMMENT]', () => {
   });
 
   it('[UNIT] | Should: create => [COMMENT]', async () => {
-    expect(true).toBe(true);
+    MODULE.use_case.create.execute.mockResolvedValue(VALID_POST_COMMENT);
+
+    const result = await MODULE.service.create(VALID_POST_COMMENT_DATA);
+
+    expect(result).toBeDefined();
+    expect(result).toStrictEqual(VALID_POST_COMMENT);
+
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledTimes(1);
+    expect(MODULE.use_case.create.execute).toHaveBeenCalledWith(
+      VALID_POST_COMMENT_DATA,
+    );
   });
 });
