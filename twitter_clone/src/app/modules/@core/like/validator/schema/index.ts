3dd17d94ccaeeb0f61d5b91a626f7738fbd608d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-08 11:38:33 -0300

    [:safety_vest:] | create: index - schema > like (module)

---
+++ twitter_clone/src/app/modules/@core/like/validator/schema/index.ts
@@ -0,0 +1,6 @@
+export * from './like.schema';
+export * from './create.schema';
+export * from './delete.schema';
+export * from './find_by_comment.schema';
+export * from './find_by_post.schema';
+export * from './find_by_user.schema';
