ff55d98e33872052dfc40724efd8511835d45d19
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-10 11:40:53 -0300

    [:clown_face:] | simulate: controller - like (mock/module)

---
+++ twitter_clone/test/mock/module/@core/like/controller/like.controller.ts
@@ -0,0 +1,4 @@
+import { LikeController } from '@/app/modules/@core/like/controller';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockLikeController = () => mockDeep<LikeController>();
