56a2988a6fc818cdd831770f929dfeaaa1b09350
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-10 12:59:10 -0300

    [:white_check_mark:] | create: [unit] test - create > like (module/gateway)

--- twitter_clone/test/app/module/gateway/axios/like.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/like.gateway.spec.ts
@@ -7,6 +7,11 @@ import 'reflect-metadata';
 import { expect } from '@jest/globals';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { AxiosLikeGateway } from '@/app/modules/gateway/axios/like/like.gateway';
+import {
+  CREATE_POST_LIKE_DATA,
+  VALID_POST_LIKE,
+  VALID_POST_LIKE_DATA,
+} from '@test/mock/data/like';
 
 describe('[GATEWAY] | Axios => [LIKE]', () => {
   let MODULE = MODULES_MOCK.GATEWAY.AXIOS.LIKE.SIMULATE();
@@ -21,6 +26,21 @@ describe('[GATEWAY] | Axios => [LIKE]', () => {
   });
 
   it('[UNIT] | Should: create => [LIKE]', async () => {
-    expect(true).toBe(true);
+    MODULE.client.post.mockResolvedValue({
+      data: {
+        like: VALID_POST_LIKE.toStruct(),
+      },
+    });
+
+    const result = await MODULE.gateway.create(CREATE_POST_LIKE_DATA);
+
+    expect(result).toStrictEqual(VALID_POST_LIKE);
+
+    expect(MODULE.client.post).toBeCalledTimes(1);
+    expect(MODULE.client.post).toBeCalledWith(
+      MODULE.gateway.fullURL,
+      CREATE_POST_LIKE_DATA,
+      undefined,
+    );
   });
 });
