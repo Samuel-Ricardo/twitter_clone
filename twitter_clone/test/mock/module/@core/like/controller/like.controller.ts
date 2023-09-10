30c148faeb2db5eacea2d0c93690ffb25afbd907
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-10 12:20:03 -0300

    [:clown_face:] | simulate: controller - like (mock/module)

--- twitter_clone/test/mock/module/@core/like/controller/like.controller.ts
+++ twitter_clone/test/mock/module/@core/like/controller/like.controller.ts
@@ -1,4 +1,16 @@
 import { LikeController } from '@/app/modules/@core/like/controller';
+import { ISimulatedLikeController } from '@test/@types/simulate/like/controller';
+import { MODULE_MOCK } from '@test/mock/module/app.registry';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
 
 export const mockLikeController = () => mockDeep<LikeController>();
+
+export const simulateLikeController = ({
+  container,
+}: interfaces.Context): ISimulatedLikeController => {
+  const service = container.get<any>(MODULE_MOCK.LIKE.SERVICE.MOCK);
+  const controller = new LikeController(service);
+
+  return { controller, service };
+};
