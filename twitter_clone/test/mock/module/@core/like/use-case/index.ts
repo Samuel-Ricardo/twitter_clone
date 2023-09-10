de2b07ab9640235653f326fb103eea2cb33e1639
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-10 10:47:37 -0300

    [:clown_face:] | create: index - use-case > like (mock/module)

---
+++ twitter_clone/test/mock/module/@core/like/use-case/index.ts
@@ -0,0 +1,5 @@
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './find_by_comment.use-case';
+export * from './find_by_post.use-case';
+export * from './find_by_user.use-case';
