0b93f490916518aa68ef5f7ce21313bfc2c34c04
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 01:50:20 -0300

    [:electric_plug:] | update: factory - gateway > axios (module)

--- twitter_clone/src/app/modules/gateway/axios/axios.factory.ts
+++ twitter_clone/src/app/modules/gateway/axios/axios.factory.ts
@@ -1,6 +1,7 @@
 import { AxiosGatewayModule } from './axios.module';
 import { AxiosGatewayRegistry } from './axios.registry';
 import { AxiosCommentGateway } from './comment/comment.gateway';
+import { AxiosFollowGateway } from './follow/follow.gateway';
 import { AxiosHTTPGateway } from './generic/http.gateway';
 import { AxiosLikeGateway } from './like/like.gateway';
 import { AxiosPostGateway } from './post/post.gateway';
@@ -21,4 +22,6 @@ export const AxiosGatewayFactory = {
     AxiosGatewayModule.get<AxiosCommentGateway>(AxiosGatewayRegistry.COMMENT),
   LIKE: () =>
     AxiosGatewayModule.get<AxiosLikeGateway>(AxiosGatewayRegistry.LIKE),
+  FOLLOW: () =>
+    AxiosGatewayModule.get<AxiosFollowGateway>(AxiosGatewayRegistry.FOLLOW),
 };
