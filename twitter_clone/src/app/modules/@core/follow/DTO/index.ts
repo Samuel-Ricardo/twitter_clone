19443dc8a2ffb1fdbe82ba915dcb00c07e351306
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 01:11:11 -0300

    [:sparkles:] | create: DTO - index > following (module)

--- twitter_clone/src/app/modules/@core/follow/DTO/index.ts
+++ twitter_clone/src/app/modules/@core/follow/DTO/index.ts
@@ -1,1 +1,7 @@
 export * from './follow.dto';
+export * from './count_followers.dto';
+export * from './count_following.dto';
+export * from './get_followers.dto';
+export * from './get_following.dto';
+export * from './delete.dto';
+export * from './create.dto';
