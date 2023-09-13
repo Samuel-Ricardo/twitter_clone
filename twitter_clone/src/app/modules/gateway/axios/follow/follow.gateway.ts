97c9f41283a75d03895d531726a2213ac37b896c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 01:32:26 -0300

    [:electric_plug:] | create: gateway - create > follow (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/follow/follow.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/follow/follow.gateway.ts
@@ -1,6 +1,8 @@
 import { injectable } from 'inversify';
 import { AxiosHTTPGateway } from '../generic/http.gateway';
 import { IFollowGateway } from '@/app/modules/@core/follow/gateway/follow.gateway';
+import { ICreateFollowDTO, IFollowDTO } from '@/app/modules/@core/follow/DTO';
+import { Follow } from '@/app/modules/@core/follow/entity';
 
 @injectable()
 export class AxiosFollowGateway
@@ -11,5 +13,14 @@ export class AxiosFollowGateway
 
   get fullURL(): string {
     return `${this.fullURL}/${this.prefix}`;
+  }
+
+  async create(follow: ICreateFollowDTO) {
+    const result = await this.post<{ follow: IFollowDTO }>(
+      this.fullURL,
+      follow,
+    );
+
+    return Follow.create(result.data.follow);
   }
 }
