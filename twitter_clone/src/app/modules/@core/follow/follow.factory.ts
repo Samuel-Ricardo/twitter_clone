26c25c6c38e1b750109ee61362171e148619abc5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-15 10:50:44 -0300

    [:sparkles:] | create: factory - follow (module)

---
+++ twitter_clone/src/app/modules/@core/follow/follow.factory.ts
@@ -0,0 +1,37 @@
+import { FOLLOW_MODULE } from './follow.module';
+import { FOLLOW_REGISTRY } from './follow.registry';
+import {
+  CountFollowersUseCase,
+  CountFollowingUseCase,
+  CreateFollowUseCase,
+  DeleteFollowUseCase,
+  GetFollowersUseCase,
+  GetFollowingUseCase,
+} from './use-case';
+
+export const FOLLOW_FACTORY = {
+  USE_CASE: {
+    CREATE: FOLLOW_MODULE.get<CreateFollowUseCase>(
+      FOLLOW_REGISTRY.USE_CASE.CREATE,
+    ),
+    DELETE: FOLLOW_MODULE.get<DeleteFollowUseCase>(
+      FOLLOW_REGISTRY.USE_CASE.DELETE,
+    ),
+    GET: {
+      FOLLOWERS: FOLLOW_MODULE.get<GetFollowersUseCase>(
+        FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWERS,
+      ),
+      FOLLOWING: FOLLOW_MODULE.get<GetFollowingUseCase>(
+        FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWING,
+      ),
+    },
+    COUNT: {
+      FOLLOWERS: FOLLOW_MODULE.get<CountFollowersUseCase>(
+        FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWERS,
+      ),
+      FOLLOWING: FOLLOW_MODULE.get<CountFollowingUseCase>(
+        FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWING,
+      ),
+    },
+  },
+};
