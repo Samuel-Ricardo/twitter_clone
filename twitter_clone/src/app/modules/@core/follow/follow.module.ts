3b58373ee7df0cf90ca6db4995dae32c0dcbe21b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-15 10:46:38 -0300

    [:sparkles:] | create: module - follow (module)

---
+++ twitter_clone/src/app/modules/@core/follow/follow.module.ts
@@ -0,0 +1,30 @@
+import { Container } from 'inversify';
+import { GatewayModule } from '../../gateway/gateway.module';
+import { FOLLOW_REGISTRY } from './follow.registry';
+import {
+  CreateFollowUseCase,
+  DeleteFollowUseCase,
+  GetFollowersUseCase,
+  GetFollowingUseCase,
+  CountFollowersUseCase,
+  CountFollowingUseCase,
+} from './use-case';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const FOLLOW_MODULE = Container.merge(MODULE, GatewayModule);
+
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.CREATE).to(CreateFollowUseCase);
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.DELETE).to(DeleteFollowUseCase);
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWERS).to(
+  GetFollowersUseCase,
+);
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWING).to(
+  GetFollowingUseCase,
+);
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWING).to(
+  CountFollowingUseCase,
+);
+FOLLOW_MODULE.bind(FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWERS).to(
+  CountFollowersUseCase,
+);
