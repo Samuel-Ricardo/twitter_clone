b7e6903398a7a4cc83970f2b43b8a63201db639e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 14:43:18 -0300

    [:clown_face:] | simulate: gateway - axios > follow (mock/module)

--- twitter_clone/test/mock/module/gateway/axios/follow/follow.gateway.ts
+++ twitter_clone/test/mock/module/gateway/axios/follow/follow.gateway.ts
@@ -1,4 +1,22 @@
 import { AxiosFollowGateway } from '@/app/modules/gateway/axios/follow/follow.gateway';
+import { ISimulatedFollowGateway } from '@test/@types/simulate/follow/gateway';
+import { MODULE_MOCK } from '@test/mock/module/app.registry';
+import axios from 'axios';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
 
 export const mockAxiosFollowGateway = () => mockDeep<AxiosFollowGateway>();
+
+export const simulateAxiosFollowGateway = ({
+  container,
+}: interfaces.Context): ISimulatedFollowGateway<
+  AxiosFollowGateway,
+  typeof axios
+> => {
+  const client = container.get<any>(MODULE_MOCK.AXIOS.MOCK);
+
+  return {
+    gateway: new AxiosFollowGateway('http://localhost:3004', client),
+    client,
+  };
+};
