ce36c2afadc3ea697bf6483fe22c71ae4735fff7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 14:53:35 -0300

    [:white_check_mark:] | create: [unit] test - create > follow (module/gateway)

--- twitter_clone/test/app/module/gateway/axios/follow.gateway.ts
+++ twitter_clone/test/app/module/gateway/axios/follow.gateway.ts
@@ -7,6 +7,7 @@ import 'reflect-metadata';
 import { expect } from '@jest/globals';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { AxiosFollowGateway } from '@/app/modules/gateway/axios/follow/follow.gateway';
+import { CREATE_FOLLOW_DATA, VALID_FOLLOW } from '@test/mock/data/follow';
 
 describe('[GATEWAY] | Axios => [LIKE]', () => {
   let MODULE = MODULES_MOCK.GATEWAY.AXIOS.FOLLOW.SIMULATE();
@@ -21,6 +22,20 @@ describe('[GATEWAY] | Axios => [LIKE]', () => {
   });
 
   it('[UNIT] | Should: create => [LIKE]', async () => {
-    expect(true).toBe(true);
+    MODULE.client.post.mockResolvedValue({
+      data: {
+        follow: VALID_FOLLOW.toStruct(),
+      },
+    });
+
+    const result = await MODULE.gateway.create(CREATE_FOLLOW_DATA);
+
+    expect(result).toStrictEqual(VALID_FOLLOW);
+    expect(MODULE.client.post).toBeCalledTimes(1);
+    expect(MODULE.client.post).toBeCalledWith(
+      MODULE.gateway.fullURL,
+      VALID_FOLLOW,
+      undefined,
+    );
   });
 });
