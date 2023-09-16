35b73981030343fb881c67cca2534dda99200ac7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 09:29:34 -0300

    [:clown_face:] | mock: index - use-case > followers (mock/module)

---
+++ twitter_clone/test/app/module/core/follow/use-case/index.ts
@@ -0,0 +1,6 @@
+export * from './create.use-case';
+export * from './delete.use-case';
+export * from './get_followers.use-case';
+export * from './get_following.use-case';
+export * from './count_followers.use-case';
+export * from './count_following.use-case';
