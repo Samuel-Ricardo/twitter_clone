c01b3468cfda4e53523be775517d18d224677295
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 13:14:43 -0300

    [:clown_face:] | update: registry - app (mock/module)

--- twitter_clone/test/mock/module/app.registry.ts
+++ twitter_clone/test/mock/module/app.registry.ts
@@ -5,12 +5,14 @@ import { GATEWAY_REGISTRY_MOCK as GATEWAY } from './gateway/gateway.registry';
 import { HOOKS_REGISTRY_MOCK as HOOKS } from './hooks/hooks.registry';
 import { COMMENT_REGISTRY_MOCK as COMMENT } from './@core/comment/comment.registry';
 import { LIKE_REGISTRY_MOCK as LIKE } from './@core/like/like.registry';
+import { FOLLOW_REGISTRY_MOCK as FOLLOW } from './@core/follow/follow.registry';
 
 export const MODULE_MOCK = {
   USER,
   POST,
   COMMENT,
   LIKE,
+  FOLLOW,
   AXIOS,
   GATEWAY,
   HOOKS,
