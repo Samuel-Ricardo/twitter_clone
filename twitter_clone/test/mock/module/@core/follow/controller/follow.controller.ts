5a2206760a42bb3ef83df2009f4dd18d709ee037
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 14:29:21 -0300

    [:clown_face:] | simulate: controller - follow (mock/module)

--- twitter_clone/test/mock/module/@core/follow/controller/follow.controller.ts
+++ twitter_clone/test/mock/module/@core/follow/controller/follow.controller.ts
@@ -1,4 +1,15 @@
 import { FollowController } from '@/app/modules/@core/follow/controller';
+import { ISimulatedFollowController } from '@test/@types/simulate/follow/controller';
+import { MODULE_MOCK } from '@test/mock/module/app.registry';
+import { interfaces } from 'inversify';
 import { mockDeep } from 'jest-mock-extended';
 
 export const mockFollowController = () => mockDeep<FollowController>();
+
+export const simulateFollowController = ({
+  container,
+}: interfaces.Context): ISimulatedFollowController => {
+  const service = container.get<any>(MODULE_MOCK.FOLLOW.SERVICE.MOCK);
+
+  return { controller: new FollowController(service), service };
+};
