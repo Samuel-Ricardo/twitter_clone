f30969c919befc8e71ae86d4bda5cc640aa9aad6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 14:22:23 -0300

    [:clown_face:] | mock: controller - follow (mock/module)

---
+++ twitter_clone/test/mock/module/@core/follow/controller/follow.controller.ts
@@ -0,0 +1,4 @@
+import { FollowController } from '@/app/modules/@core/follow/controller';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockFollowController = () => mockDeep<FollowController>();
