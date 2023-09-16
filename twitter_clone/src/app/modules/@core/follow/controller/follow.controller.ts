02dd2c1507b330a476aa6de6fdb0280853bb895a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-16 09:10:44 -0300

    [:sparkles:] | create: controller - create > follow (module)

--- twitter_clone/src/app/modules/@core/follow/controller/follow.controller.ts
+++ twitter_clone/src/app/modules/@core/follow/controller/follow.controller.ts
@@ -1,6 +1,7 @@
 import { MODULE } from '@/app/modules/app.registry';
 import { inject, injectable } from 'inversify';
 import { FollowService } from '../service';
+import { ICreateFollowDTO } from '../DTO';
 
 @injectable()
 export class FollowController {
@@ -8,4 +9,9 @@ export class FollowController {
     @inject(MODULE.FOLLOW.SERVICE)
     private readonly service: FollowService,
   ) {}
+
+  async follow(user: ICreateFollowDTO) {
+    const result = await this.service.create(user);
+    return { user: result.toStruct() };
+  }
 }
