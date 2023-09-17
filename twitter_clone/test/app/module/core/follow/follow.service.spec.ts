781974f45f8f915ce637c448d6a5b900165564de
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-17 17:18:02 -0300

    [:white_check_mark:] | create: [unit] test - get > followers (module/service)

--- twitter_clone/test/app/module/core/follow/follow.service.spec.ts
+++ twitter_clone/test/app/module/core/follow/follow.service.spec.ts
@@ -8,7 +8,12 @@ import { expect } from '@jest/globals';
 import { ISimulatedFollowService } from '@test/@types/simulate/follow/service';
 import { MODULES_MOCK } from '@test/mock/module/app.factory';
 import { FollowService } from '@/app/modules/@core/follow/service';
-import { CREATE_FOLLOW_DATA, VALID_FOLLOW } from '@test/mock/data/follow';
+import {
+  CREATE_FOLLOW_DATA,
+  SWR_FOLLOW,
+  USER_FOLLOWED,
+  VALID_FOLLOW,
+} from '@test/mock/data/follow';
 
 describe('[CORE] | SERVICE => [FOLLOW]', () => {
   let MODULE: ISimulatedFollowService;
@@ -44,6 +49,23 @@ describe('[CORE] | SERVICE => [FOLLOW]', () => {
     expect(MODULE.use_case.deleteFollow.execute).toHaveBeenCalledTimes(1);
     expect(MODULE.use_case.deleteFollow.execute).toHaveBeenCalledWith({
       id: VALID_FOLLOW.id,
+    });
+  });
+
+  it('[UNIT] | Should: get [followers] => [FOLLOW]', async () => {
+    MODULE.use_case.get.followers.execute.mockReturnValue(
+      SWR_FOLLOW([VALID_FOLLOW.toStruct()]) as any,
+    );
+
+    const result = MODULE.service.getFollowersOf({
+      followingId: USER_FOLLOWED.id,
+    });
+
+    expect(result.data).toEqual([VALID_FOLLOW.toStruct()]);
+
+    expect(MODULE.use_case.get.followers.execute).toHaveBeenCalledTimes(1);
+    expect(MODULE.use_case.get.followers.execute).toHaveBeenCalledWith({
+      followingId: USER_FOLLOWED.id,
     });
   });
 });
