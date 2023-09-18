4efe7aee38a1839cee092dd4f9136d01589c2908
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-18 16:41:13 -0300

    [:sparkles:] | create: DTO - type > notification (module)

---
+++ twitter_clone/src/app/modules/@core/notification/DTO/type.dto.ts
@@ -0,0 +1,11 @@
+export enum NotificationType {
+  FOLLOW = 'FOLLOW',
+  UNFOLLOW = 'UNFOLLOW',
+  LIKE = 'LIKE',
+  DISLIKE = 'DISLIKE',
+  COMMENT = 'COMMENT',
+  POST = 'POST',
+}
+
+export const { FOLLOW, UNFOLLOW, LIKE, DISLIKE, COMMENT, POST } =
+  NotificationType;
