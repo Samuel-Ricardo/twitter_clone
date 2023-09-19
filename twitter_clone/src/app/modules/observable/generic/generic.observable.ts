ea099d7aa1b5e3a3efcdc24313124f24e17cbfb8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-19 15:47:25 -0300

    [:mag_right:] | create: observable - node > listen (module)

--- twitter_clone/src/app/modules/observable/generic/generic.observable.ts
+++ twitter_clone/src/app/modules/observable/generic/generic.observable.ts
@@ -1,5 +1,5 @@
 export interface IObservable {
-  listen(event: string): any | void;
+  listen(event: string, action: (...data: any) => any): any | void;
   stopListening(event: string): any | void;
   emit(event: string, data: any): any | void;
 }
--- twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
+++ twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'inversify';
 import { IObservable } from '../../generic/generic.observable';
 import { MODULE } from '@/app/modules/app.registry';
+import { GenericAction } from '@/app/@types/event/action/generic';
 
 @injectable()
 export class NodeObservable implements IObservable {
@@ -8,8 +9,8 @@ export class NodeObservable implements IObservable {
     @inject(MODULE.EVENT.EMITER[2])
     protected readonly event: EventEmitter2,
   ) {}
-  listen(event: string) {
-    throw new Error('Method not implemented.');
+  listen(event: string, action: GenericAction) {
+    this.event.on(event, action);
   }
   stopListening(event: string) {
     throw new Error('Method not implemented.');
