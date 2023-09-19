03203e78ed1cdc08c9479d596f4575effa05dc3b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-19 16:44:55 -0300

    [:mag_right:] | create: observable - node > getter (module)

--- twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
+++ twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
@@ -7,15 +7,20 @@ import { GenericAction } from '@/app/@types/event/action/generic';
 export class NodeObservable implements IObservable {
   constructor(
     @inject(MODULE.EVENT.EMITER[2])
-    protected readonly event: EventEmitter2,
+    protected readonly _events: EventEmitter2,
   ) {}
+
   listen(event: string, action: GenericAction) {
-    this.event.on(event, action);
+    this._events.on(event, action);
   }
   stopListening(event: string, action: GenericAction) {
-    this.event.off(event, action);
+    this._events.off(event, action);
   }
   emit(event: string, data: any) {
-    this.event.emit(event, data);
+    this._events.emit(event, data);
+  }
+
+  get events() {
+    return this._events;
   }
 }
