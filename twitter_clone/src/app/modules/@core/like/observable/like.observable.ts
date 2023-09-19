0f6b281f52656f8813a17f012c4ba2e6cd28aac8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-19 16:50:04 -0300

    [:mag_right:] | update: observable - like (module)

--- twitter_clone/src/app/modules/@core/like/observable/like.observable.ts
+++ twitter_clone/src/app/modules/@core/like/observable/like.observable.ts
@@ -4,8 +4,15 @@ import { IListenDislikeDTO } from '../DTO/observable/listen/dislike.dto';
 import { IListenLikeDTO } from '../DTO/observable/listen/like.dto';
 
 export interface ILikeObservable {
-  listenLike(schedule: IListenLikeDTO): any | Promise<any>;
-  listenDislike(schedule: IListenDislikeDTO): any | Promise<any>;
-  emitLike(like: IEmitLikeDTO): any | Promise<any>;
-  emitDislike(like: IEmitDislikeDTO): any | Promise<any>;
+  listenPostLike(schedule: IListenLikeDTO): any | Promise<any>;
+  listenCommentLike(schedule: IListenLikeDTO): any | Promise<any>;
+
+  listenPostDislike(schedule: IListenDislikeDTO): any | Promise<any>;
+  listenCommentDislike(schedule: IListenDislikeDTO): any | Promise<any>;
+
+  emitPostLike(like: IEmitLikeDTO): any | Promise<any>;
+  emitPostDislike(like: IEmitDislikeDTO): any | Promise<any>;
+
+  emitCommentLike(like: IEmitLikeDTO): any | Promise<any>;
+  emitCommentDislike(like: IEmitDislikeDTO): any | Promise<any>;
 }
