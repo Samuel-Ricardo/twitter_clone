196e1ad978341650f71968e9fb8f879680dd5f34
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-20 19:14:53 -0300

    [:sparkles:] | update: ModalContentContainer (component)

--- twitter_clone/src/app/component/modal/generic/container.component.tsx
+++ twitter_clone/src/app/component/modal/generic/container.component.tsx
@@ -1,20 +1,25 @@
-export const ModalContainer = (props: { children: JSX.Element }) => (
+export const ModalContainer = (props: { children: React.ReactNode }) => (
   <div
     className="
         justify-center 
         items-center 
         flex 
+        flex-col
 
         overflow-x-hidden 
         overflow-y-auto
       
-        fixed
-        inset-0
+        fixed 
         z-50
       
         outline-none
         focus:outline-none
-      
+              
+        rounded-md
+    
+        pb-6
+        px-6
+    
         bg-neutral-800 
         bgroup-opacity-70 
       "
