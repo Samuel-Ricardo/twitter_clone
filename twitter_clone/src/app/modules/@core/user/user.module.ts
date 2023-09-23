57503ec8e13859b57243cd0b870772a1eb275cd8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-22 22:48:33 -0300

    [:sparkles:] | update: module - user (module)

--- twitter_clone/src/app/modules/@core/user/user.module.ts
+++ twitter_clone/src/app/modules/@core/user/user.module.ts
@@ -10,6 +10,7 @@ import {
 } from './use-case';
 import { UserService } from './service';
 import { UserController } from './controller';
+import { SelectUserByCredentialsUseCase } from './use-case/select_by_credentials.use-case';
 
 const Modue = new Container({ autoBindInjectable: true });
 
@@ -20,6 +21,9 @@ UserModule.bind(UserRegistry.USE_CASE.UPDATE).to(UpdateUserUseCase);
 UserModule.bind(UserRegistry.USE_CASE.DELETE).to(DeleteUserUseCase);
 UserModule.bind(UserRegistry.USE_CASE.GET.ALL).to(ListAllUsersUseCase);
 UserModule.bind(UserRegistry.USE_CASE.GET.BY.ID).to(SelectUserByIdUseCase);
+UserModule.bind(UserRegistry.USE_CASE.GET.BY.CREDENTIALS).to(
+  SelectUserByCredentialsUseCase,
+);
 
 UserModule.bind(UserRegistry.SERVICE).to(UserService);
 UserModule.bind(UserRegistry.CONTROLLER).to(UserController);
