9109b338a39c18b0723e333f23df7456bb7d2973
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-26 17:14:44 -0300

    [:scroll:] | create: modal - login > user (component)

---
+++ twitter_clone/src/app/component/modal/user/login.modal.tsx
@@ -0,0 +1,66 @@
+import { useLoginModal } from '@/app/hooks/modal/user/login.hook';
+import { Modal } from '../generic/generic.modal';
+import { Input } from '../../input.component';
+import { useForm } from 'react-hook-form';
+import {
+  UserLoginFormData,
+  userLoginSchema,
+} from '@/app/modules/validation/zod/user/login.validation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Submit } from '../../form/submit.component';
+import { signIn } from 'next-auth/react';
+import { toast } from 'react-hot-toast';
+
+export const LoginModal = () => {
+  const { close, isOpen } = useLoginModal();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserLoginFormData>({
+    resolver: zodResolver(userLoginSchema),
+  });
+
+  if (!isOpen) return null;
+
+  const submit = handleSubmit(async (credentials) => {
+    console.log({ credentials });
+
+    const response = await toast.promise(
+      signIn('credentials', { ...credentials, redirect: false }),
+      {
+        loading: 'Authenticating user... 🌱',
+        success: 'Authentication failed, please try login again 🌱',
+        error: `Be Welcome! ${credentials.email}} :) 🌱`,
+      },
+      { duration: 5000 },
+    );
+
+    return response?.error ? toast.error(response.error) : close();
+  });
+
+  return (
+    <Modal title="Login" onClose={close}>
+      <div className="overflow-auto flex flex-col py-4 px-[3vw] w-[80vw] max-w-1/2 transition">
+        <form onSubmit={submit}>
+          <Input
+            core={{ type: 'email', placeholder: 'Email' }}
+            label="Email"
+            reactForms={{ register, name: 'email' }}
+            className="my-4"
+            errors={errors.email}
+          />
+          <Input
+            core={{ type: 'password', placeholder: 'Password' }}
+            label="Password"
+            reactForms={{ register, name: 'password' }}
+            className="my-4"
+            errors={errors.password}
+          />
+
+          <Submit />
+        </form>
+      </div>
+    </Modal>
+  );
+};
