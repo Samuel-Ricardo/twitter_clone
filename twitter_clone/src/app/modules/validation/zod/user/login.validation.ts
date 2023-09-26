89858a52bd42f8426e9b9448757819d6bfc5f6d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-26 15:17:08 -0300

    [:safety_vest:] | create: validation - login > user (module)

---
+++ twitter_clone/src/app/modules/validation/zod/user/login.validation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const userLoginSchema = z.object({
+  email: z
+    .string()
+    .nonempty("Email can't be empty")
+    .email('Please enter a valid email')
+    .toLowerCase(),
+  password: z.string().nonempty("Password can't be empty").min(6),
+});
+
+export type UserLoginFormData = z.infer<typeof userLoginSchema>;
