a54657c130a565cba85dd22a88137303f98710dd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-27 09:28:44 -0300

    [:safety_vest:] | create: validation - edit > user (module)

---
+++ twitter_clone/src/app/modules/validation/zod/user/edit.validation.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+import { maxFileSize } from './rules/file/size.rule';
+import { toBase64 } from './transformers/file/toBase64.transformer';
+import { UppercaseFirstLetters } from './transformers/name.transformer';
+
+export const userEditSchema = z.object({
+  profileImage: z
+    .instanceof(File, { message: 'Avatar must be an image file' })
+    .nullish()
+    .refine(maxFileSize(5 * 1024 * 1024), 'File size must be less than 5MB')
+    .transform(toBase64),
+  coverImage: z
+    .instanceof(File, { message: 'Cover image must be an image file' })
+    .nullish()
+    .refine(maxFileSize(5 * 1024 * 1024), 'File size must be less than 5MB')
+    .transform(toBase64),
+  name: z
+    .string()
+    .nonempty("Name can't be empty")
+    .transform(UppercaseFirstLetters),
+  username: z.string().nonempty("Username can't be empty"),
+  bio: z.string().nullish(),
+});
+
+export type UserEditFormData = z.infer<typeof userEditSchema>;
