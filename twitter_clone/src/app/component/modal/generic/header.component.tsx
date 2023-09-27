1022914b11ef5de2f28908e3f943a0a3485a8c5f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-27 10:38:31 -0300

    [:lipstick:] | update: header - modal (style)

--- twitter_clone/src/app/component/modal/generic/header.component.tsx
+++ twitter_clone/src/app/component/modal/generic/header.component.tsx
@@ -1,15 +1,7 @@
-import { AiOutlineClose } from 'react-icons/ai';
-
 export const ModalHeader = (props: { title: string; onClose: () => void }) => {
   return (
-    <div className="flex items-center justify-between p-10 rounded-t">
+    <div className="flex items-center justify-between rounded py-3 px-5 mx-4 my-2 bg-gradient-to-br from-blue-500 to-purple-500">
       <h3 className="text-3xl font-semibold text-white">{props.title}</h3>
-      <button
-        className=" text-white p-1 ml-auto border-0 hover:opacity-60 transition"
-        onClick={props.onClose}
-      >
-        <AiOutlineClose />
-      </button>
     </div>
   );
 };
