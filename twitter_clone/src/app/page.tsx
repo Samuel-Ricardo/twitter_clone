87fa73d1247473c4a5af42edb36ace11e8baf808
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 11:49:03 -0300

    [:book:] | update: page - home (app)

--- twitter_clone/src/app/page.tsx
+++ twitter_clone/src/app/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-
 import { HomePage } from './page/home.page';
-import { BaseLayout } from './page/layout/base.layout';
 
 export default function Home() {
-  return (
-    <BaseLayout>
-      <HomePage />
-    </BaseLayout>
-  );
+  return <HomePage />;
 }
