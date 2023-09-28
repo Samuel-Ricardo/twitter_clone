0bb1afb123d7b3e789a21e739887873082465c2a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 09:54:59 -0300

    [:electric_plug:] | update: support - swr > gateway (module/user)

--- twitter_clone/src/app/modules/@core/user/gateway/support/swr.support.ts
+++ twitter_clone/src/app/modules/@core/user/gateway/support/swr.support.ts
@@ -3,7 +3,9 @@ import { ISelectUserByIdDTO, IUserDTO } from '../../DTO';
 import { ISelectUserByEmailDTO } from '../../DTO/select_by_email.dto';
 
 export interface ISWRSupport {
-  swrListAll(): SWRResponse<IUserDTO[]>;
-  swrSelectById(props: ISelectUserByIdDTO): SWRResponse<IUserDTO>;
-  swrSelectByEmail(props: ISelectUserByEmailDTO): SWRResponse<IUserDTO>;
+  swrListAll(): SWRResponse<{ users: IUserDTO[] }>;
+  swrSelectById(props: ISelectUserByIdDTO): SWRResponse<{ user: IUserDTO }>;
+  swrSelectByEmail(
+    props: ISelectUserByEmailDTO,
+  ): SWRResponse<{ user: IUserDTO }>;
 }
