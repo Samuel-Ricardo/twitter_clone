45b4d4c7572b0f3482d813de7218233d66df3183
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 11:48:47 -0300

    [:triangular_ruler:] | update: layout - root (app)

--- twitter_clone/src/app/layout.tsx
+++ twitter_clone/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { BaseLayout } from './page/layout/base.layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <BaseLayout>{children}</BaseLayout>
+      </body>
     </html>
   );
 }
