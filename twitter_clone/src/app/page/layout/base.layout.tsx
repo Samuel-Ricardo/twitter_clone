08f2c89425139013197c678fc2398d453b40db65
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 11:45:49 -0300

    [:triangular_ruler:] | create: layout - base (app)

--- twitter_clone/src/app/page/layout/base.layout.tsx
+++ twitter_clone/src/app/page/layout/base.layout.tsx
@@ -1,3 +1,25 @@
+'use client';
+
+import { EditUserModal } from '@/app/component/modal/user/edit.modal';
+import { LoginModal } from '@/app/component/modal/user/login.modal';
+import { RegisterModal } from '@/app/component/modal/user/register.modal';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SessionProvider } from 'next-auth/react';
+import { Toaster } from 'react-hot-toast';
+
 export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <SessionProvider>
+        <div className="flex h-[100vh] w-[100vw]">
+          <Toaster position="bottom-center" />
+          <LoginModal />
+          <RegisterModal />
+          <EditUserModal />
+
+          {children}
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
+  );
 };
