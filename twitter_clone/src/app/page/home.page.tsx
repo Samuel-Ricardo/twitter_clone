ada826421637e6f2c722a1ff5c5e9b2dbdd4fe8c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 11:48:27 -0300

    [:book:] | create: page - home (app)

---
+++ twitter_clone/src/app/page/home.page.tsx
@@ -0,0 +1,9 @@
+import { Header } from '../component/header.component';
+import { PostFeed } from '../component/post/feed.component';
+
+export const HomePage = () => (
+  <>
+    <Header label="Home" />
+    <PostFeed />
+  </>
+);
