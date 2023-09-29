fef252a3bbbdceea4a24e768ca734f5d5e35729e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-29 10:17:05 -0300

    [:sparkles:] | create: component - container (post/item)

---
+++ twitter_clone/src/app/component/post/item/container.component.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+export const PostItemContainer = ({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick?: () => void;
+}) => (
+  <div
+    onClick={onClick}
+    className="border-b-[1px] border-neutral-800 border-opacity-90 p-5 cursor-pointer hover:bg-neutral-900 hover:scale-110 transition-all duration-300 ease-in-out"
+  >
+    <div className="flex flex-row items-start gap-3">{children}</div>
+  </div>
+);
