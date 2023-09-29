68c39f7e839b7ed7b9dc6e346bf40bb1adcb08e3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-29 18:19:02 -0300

    [:sparkles:] | update: component - item (post)

--- twitter_clone/src/app/component/post/item.component.tsx
+++ twitter_clone/src/app/component/post/item.component.tsx
@@ -3,7 +3,6 @@ import { useHasLiked } from '@/app/hooks/like/has_liked.hook';
 import { useTweetLikes } from '@/app/hooks/like/post.hook';
 import { useLike } from '@/app/hooks/like/post/mutate/create.hook';
 import { useDislike } from '@/app/hooks/like/post/mutate/delete.hook';
-import { useCurrentUser } from '@/app/hooks/user/current.hook';
 import { useRouter } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
 import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
@@ -13,6 +12,7 @@ import Image from 'next/image';
 import { useUser } from '@/app/hooks/user/one.hook';
 import { PostItemContainer } from './item/container.component';
 import { PostItemHeader } from './item/header.component';
+import { PostItemFooter } from './item/footer.component';
 
 export const PostItem = ({
   post,
@@ -20,21 +20,10 @@ export const PostItem = ({
   onPostClick,
   onLikeClick,
 }: IPostItemProps) => {
-  const { data: fetchLikes, mutate: syncLikes } = useTweetLikes({
-    likedId: post.id,
-  });
-  const likes = useMemo(() => fetchLikes, [fetchLikes]);
-  const { giveLike } = useLike();
-  const { dislike } = useDislike();
   const { data } = useUser({ id: post.authorId });
   const author = useMemo(() => data?.user, [data]);
 
-  const { hasLiked } = useHasLiked({
-    likedId: post.id,
-    userId: currentUser?.id,
-  });
-
-  console.log({ post, author, currentUser, likes });
+  console.log({ post, author, currentUser });
 
   const router = useRouter();
 
@@ -49,27 +38,6 @@ export const PostItem = ({
     [router, post.id, onPostClick],
   );
 
-  const toggleLike = useCallback(() => {
-    if (!currentUser) return;
-
-    hasLiked()
-      ? dislike({ id: post.id })
-      : giveLike({ userId: currentUser.id, likedId: post.id });
-
-    syncLikes();
-  }, [hasLiked, dislike, giveLike, currentUser, post.id, syncLikes]);
-
-  const handleLike = useCallback(
-    (event: React.MouseEvent<any>) => {
-      event.stopPropagation();
-
-      if (onLikeClick) onLikeClick(event);
-
-      toggleLike();
-    },
-    [onLikeClick, toggleLike],
-  );
-
   const createdAt = useMemo(
     () =>
       post?.createdAt && formatDistanceToNowStrict(new Date(post?.createdAt)),
@@ -78,24 +46,40 @@ export const PostItem = ({
 
   return (
     <PostItemContainer onClick={goToPost}>
-      <Avatar userId={author?.id} image={author?.profileImage} />
-      <div>
-        <PostItemHeader user={author} createdAt={createdAt} />
-        <div>{post?.body}</div>
-        {post?.image && <Image src={post?.image} alt="post image" />}
-        <div>
-          <AiOutlineMessage size={20} />
-          <p>comments count</p>
+      <div className="flex flex-row items-start gap-3">
+        <div className="min-w-fit">
+          <Avatar userId={author?.id} image={author?.profileImage} />
         </div>
-        <div onClick={handleLike}>
-          {hasLiked() ? (
-            <AiFillHeart size={20} color="red" />
-          ) : (
-            <AiOutlineHeart size={20} />
-          )}
-          <p>{likes?.length}</p>
+        <div>
+          <PostItemHeader user={author} createdAt={createdAt} />
+
+          <div className="my-4 mx-2 bg-[rgba(255,255,255,0.35)] p-2 rounded-md overflow-clip break-all hidden md:block ">
+            {post?.body +
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+          </div>
         </div>
       </div>
+
+      <div className="my-4 mx-2 bg-[rgba(255,255,255,0.35)] p-2 rounded-md overflow-clip break-all md:hidden ">
+        {post?.body +
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+      </div>
+
+      {post.image ||
+        (true && (
+          <Image
+            src={currentUser?.coverImage || '/user/images/placeholder.png'}
+            alt="post image"
+            width={1080}
+            height={1920}
+            className=" mx-5 w-auto max-w-full h-full max-h-[50vh]  rounded-md"
+          />
+        ))}
+      <PostItemFooter
+        onLikeClick={onLikeClick}
+        postId={post.id}
+        currentUserId={currentUser?.id}
+      />
     </PostItemContainer>
   );
 };
