22a214d2fe4eabfb0c65132dfb150959b2dba71c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 23:05:49 -0300

    [:sparkles:] | update: component - TweeterButton (sidebar)

--- twitter_clone/src/app/component/section/sidebar/button/tweet.component.tsx
+++ twitter_clone/src/app/component/section/sidebar/button/tweet.component.tsx
@@ -1,18 +1,20 @@
+'use client';
+
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 import { FaFeather } from 'react-icons/fa';
 
-export const SidebarTweetButton = () => {
+export const SidebarTweetButton = ({ className }: { className?: string }) => {
   const { push: navigateTo } = useRouter();
 
   const handleClick = useCallback(() => navigateTo('/'), [navigateTo]);
 
   return (
-    <div onClick={handleClick}>
-      <div className="flex mt-6 bg-sky-500 hover:bg-opacity-80 transition lg:hidden rounded-full h-14 w-14 p-4 items-center justify-center cursor-pointer">
+    <div onClick={handleClick} className={className}>
+      <div className="flex mt-6 bg-sky-500 hover:bg-opacity-60 hover:scale-110 transition-all duration-300 ease-in-out lg:hidden rounded-full h-14 w-14 p-4 items-center justify-center cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
-      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-80 cursor-pointer">
+      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-70 cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out ">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
           Tweet
         </p>
