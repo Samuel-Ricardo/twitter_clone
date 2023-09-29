33f46f077f3de6ff24d96d8d5896bf63fd12edf5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 22:59:35 -0300

    [:sparkles:] | update: component - sidebar > item

--- twitter_clone/src/app/component/section/sidebar/item.component.tsx
+++ twitter_clone/src/app/component/section/sidebar/item.component.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ISidebarItemProps } from '@/app/@types/props/section/sidebar/item';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
@@ -5,7 +7,7 @@ import { BsDot } from 'react-icons/bs';
 
 export const SideBarItem = ({
   onClick,
-  Icon,
+  icon: Icon,
   label,
   href,
   alert,
@@ -13,21 +15,25 @@ export const SideBarItem = ({
   const router = useRouter();
 
   const handleClick = useCallback(() => {
-    onClick && onClick;
+    onClick && onClick();
     href && router.push(href);
   }, [onClick, href, router]);
 
   return (
     <div onClick={handleClick} className="flex flex-row items-center">
-      <div className=" relative rounded-full h-14 w-14 flex items-center justify-center p-4 cursor-pointer lg:hidden ">
-        <Icon size={28} color="white" className="hover:text-cyan" />
+      <div className=" relative rounded-full h-14 w-14 flex items-center justify-center p-4 cursor-pointer lg:hidden hover:bg-blue-300 hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out">
+        <Icon
+          size={28}
+          color="white"
+          className="hover:fill-black transition "
+        />
         {alert && (
           <BsDot className="absolute -top-4 left-0" size={70} color="red" />
         )}
       </div>
-      <div className=" relative hidden lg:flex items-row gap-4 p-4 rounded-full cursor-pointer items-center ">
-        <Icon size={24} color="white" className="hover:text-cyan" />
-        <p>{label}</p>
+      <div className=" relative hidden lg:flex items-row gap-4 p-4 rounded-full cursor-pointer items-center hover:bg-blue-300 hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out">
+        <Icon size={24} color="white" className="hover:fill-black transition" />
+        <p className="hidden lg:block">{label}</p>
         {alert && (
           <BsDot className="absolute -top-4 left-0" size={70} color="red" />
         )}
