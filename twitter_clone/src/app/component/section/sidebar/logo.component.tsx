38e26e77b0d33fb82057ad6ed32b88f67aee59d7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-28 23:07:16 -0300

    [:sparkles:] | update: component - logo > tweeter (sidebar)

--- twitter_clone/src/app/component/section/sidebar/logo.component.tsx
+++ twitter_clone/src/app/component/section/sidebar/logo.component.tsx
@@ -1,13 +1,18 @@
-import { useRouter } from 'next/router';
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { BsTwitter } from 'react-icons/bs';
 
-export const SidebarLogo = () => {
+export const SidebarLogo = ({ className }: { className?: string }) => {
   const { push } = useRouter();
 
   return (
     <div
       onClick={() => push('/')}
-      className="rounded-full h-14 w-14 p-4 flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out hover:bg-blue-300"
+      className={
+        'rounded-full h-14 w-14 p-4 flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out hover:bg-blue-300 ' +
+        className
+      }
     >
       <BsTwitter size={30} color="white" />
     </div>
