020ea7864ad5a8ee9b78014d4c657994bd903408
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-30 09:45:27 -0300

    [:link:] | update: hook - find by [tweet] > post (module)

--- twitter_clone/src/app/hooks/post/one.hook.ts
+++ twitter_clone/src/app/hooks/post/one.hook.ts
@@ -1,5 +1,9 @@
 import { MODULES } from '@/app/modules';
 import { IFindPostByIdDTO } from '@/app/modules/@core/post';
+import { useMemo } from 'react';
 
-export const usePost = (post: IFindPostByIdDTO) =>
-  MODULES.POST.MAIN().findById(post).post;
+export const usePost = (data: IFindPostByIdDTO) => {
+  const result = MODULES.POST.MAIN().findById(data).post;
+  const post = useMemo(() => result?.data?.post, [result]);
+  return { ...result, post };
+};
