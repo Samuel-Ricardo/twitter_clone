f727f9444c8f47c3d0cf25770c3e9951076f92be
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-30 09:15:08 -0300

    [:link:] | update: hook - like > tweets (fetch)

--- twitter_clone/src/app/hooks/like/post.hook.ts
+++ twitter_clone/src/app/hooks/like/post.hook.ts
@@ -1,5 +1,9 @@
 import { MODULES } from '@/app/modules';
 import { IFindPostLikesDTO } from '@/app/modules/@core/like/DTO';
+import { useMemo } from 'react';
 
-export const useTweetLikes = (tweet: IFindPostLikesDTO) =>
-  MODULES.LIKE.MAIN().getTweetLikes(tweet).likes;
+export const useTweetLikes = (tweet: IFindPostLikesDTO) => {
+  const result = MODULES.LIKE.MAIN().getTweetLikes(tweet).likes;
+  const likes = useMemo(() => result?.data?.likes || [], [result]);
+  return { ...result, likes };
+};
