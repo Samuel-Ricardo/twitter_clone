b200210a561cb90128fbdf67f67e41952fe77fc6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-30 09:19:16 -0300

    [:link:] | update: hook - find by [tweet] > comment (module)

--- twitter_clone/src/app/hooks/comment/post.hook.ts
+++ twitter_clone/src/app/hooks/comment/post.hook.ts
@@ -1,5 +1,9 @@
 import { MODULES } from '@/app/modules';
 import { IFindPostCommentsDTO } from '@/app/modules/@core/comment/DTO';
+import { useMemo } from 'react';
 
-export const usePostComment = (post: IFindPostCommentsDTO) =>
-  MODULES.COMMENT.MAIN().findPostComments(post).comments;
+export const useTweetComments = (post: IFindPostCommentsDTO) => {
+  const result = MODULES.COMMENT.MAIN().findPostComments(post).comments;
+  const comments = useMemo(() => result?.data?.comments || [], [result]);
+  return { ...result, comments };
+};
