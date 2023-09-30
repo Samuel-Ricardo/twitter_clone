faac26c6ab6583e7a4685c2a0761adac782285f4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-30 09:25:00 -0300

    [:label:] | update: props - footer > item (@types/post)

--- twitter_clone/src/app/@types/props/post/item_footer.ts
+++ twitter_clone/src/app/@types/props/post/item_footer.ts
@@ -1,5 +1,6 @@
 export interface IPostItemFooterProps {
   onLikeClick?: (event: React.MouseEvent<any>) => void | any;
+  onCommentClick?: (event: React.MouseEvent<any>) => void | any;
   postId: string;
   currentUserId?: string;
 }
