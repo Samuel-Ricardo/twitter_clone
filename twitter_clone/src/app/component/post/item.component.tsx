9fd7b5aa4349d146b7c6c2f38455b85f94498065
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-02 15:45:04 -0300

    [:sparkles:] | update: component - item (post)

--- twitter_clone/src/app/component/post/item.component.tsx
+++ twitter_clone/src/app/component/post/item.component.tsx
@@ -11,13 +11,12 @@ import { PostItemContent } from './item/content.component';
 export const PostItem = ({
   post,
   currentUser,
+  fitScreen,
   onPostClick,
   onLikeClick,
 }: IPostItemProps) => {
-  const { data } = useUser({ id: post.authorId });
+  const { data } = useUser({ id: post?.authorId });
   const author = useMemo(() => data?.user, [data]);
-
-  console.log({ post, author, currentUser });
 
   const router = useRouter();
 
@@ -27,9 +26,9 @@ export const PostItem = ({
 
       if (onPostClick) onPostClick(event);
 
-      router.push(`/post/${post.id}`);
+      router.push(`/post/${post?.id}`);
     },
-    [router, post.id, onPostClick],
+    [router, post?.id, onPostClick],
   );
 
   const createdAt = useMemo(
@@ -40,14 +39,22 @@ export const PostItem = ({
 
   return (
     <PostItemContainer onClick={goToPost}>
-      <PostItemHeader user={author} createdAt={createdAt} body={post.body} />
+      <PostItemHeader
+        user={author}
+        createdAt={createdAt || ''}
+        body={post?.body || ''}
+      />
 
-      <PostItemContent body={post.body} image={currentUser?.coverImage} />
+      <PostItemContent
+        body={post?.body}
+        image={currentUser?.coverImage}
+        fitScreen={fitScreen}
+      />
 
       <PostItemFooter
         onLikeClick={onLikeClick}
         onCommentClick={goToPost}
-        postId={post.id}
+        postId={post?.id || ''}
         currentUserId={currentUser?.id}
       />
     </PostItemContainer>
