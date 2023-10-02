c4bdb26ef46efd96c91651c37267e86f82d732aa
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-02 15:42:13 -0300

    [:sparkles:] | update: component - TextArea (form/input)

--- twitter_clone/src/app/component/form/input/textarea.component.tsx
+++ twitter_clone/src/app/component/form/input/textarea.component.tsx
@@ -4,6 +4,7 @@ export const TextArea = (props: ITextAreaProps) => {
   return (
     <textarea
       {...props}
+      {...props.hook?.register()}
       className={` flex rounded-md text-black my-4 mx-2 bg-[rgba(255,255,255,0.35)] py-2 pl-2 break-all outline-none  ${props.className}`}
     />
   );
