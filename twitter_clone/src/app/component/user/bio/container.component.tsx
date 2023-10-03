9f5af5b895864ef58ee6806e48d613f1928e32a3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-03 16:27:05 -0300

    [:sparkles:] | create: component - bio > container (user/profile)

---
+++ twitter_clone/src/app/component/user/bio/container.component.tsx
@@ -0,0 +1,5 @@
+export const BioContainer = ({ children }: { children: React.ReactNode }) => (
+  <div className="flex flex-col flex-1 -mt-2 pt-2 rounded-b-xl bg-gradient-to-br from-transparent to-gray-400/40 ">
+    {children}
+  </div>
+);
