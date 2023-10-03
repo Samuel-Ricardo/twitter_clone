5b92075dcfdb9dcb1932ada7fa4ffbf95697ac81
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-03 16:10:36 -0300

    [:book:] | create: page - user > profile (app)

---
+++ twitter_clone/src/app/page/user/profile.page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { Header } from '@/app/component/header.component';
+import { UserFeed } from '@/app/component/post/user/feed.component';
+import { ProfileBio } from '@/app/component/user/bio.component';
+import { ProfileCover } from '@/app/component/user/cover.component';
+import { useUser } from '@/app/hooks/user/one.hook';
+import { useParams } from 'next/navigation';
+
+export const ProfilePage = () => {
+  const { id } = useParams();
+  const { user } = useUser({ id: id as string });
+
+  return (
+    <div className="flex flex-col flex-1">
+      <div className="flex flex-col mb-24">
+        <Header label="Profile" />
+        <ProfileCover
+          userId={id as string}
+          cover={user?.coverImage}
+          avatar={user?.profileImage}
+        />
+      </div>
+      <ProfileBio bio={user?.bio || ''} />
+      <UserFeed userId={id as string} />
+    </div>
+  );
+};
