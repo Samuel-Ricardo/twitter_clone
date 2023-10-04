38f2c5b31020c753ae83115891442c6b94166c39
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-04 10:28:55 -0300

    [:sparkles:] | create: toggle - follow (button/user/follow)

--- twitter_clone/src/app/component/button/user/follow.button.tsx
+++ twitter_clone/src/app/component/button/user/follow.button.tsx
@@ -1,23 +1,31 @@
+import { useFollowers } from '@/app/hooks/follow/followers.hook';
 import { useToggleFollow } from '@/app/hooks/follow/mutate/toggle.hook';
-import { FiUserCheck, FiUserX } from 'react-icons/fi';
+import { useCallback } from 'react';
+import { FiUserCheck, FiUserPlus } from 'react-icons/fi';
 
 export const FollowButton = ({ userId }: { userId: string }) => {
   const { toggleAsync, alredyFollowing } = useToggleFollow({
     followingId: userId,
   });
+  const { mutate: sync } = useFollowers({ followingId: userId });
+  const toggle = useCallback(async () => {
+    await toggleAsync();
+    sync();
+  }, [toggleAsync, sync]);
+
   return (
     <div>
       {alredyFollowing() ? (
-        <FiUserX
-          size={38}
+        <FiUserCheck
+          size={36}
           className="cursor-pointer transition-all duration-500 ease-in-out hover:scale-110 hover:text-cyan-300"
-          onClick={toggleAsync}
+          onClick={toggle}
         />
       ) : (
-        <FiUserCheck
-          size={38}
+        <FiUserPlus
+          size={36}
           className="cursor-pointer transition-all duration-500 ease-in-out hover:scale-110 hover:text-cyan-300"
-          onClick={toggleAsync}
+          onClick={toggle}
         />
       )}
     </div>
