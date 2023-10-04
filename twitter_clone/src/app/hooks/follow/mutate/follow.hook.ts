fa44c5fd373f1911950637cafa29a9dc1216051b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-03 21:52:32 -0300

    [:link:] | update: hook - create > follow (mutate)

--- twitter_clone/src/app/hooks/follow/mutate/follow.hook.ts
+++ twitter_clone/src/app/hooks/follow/mutate/follow.hook.ts
@@ -1,6 +1,7 @@
 import { MODULES } from '@/app/modules';
 import { ICreateFollowDTO, IFollowDTO } from '@/app/modules/@core/follow/DTO';
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 
 export const useFollow = () => {
   const {
@@ -11,7 +12,14 @@ export const useFollow = () => {
     isLoading,
     status,
   } = useMutation<IFollowDTO, Error, ICreateFollowDTO>({
-    mutationFn: async (DTO) => (await MODULES.FOLLOW.MAIN().follow(DTO)).follow,
+    mutationFn: async (DTO) =>
+      (
+        await toast.promise(MODULES.FOLLOW.MAIN().follow(DTO), {
+          loading: `Creating follow... 💌`,
+          success: `User Followed 👌`,
+          error: `Failed on follow 👎 :(`,
+        })
+      ).follow,
     mutationKey: ['create', 'follow'],
   });
 
