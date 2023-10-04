8955ab70338ec0b7f9c66b261636e318379107c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-04 15:40:06 -0300

    [:sparkles:] | update: component - bio (user/profile)

--- twitter_clone/src/app/component/user/bio.component.tsx
+++ twitter_clone/src/app/component/user/bio.component.tsx
@@ -3,20 +3,35 @@ import { BioContainer } from './bio/container.component';
 import { EditUserButton } from '../button/user/edit.button';
 import { useCurrentUser } from '@/app/hooks/user/current.hook';
 import { FollowButton } from '../button/user/follow.button';
+import { BioContentContainer } from './bio/content.container';
+import { BioData } from './bio/data.component';
+import { BioBody } from './bio/body.component';
+import { useMemo } from 'react';
+import { format } from 'date-fns';
+import { BioFollowers } from './bio/followers.component';
 
-export const ProfileBio = (props: IProfileBIoProps) => {
+export const ProfileBio = ({ user }: IProfileBIoProps) => {
   const { currentUser } = useCurrentUser({ displayLogin: true });
+
+  const createdAt = useMemo(
+    () => (user ? format(new Date(user.createdAt), 'MMMM yyyy') : ''),
+    [user],
+  );
 
   return (
     <BioContainer>
       <div className="flex justify-end">
-        {currentUser?.id === props.userId ? (
+        {currentUser?.id === user?.id ? (
           <EditUserButton />
         ) : (
-          <FollowButton userId={props.userId} />
+          <FollowButton userId={user?.id || ''} />
         )}
       </div>
-      {props.bio}
+      <BioContentContainer>
+        <BioData user={user?.name || ''} username={user?.username || ''} />
+        <BioBody bio={user?.bio || ''} createdAt={createdAt} />
+        <BioFollowers userId={user?.id || ''} />
+      </BioContentContainer>
     </BioContainer>
   );
 };
