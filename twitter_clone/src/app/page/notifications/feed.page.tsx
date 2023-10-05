2288be4818fa5bfa1fbe4b9b7b33e1c17054e72c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-05 09:35:45 -0300

    [:book:] | create: page - notifications > feed (app)

---
+++ twitter_clone/src/app/page/notifications/feed.page.tsx
@@ -0,0 +1,10 @@
+import { Header } from '@/app/component/header.component';
+
+export const NotificationsPage = () => {
+  return (
+    <div className="flex flex-1 flex-col">
+      <Header label="Notification" />
+      NotificationsPage
+    </div>
+  );
+};
