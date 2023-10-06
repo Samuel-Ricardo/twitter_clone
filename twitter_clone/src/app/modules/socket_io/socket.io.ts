3c5df6b0b1e1f5478d4019b92b032d9994a356f1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-06 10:15:44 -0300

    [:electric_plug:] | create: instance - Socket.io (module)

---
+++ twitter_clone/src/app/modules/socket_io/socket.io.ts
@@ -0,0 +1,10 @@
+import { io, Socket } from 'socket.io-client';
+import { ENV } from '../config/env/app.env';
+
+const globalSocketIO = globalThis as unknown as {
+  socketIO: Socket | undefined;
+};
+
+export const socketIO = globalSocketIO.socketIO || io(ENV.API.SOCKET.URL);
+
+globalSocketIO.socketIO = socketIO;
