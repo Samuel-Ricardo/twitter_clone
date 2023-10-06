97f702c58cb9adc158cd6db1fedf13cc940b8420
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-06 18:09:08 -0300

    [:mag_right:] | implements: observable - notification > emitNotification  (module/node)

--- twitter_clone/src/app/modules/observable/node/notification/notification.observable.ts
+++ twitter_clone/src/app/modules/observable/node/notification/notification.observable.ts
@@ -1,6 +1,14 @@
 import { INotificationObservable } from '@/app/modules/@core/notification/observable/notification.observable';
 import { NodeObservable } from '../generic/generic.observable';
+import { IListenDislikeDTO } from '@/app/modules/@core/like/DTO/observable/listen/dislike.dto';
+import { IListenLikeDTO } from '@/app/modules/@core/like/DTO/observable/listen/like.dto';
+import { IEmitNotificationDTO } from '@/app/modules/@core/notification/DTO/observable/emit/create.dto';
 
 export class NodeNotificationObservable
   extends NodeObservable
-  implements INotificationObservable {}
+  implements INotificationObservable
+{
+  emitNotification(notification: IEmitNotificationDTO) {
+    this.emit(this.EVENTS.NOTIFICATION.CREATED, notification);
+  }
+}
