2246f733d15f4f85b5320551eb57c385ee7fc76c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-06 11:07:36 -0300

    [:gear:] | create: config - socket (module/reactive)

---
+++ twitter_clone/src/app/modules/reactive/reactive.config.ts
@@ -0,0 +1,24 @@
+export const SOCKET = {
+  ERROR: {
+    APP: 'error.app',
+  },
+
+  START: {
+    CONNECTION: 'start.connection',
+  },
+
+  CONNECT: 'connect',
+  CONNECTION: 'connection',
+  DISCONNECT: 'disconnect',
+
+  HEALTH_CHECK: 'HEALTH_CHECK',
+  HANDSHAKE: 'HANDSHAKE',
+
+  NOTIFICATION: {
+    NEW: 'new.notification',
+    CREATED: 'notification.created',
+    VISUALIZE: 'visualize.notification',
+    VISUALIZED: 'notification.visualized',
+    TOGGLE: 'notification.toggle',
+  },
+};
