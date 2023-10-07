bb40292b063e1f26e22f38be99a5375b0b785c19
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-07 00:08:31 -0300

    [:electric_plug:] | implements: gateway - websocket > publish (module/socket.io)

---
+++ twitter_clone/src/app/@types/gateway/reactive/websocket/publish.ts
@@ -0,0 +1,4 @@
+export interface IPublishParams {
+  event: string;
+  data: any;
+}
--- twitter_clone/src/app/modules/gateway/reactive/socketio/generic/websocket.gateway.ts
+++ twitter_clone/src/app/modules/gateway/reactive/socketio/generic/websocket.gateway.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'inversify';
 import { WebSocketGateway } from '../../../generic/websocket.gateway';
 import { SocketIO } from '@/app/modules/reactive/socket/io';
 import { MODULE } from '@/app/modules/app.registry';
+import { IPublishParams } from '@/app/@types/gateway/reactive/websocket/publish';
 
 @injectable()
 export class SocketIOGateway implements WebSocketGateway<SocketIO> {
@@ -18,5 +19,9 @@ export class SocketIOGateway implements WebSocketGateway<SocketIO> {
 
   get socket() {
     return this._socket;
+  }
+
+  publish(payload: IPublishParams) {
+    this.socket.io.emit(payload.event, payload.data);
   }
 }
