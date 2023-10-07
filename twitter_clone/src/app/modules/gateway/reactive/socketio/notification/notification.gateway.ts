7f99f231bb3aec211bc830d61b8f394fac9e5012
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-07 10:56:53 -0300

    [:electric_plug:] | implements: gateway - notification > publishNew (module/socket.io)

---
+++ twitter_clone/src/app/modules/gateway/reactive/socketio/notification/notification.gateway.ts
@@ -0,0 +1,16 @@
+import { IReactiveNotificationGateway } from '@/app/modules/@core/notification/gateway/reactive/notification.gateway';
+import { SocketIOGateway } from '../generic/websocket.gateway';
+import { IPublishNotificationDTO } from '@/app/modules/@core/notification/DTO/reactive/publish/create.dto';
+import { SOCKET } from '@/app/modules/reactive/reactive.config';
+import { IPublishNotificationViewedDTO } from '@/app/modules/@core/notification/DTO/reactive/publish/view.dto';
+import { IPublishNotificationDeleteDTO } from '@/app/modules/@core/notification/DTO/reactive/publish/delete.dto';
+import { ISubscribeNotificationCreatedDTO } from '@/app/modules/@core/notification/DTO/reactive/subscribe/created.dto';
+
+export class SocketIONotificationGateway
+  extends SocketIOGateway
+  implements IReactiveNotificationGateway
+{
+  publishNewNotification(data: IPublishNotificationDTO) {
+    this.publish({ event: SOCKET.NOTIFICATION.NEW, data });
+  }
+}
