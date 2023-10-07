b774413ce7116284f048ea11eb894518734908cb
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-06 21:53:57 -0300

    [:electric_plug:] | implement: gateway - notification > create (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/notification/notification.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/notification/notification.gateway.ts
@@ -1,6 +1,14 @@
 import { INotificationGateway } from '@/app/modules/@core/notification/gateway/notification.gateway';
 import { AxiosHTTPGateway } from '../generic/http.gateway';
 import { injectable } from 'inversify';
+import {
+  ICreateNotificationDTO,
+  ISetNotificationVisualizedDTO,
+  IGetNotificationsByUserDTO,
+  INotificationDTO,
+} from '@/app/modules/@core/notification/DTO';
+import { Notification } from '@/app/modules/@core/notification/entity';
+import { SWRResponse } from 'swr';
 
 @injectable()
 export class AxiosNotificationGateway
@@ -11,5 +19,25 @@ export class AxiosNotificationGateway
 
   get fullURL() {
     return `${this.URL}/${this.prefix}`;
+  }
+
+  async create(notification: ICreateNotificationDTO): Promise<Notification> {
+    const result = await this.post<{ notification: INotificationDTO }>(
+      this.fullURL,
+      notification,
+    );
+
+    return Notification.create(result.data.notification);
+  }
+  setVisualized(
+    notification: ISetNotificationVisualizedDTO,
+  ): Promise<Notification> {
+    throw new Error('Method not implemented.');
+  }
+  getByUser(notification: IGetNotificationsByUserDTO): Promise<Notification[]> {
+    throw new Error('Method not implemented.');
+  }
+  swrGetByUser(): SWRResponse<INotificationDTO[], any, any> {
+    throw new Error('Method not implemented.');
   }
 }
