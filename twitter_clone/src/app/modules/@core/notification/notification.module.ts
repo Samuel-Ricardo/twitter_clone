a861da878459f380622de72a36dcf56a44bd7ff8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 17:37:05 -0300

    [:sparkles:] | update: module - notification (module)

--- twitter_clone/src/app/modules/@core/notification/notification.module.ts
+++ twitter_clone/src/app/modules/@core/notification/notification.module.ts
@@ -7,6 +7,8 @@ import { ViewNotificationUseCase } from './use-case/view.use-case';
 import { DeleteNotificationUseCase } from './use-case/delete.use-case';
 import { FindNotificationByUserUseCase } from './use-case/findByUser.use-case';
 import { NotificationService } from './service/notification.service';
+import { ReactiveNotificationService } from './service/reactive/notification.service';
+import { SCOPE } from './notification.tag';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -32,4 +34,10 @@ NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.USE_CASE.FIND.BY.USER).to(
   FindNotificationByUserUseCase,
 );
 
-NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.SERVICE).to(NotificationService);
+NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.SERVICE)
+  .to(NotificationService)
+  .whenTargetTagged(SCOPE.TAG, SCOPE.MAIN);
+
+NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.SERVICE)
+  .to(ReactiveNotificationService)
+  .whenTargetTagged(SCOPE.TAG, SCOPE.REACTIVE);
