9c4b9fde2fe082a153cb2083515e1fe4d8f52c94
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 16:43:30 -0300

    [:sparkles:] | create: module - notification (module)

---
+++ twitter_clone/src/app/modules/@core/notification/notification.module.ts
@@ -0,0 +1,27 @@
+import { Container } from 'inversify';
+import { OBSERVABLE_MODULE } from '../../observable/observable.module';
+import { GatewayModule } from '../../gateway/gateway.module';
+import { NOTIFICATION_REGISTRY } from './notification.registry';
+import { CreateNotificationUseCase } from './use-case/create.use-case';
+import { ViewNotificationUseCase } from './use-case/view.use-case';
+import { DeleteNotificationUseCase } from './use-case/delete.use-case';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const NOTIFICATION_MODULE = Container.merge(
+  MODULE,
+  OBSERVABLE_MODULE,
+  GatewayModule,
+);
+
+NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.USE_CASE.CREATE).to(
+  CreateNotificationUseCase,
+);
+
+NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.USE_CASE.VIEW).to(
+  ViewNotificationUseCase,
+);
+
+NOTIFICATION_MODULE.bind(NOTIFICATION_REGISTRY.USE_CASE.DELETE).to(
+  DeleteNotificationUseCase,
+);
