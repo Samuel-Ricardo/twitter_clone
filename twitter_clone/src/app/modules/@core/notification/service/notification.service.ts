07c54e05122eafc82c71642f4efae9444cdc0ec9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 16:51:44 -0300

    [:sparkles:] | create: service - notification > view (module)

--- twitter_clone/src/app/modules/@core/notification/service/notification.service.ts
+++ twitter_clone/src/app/modules/@core/notification/service/notification.service.ts
@@ -1,12 +1,30 @@
-import { injectable } from 'inversify';
-import { ICreateNotificationDTO } from '../DTO';
+import { inject, injectable } from 'inversify';
+import {
+  ICreateNotificationDTO,
+  IDeleteNotificationDTO,
+  ISetNotificationVisualizedDTO,
+} from '../DTO';
 import { CreateNotificationUseCase } from '../use-case/create.use-case';
+import { MODULE } from '@/app/modules/app.registry';
+import { ViewNotificationUseCase } from '../use-case/view.use-case';
+import { DeleteNotificationUseCase } from '../use-case/delete.use-case';
 
 @injectable()
 export class NotificationService {
-  constructor(private readonly createNotification: CreateNotificationUseCase) {}
+  constructor(
+    @inject(MODULE.NOTIFICATION.USE_CASE.CREATE)
+    private readonly createNotification: CreateNotificationUseCase,
+    @inject(MODULE.NOTIFICATION.USE_CASE.VIEW)
+    private readonly viewNotification: ViewNotificationUseCase,
+    @inject(MODULE.NOTIFICATION.USE_CASE.DELETE)
+    private readonly deleteNotification: DeleteNotificationUseCase,
+  ) {}
 
   create(notification: ICreateNotificationDTO) {
     this.createNotification.execute(notification);
+  }
+
+  view(notification: ISetNotificationVisualizedDTO) {
+    this.viewNotification.execute(notification);
   }
 }
