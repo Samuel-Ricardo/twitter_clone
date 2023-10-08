01457f90e9af3d4ed47ba72af44a7551f95d08de
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 12:21:04 -0300

    [:sparkles:] | create: support - gateway > reactive (module/notification)

---
+++ twitter_clone/src/app/modules/@core/notification/gateway/reactive/gateway.support.ts
@@ -0,0 +1,11 @@
+import { MODULE } from '@/app/modules/app.registry';
+import { inject, injectable } from 'inversify';
+import { type IReactiveNotificationGateway } from './notification.gateway';
+
+@injectable()
+export abstract class NotificationReactiveGatewaySupport {
+  constructor(
+    @inject(MODULE.GATEWAY.REACTIVE.SOCKET.IO.NOTIFICATION)
+    protected readonly gateway: IReactiveNotificationGateway,
+  ) {}
+}
--- twitter_clone/src/app/modules/gateway/gateway.module.ts
+++ twitter_clone/src/app/modules/gateway/gateway.module.ts
@@ -1,7 +1,15 @@
 import { Container } from 'inversify';
 import { AxiosGatewayModule } from './axios/axios.module';
 import { REACTIVE_GATEWAY_MODULE } from './reactive/reactive.module';
+import { ConfigRegistry } from '../config/config.registry';
+import { CONFIG } from '../config/app.config';
 
 const Module = new Container({ autoBindInjectable: true });
 
-export const GatewayModule = Container.merge(Module, AxiosGatewayModule);
+export const GatewayModule = Container.merge(
+  Module,
+  AxiosGatewayModule,
+  REACTIVE_GATEWAY_MODULE,
+);
+
+GatewayModule.rebind(ConfigRegistry.API.URL).toConstantValue(CONFIG.API.URL);
