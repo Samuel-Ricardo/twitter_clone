eb3cd9e1875ebaf163d60b2ddfceb95dfff0cf7e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 16:39:28 -0300

    [:sparkles:] | create: registry - notification (module)

---
+++ twitter_clone/src/app/modules/@core/notification/notification.registry.ts
@@ -0,0 +1,7 @@
+export const NOTIFICATION_REGISTRY = {
+  USE_CASE: {
+    CREATE: Symbol.for('module:notification.use-case.create'),
+    DELETE: Symbol.for('module:notification.use-case.delete'),
+    VIEW: Symbol.for('module:notification.use-case.view'),
+  },
+};
