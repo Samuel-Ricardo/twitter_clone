4b34b391b8495dc4bb308ecedd5deb61c7ef64e2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 17:04:08 -0300

    [:sparkles:] | create: controller - notification > create (module)

--- twitter_clone/src/app/modules/@core/notification/controller/notification.controller.ts
+++ twitter_clone/src/app/modules/@core/notification/controller/notification.controller.ts
@@ -1,6 +1,7 @@
 import { MODULE } from '@/app/modules/app.registry';
 import { inject, injectable } from 'inversify';
 import { NotificationService } from '../service/notification.service';
+import { ICreateNotificationDTO } from '../DTO';
 
 @injectable()
 export class NotificationController {
@@ -8,4 +9,8 @@ export class NotificationController {
     @inject(MODULE.NOTIFICATION.SERVICE)
     private readonly service: NotificationService,
   ) {}
+
+  create(notification: ICreateNotificationDTO) {
+    this.service.create(notification);
+  }
 }
