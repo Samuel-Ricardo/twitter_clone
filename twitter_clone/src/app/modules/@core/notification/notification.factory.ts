28720342d20ba614d8471ee500f38675b5fc0fed
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 17:44:18 -0300

    [:sparkles:] | update: factory - notification > reactive (module)

--- twitter_clone/src/app/modules/@core/notification/notification.factory.ts
+++ twitter_clone/src/app/modules/@core/notification/notification.factory.ts
@@ -1,14 +1,30 @@
 import { NOTIFICATION_MODULE } from './notification.module';
 import { NOTIFICATION_REGISTRY } from './notification.registry';
+import { SCOPE } from './notification.tag';
 import { NotificationService } from './service/notification.service';
+import { ReactiveNotificationService } from './service/reactive/notification.service';
 import { CreateNotificationUseCase } from './use-case/create.use-case';
 import { DeleteNotificationUseCase } from './use-case/delete.use-case';
 import { FindNotificationByUserUseCase } from './use-case/findByUser.use-case';
 import { ViewNotificationUseCase } from './use-case/view.use-case';
 
 export const NOTIFICATION_FACTORY = {
-  SERVICE: () =>
-    NOTIFICATION_MODULE.get<NotificationService>(NOTIFICATION_REGISTRY.SERVICE),
+  CONTROLLER: {},
+  SERVICE: {
+    MAIN: () =>
+      NOTIFICATION_MODULE.getTagged<ReactiveNotificationService>(
+        NOTIFICATION_REGISTRY.SERVICE,
+        SCOPE.TAG,
+        SCOPE.MAIN,
+      ),
+
+    REACTIVE: () =>
+      NOTIFICATION_MODULE.getTagged<NotificationService>(
+        NOTIFICATION_REGISTRY.SERVICE,
+        SCOPE.TAG,
+        SCOPE.REACTIVE,
+      ),
+  },
   USE_CASE: {
     CREATE: () =>
       NOTIFICATION_MODULE.get<CreateNotificationUseCase>(
