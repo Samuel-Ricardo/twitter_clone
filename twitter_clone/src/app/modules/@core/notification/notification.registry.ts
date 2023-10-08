fcb153e4d9fc69759c889659ffcbd6317c720072
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 16:57:30 -0300

    [:sparkles:] | update: registry - notification > use-case (module)

--- twitter_clone/src/app/modules/@core/notification/notification.registry.ts
+++ twitter_clone/src/app/modules/@core/notification/notification.registry.ts
@@ -3,5 +3,8 @@ export const NOTIFICATION_REGISTRY = {
     CREATE: Symbol.for('module:notification.use-case.create'),
     DELETE: Symbol.for('module:notification.use-case.delete'),
     VIEW: Symbol.for('module:notification.use-case.view'),
+    FIND: {
+      BY: { USER: Symbol.for('module:notification.use-case.find.by.user') },
+    },
   },
 };
