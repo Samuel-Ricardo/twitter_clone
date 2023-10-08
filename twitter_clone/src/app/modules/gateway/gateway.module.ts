48769b969480c8b85bbc5b24e6b49f1c639bb18b
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 10:13:05 -0300

    [:electric_plug:] | update: registry - gateway > reactive (module)

--- twitter_clone/src/app/modules/gateway/gateway.module.ts
+++ twitter_clone/src/app/modules/gateway/gateway.module.ts
@@ -1,5 +1,6 @@
 import { Container } from 'inversify';
 import { AxiosGatewayModule } from './axios/axios.module';
+import { REACTIVE_GATEWAY_MODULE } from './reactive/reactive.module';
 
 const Module = new Container({ autoBindInjectable: true });
 
--- twitter_clone/src/app/modules/gateway/gateway.registry.ts
+++ twitter_clone/src/app/modules/gateway/gateway.registry.ts
@@ -1,5 +1,7 @@
 import { AxiosGatewayRegistry as AXIOS } from './axios/axios.registry';
+import { REACTIVE_GATEWAY_REGISTRY as REACTIVE } from './reactive/reactive.registry';
 
 export const GatewayRegistry = {
   AXIOS,
+  REACTIVE,
 };
