6542f252fd71677ef067a84ed635b62225c210c2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 17:37:30 -0300

    [:sparkles:] | update: registry - notification (module)

--- twitter_clone/src/app/modules/@core/notification/notification.registry.ts
+++ twitter_clone/src/app/modules/@core/notification/notification.registry.ts
@@ -1,4 +1,7 @@
 export const NOTIFICATION_REGISTRY = {
+  MAIN: Symbol.for('module:notification.main'),
+  REACTIVE: Symbol.for('module:notification.reactive'),
+  CONTROLLER: Symbol.for('module:notification.controller'),
   SERVICE: Symbol.for('module:notification.service'),
   USE_CASE: {
     CREATE: Symbol.for('module:notification.use-case.create'),
