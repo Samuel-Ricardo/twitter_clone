e8f21277e22f32db032f2fc5048af324530e6e41
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-08 16:17:48 -0300

    [:mag_right:] | update: observable - notification (module)

--- twitter_clone/src/app/modules/@core/notification/observable/notification.observable.ts
+++ twitter_clone/src/app/modules/@core/notification/observable/notification.observable.ts
@@ -1,12 +1,36 @@
-import { IListenDislikeDTO } from '../../like/DTO/observable/listen/dislike.dto';
-import { IListenLikeDTO } from '../../like/DTO/observable/listen/like.dto';
-import { IEmitNotificationDTO } from '../DTO/observable/emit/create.dto';
+import {
+  IEmitNotificationDTO,
+  IEmitNotificationDeletedDTO,
+  IEmitNotificationViewedDTO,
+} from '../DTO/observable/emit';
+import {
+  IListenNotificationDTO,
+  IListenNotificationDeletedDTO,
+  IListenNotificationViewedDTO,
+} from '../DTO/observable/listen';
 
 export interface INotificationObservable {
   emitNotification(notification: IEmitNotificationDTO): any | Promise<any>;
+  listenNotification(scheduled: IListenNotificationDTO): any | Promise<any>;
 
-  listenLike: (schedule: IListenLikeDTO) => any | Promise<any>;
-  listenDislike: (schedule: IListenDislikeDTO) => any | Promise<any>;
+  emitNotificationViewed(
+    notification: IEmitNotificationViewedDTO,
+  ): any | Promise<any>;
+  listenNotificationViewed(
+    scheduled: IListenNotificationViewedDTO,
+  ): any | Promise<any>;
+
+  emitNotificationDeleted(
+    notification: IEmitNotificationDeletedDTO,
+  ): any | Promise<any>;
+  listenNotificationDeleted(
+    scheduled: IListenNotificationDeletedDTO,
+  ): any | Promise<any>;
+
+  //  emitDeleteNotification(notification: IEmitNotificationDTO): any | Promise<any>;
+
+  //  listenLike: (schedule: IListenLikeDTO) => any | Promise<any>;
+  //  listenDislike: (schedule: IListenDislikeDTO) => any | Promise<any>;
   // listenPost: (schedule: IListenPostDTO) => any | Promise<any>;
   // listenComment: (schedule: IListenCommentDTO) => any | Promise<any>;
   // listenFollow: (schedule: IListenFollowDTO) => any | Promise<any>;
