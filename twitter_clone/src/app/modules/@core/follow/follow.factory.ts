9a62c9fa314c115be1d0661ef96c30c60cccf916
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 20:51:37 -0300

    [:sparkles:] | update: module - follow (module)

--- twitter_clone/src/app/modules/@core/follow/follow.factory.ts
+++ twitter_clone/src/app/modules/@core/follow/follow.factory.ts
@@ -10,33 +10,62 @@ import {
   GetFollowersUseCase,
   GetFollowingUseCase,
 } from './use-case';
+import { EmitFollowUseCase } from './use-case/observable/emit/created.use-case';
+import { EmitUnfollowUseCase } from './use-case/observable/emit/deleted.use-case';
+import { ListenFollowUseCase } from './use-case/observable/listen/created.use-case';
+import { ListenUnfollowUseCase } from './use-case/observable/listen/deleted.use-case';
 
 export const FOLLOW_FACTORY = {
   MAIN: () => FOLLOW_MODULE.get<FollowController>(FOLLOW_REGISTRY.CONTROLLER),
-  CONTROLLER: FOLLOW_MODULE.get<FollowController>(FOLLOW_REGISTRY.CONTROLLER),
-  SERVICE: FOLLOW_MODULE.get<FollowService>(FOLLOW_REGISTRY.SERVICE),
+  CONTROLLER: () =>
+    FOLLOW_MODULE.get<FollowController>(FOLLOW_REGISTRY.CONTROLLER),
+  SERVICE: () => FOLLOW_MODULE.get<FollowService>(FOLLOW_REGISTRY.SERVICE),
   USE_CASE: {
-    CREATE: FOLLOW_MODULE.get<CreateFollowUseCase>(
-      FOLLOW_REGISTRY.USE_CASE.CREATE,
-    ),
-    DELETE: FOLLOW_MODULE.get<DeleteFollowUseCase>(
-      FOLLOW_REGISTRY.USE_CASE.DELETE,
-    ),
+    CREATE: () =>
+      FOLLOW_MODULE.get<CreateFollowUseCase>(FOLLOW_REGISTRY.USE_CASE.CREATE),
+    DELETE: () =>
+      FOLLOW_MODULE.get<DeleteFollowUseCase>(FOLLOW_REGISTRY.USE_CASE.DELETE),
     GET: {
-      FOLLOWERS: FOLLOW_MODULE.get<GetFollowersUseCase>(
-        FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWERS,
-      ),
-      FOLLOWING: FOLLOW_MODULE.get<GetFollowingUseCase>(
-        FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWING,
-      ),
+      FOLLOWERS: () =>
+        FOLLOW_MODULE.get<GetFollowersUseCase>(
+          FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWERS,
+        ),
+      FOLLOWING: () =>
+        FOLLOW_MODULE.get<GetFollowingUseCase>(
+          FOLLOW_REGISTRY.USE_CASE.GET.FOLLOWING,
+        ),
     },
     COUNT: {
-      FOLLOWERS: FOLLOW_MODULE.get<CountFollowersUseCase>(
-        FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWERS,
-      ),
-      FOLLOWING: FOLLOW_MODULE.get<CountFollowingUseCase>(
-        FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWING,
-      ),
+      FOLLOWERS: () =>
+        FOLLOW_MODULE.get<CountFollowersUseCase>(
+          FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWERS,
+        ),
+      FOLLOWING: () =>
+        FOLLOW_MODULE.get<CountFollowingUseCase>(
+          FOLLOW_REGISTRY.USE_CASE.COUNT.FOLLOWING,
+        ),
+    },
+    OBSERVABLE: {
+      EMIT: {
+        CREATED: () =>
+          FOLLOW_MODULE.get<EmitFollowUseCase>(
+            FOLLOW_REGISTRY.USE_CASE.OBSERVABLE.EMIT.CREATED,
+          ),
+        DELETED: () =>
+          FOLLOW_MODULE.get<EmitUnfollowUseCase>(
+            FOLLOW_REGISTRY.USE_CASE.OBSERVABLE.EMIT.DELETED,
+          ),
+      },
+      LISTEN: {
+        CREATED: () =>
+          FOLLOW_MODULE.get<ListenFollowUseCase>(
+            FOLLOW_REGISTRY.USE_CASE.OBSERVABLE.LISTEN.CREATED,
+          ),
+        DELETED: () =>
+          FOLLOW_MODULE.get<ListenUnfollowUseCase>(
+            FOLLOW_REGISTRY.USE_CASE.OBSERVABLE.LISTEN.DELETED,
+          ),
+      },
     },
   },
 };
