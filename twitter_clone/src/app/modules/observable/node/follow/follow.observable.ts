c6a8b5aa35aaf5ce4f4397c3d9117c794534d25f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 20:13:57 -0300

    [:mag_right:] | create: observable - emit > unfollow (module/node)

--- twitter_clone/src/app/modules/observable/node/follow/follow.observable.ts
+++ twitter_clone/src/app/modules/observable/node/follow/follow.observable.ts
@@ -1,12 +1,17 @@
 import { IFollowObservable } from '@/app/modules/@core/follow/observable/follow.observable';
 import { NodeObservable } from '../generic/generic.observable';
 import { IEmitFollowDTO } from '@/app/modules/@core/follow/DTO/observable/emit/created.dto';
+import { IEmitUnfollowDTO } from '@/app/modules/@core/follow/DTO/observable/emit/deleted.dto';
 
 export class NodeFollowObservable
   extends NodeObservable
   implements IFollowObservable
 {
   emitFollow(follow: IEmitFollowDTO): any | Promise<any> {
-    throw new Error('Method not implemented.');
+    this.emit(this.EVENTS.FOLLOW.CREATED, follow);
+  }
+
+  emitUnfollow(unfollow: IEmitUnfollowDTO): any | Promise<any> {
+    this.emit(this.EVENTS.FOLLOW.DELETED, unfollow);
   }
 }
