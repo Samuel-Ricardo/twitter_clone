befc524ab42bd1c3f8bf783427f7e29efcb384d4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 15:26:25 -0300

    [:sparkles:] | create: DTO - find by [id] > comment (module)

---
+++ twitter_clone/src/app/modules/@core/comment/DTO/get_by_id.dto.ts
@@ -0,0 +1,3 @@
+export interface IFindCommentByIDDTO {
+  id: string;
+}
--- twitter_clone/src/app/modules/@core/notification/service/reactive/notification.service.ts
+++ twitter_clone/src/app/modules/@core/notification/service/reactive/notification.service.ts
@@ -1,21 +1,53 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { IListenNotificationDTO } from '../../DTO/observable/listen/created.dto';
 import { EmitNotificationUseCase } from '../../use-case/observable/emit/created.use-case';
 import { ListenNotificationUseCase } from '../../use-case/observable/listen/created.use-case';
 import { SubscribeNotificationUseCase } from '../../use-case/reactive/subscribe/created.use-case';
+import { ListenPostLikeUseCase } from '../../../like/use-case/observable/listen/post/created.use-case';
+import { ListenPostDislikeUseCase } from '../../../like/use-case/observable/listen/post/deleted.use-case';
+import { LIKE } from '../../entity';
+import { PostController } from '../../../post';
+import { NotificationController } from '../../controller/notification.controller';
+import { MODULE } from '@/app/modules/app.registry';
+import { ReactiveLikeController } from '../../../like/controller/reactive/like.controller';
 
 //singleton
 @injectable()
 export class ReactiveNotificationService {
   constructor(
+    private readonly notification: NotificationController,
+    @inject(MODULE.POST.MAIN)
+    private readonly postModule: PostController,
+    @inject(MODULE.LIKE.REACTIVE)
+    private readonly likeModule: ReactiveLikeController,
     private readonly subscribeNotification: SubscribeNotificationUseCase,
     private readonly listenNotification: ListenNotificationUseCase,
-    private readonly emitNotification: EmitNotificationUseCase, //private readonly listenNotification: ListenNotificationUseCase
+    private readonly emitNotification: EmitNotificationUseCase,
   ) {}
 
-  observehNotifications() {
+  observeNotifications() {
     this.subscribeNotification.execute({
       job: (notification) => this.emitNotification.execute(notification),
+    });
+  }
+
+  observeLikes() {
+    this.observeTweetLikes();
+  }
+
+  observeTweetLikes() {
+    this.likeModule.onTweetLike({
+      action: async (like) => {
+        const { post } = await this.postModule.findByIdAsync({
+          id: like.likedId,
+        });
+        this.notification.create({
+          type: LIKE,
+          body: `Someone Like Your Tweet 👍 - ${post.body.slice(0, 15)}...`,
+          userId: post.authorId,
+          eventId: like.id,
+        });
+      },
     });
   }
 
