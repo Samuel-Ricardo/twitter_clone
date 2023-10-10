ee10be435f1a6e831f3b5ee79e0bbca34eb88ab5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 20:11:09 -0300

    [:gear:] | update: config - events (module)

--- twitter_clone/src/app/modules/config/event/event.config.ts
+++ twitter_clone/src/app/modules/config/event/event.config.ts
@@ -1,7 +1,11 @@
 import { LIKE_EVENTS as LIKE } from '../../@core/like/observable/observable.config';
 import { NOTIFICATION_EVENTS as NOTIFICATION } from '../../@core/notification/observable/observable.config';
+import { FOLLOW_EVENTS as FOLLOW } from '../../@core/follow/observable/observable.config';
+import { SOCKET } from '../../reactive/reactive.config';
 
 export const EVENT = {
   ...LIKE,
   NOTIFICATION,
+  FOLLOW,
+  SOCKET,
 };
