d7f053831fb2c338a4c7feb140bdd540c5544747
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 15:36:35 -0300

    [:sparkles:] | update: module - comment (module)

--- twitter_clone/src/app/modules/@core/comment/comment.module.ts
+++ twitter_clone/src/app/modules/@core/comment/comment.module.ts
@@ -10,6 +10,7 @@ import {
 import { COMMENT_REGISTRY } from './comment.registry';
 import { CommentService } from './service/comment.service';
 import { CommentController } from './controller/comment.controller';
+import { FindCommentByIDUseCase } from './use-case/find_by_id.use-case';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -25,6 +26,11 @@ COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.FIND.BY.POST.ID).to(
 COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.FIND.BY.AUTHOR.ID).to(
   FindUserCommentsUseCase,
 );
+COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.FIND.BY.ID).to(
+  FindCommentByIDUseCase,
+);
 
 COMMENT_MODULE.bind(COMMENT_REGISTRY.SERVICE).to(CommentService);
 COMMENT_MODULE.bind(COMMENT_REGISTRY.CONTROLLER).to(CommentController);
+
+COMMENT_MODULE.bind(COMMENT_REGISTRY.MAIN).to(CommentController);
