b00ef0c4e48643601c9f428e18b2c7d898373e63
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 15:40:12 -0300

    [:sparkles:] | update: service - notification (module/reactive)

--- twitter_clone/src/app/modules/@core/notification/service/reactive/notification.service.ts
+++ twitter_clone/src/app/modules/@core/notification/service/reactive/notification.service.ts
@@ -3,23 +3,24 @@ import { IListenNotificationDTO } from '../../DTO/observable/listen/created.dto'
 import { EmitNotificationUseCase } from '../../use-case/observable/emit/created.use-case';
 import { ListenNotificationUseCase } from '../../use-case/observable/listen/created.use-case';
 import { SubscribeNotificationUseCase } from '../../use-case/reactive/subscribe/created.use-case';
-import { ListenPostLikeUseCase } from '../../../like/use-case/observable/listen/post/created.use-case';
-import { ListenPostDislikeUseCase } from '../../../like/use-case/observable/listen/post/deleted.use-case';
 import { LIKE } from '../../entity';
 import { PostController } from '../../../post';
 import { NotificationController } from '../../controller/notification.controller';
 import { MODULE } from '@/app/modules/app.registry';
 import { ReactiveLikeController } from '../../../like/controller/reactive/like.controller';
+import { CommentController } from '../../../comment/controller/comment.controller';
 
 //singleton
 @injectable()
 export class ReactiveNotificationService {
   constructor(
     private readonly notification: NotificationController,
-    @inject(MODULE.POST.MAIN)
-    private readonly postModule: PostController,
     @inject(MODULE.LIKE.REACTIVE)
     private readonly likeModule: ReactiveLikeController,
+    @inject(MODULE.POST.MAIN)
+    private readonly postModule: PostController,
+    @inject(MODULE.COMMENT.MAIN)
+    private readonly commentModule: CommentController,
     private readonly subscribeNotification: SubscribeNotificationUseCase,
     private readonly listenNotification: ListenNotificationUseCase,
     private readonly emitNotification: EmitNotificationUseCase,
@@ -33,6 +34,7 @@ export class ReactiveNotificationService {
 
   observeLikes() {
     this.observeTweetLikes();
+    this.observeCommentLikes();
   }
 
   observeTweetLikes() {
@@ -45,6 +47,25 @@ export class ReactiveNotificationService {
           type: LIKE,
           body: `Someone Like Your Tweet 👍 - ${post.body.slice(0, 15)}...`,
           userId: post.authorId,
+          eventId: like.id,
+        });
+      },
+    });
+  }
+
+  observeCommentLikes() {
+    this.likeModule.onCommentLike({
+      action: async (like) => {
+        const { comment } = await this.commentModule.findAsyncByID({
+          id: like.likedId,
+        });
+        this.notification.create({
+          type: LIKE,
+          body: `Someone Like Your Comment 👍 - ${comment.body.slice(
+            0,
+            15,
+          )}...`,
+          userId: comment.authorId,
           eventId: like.id,
         });
       },
