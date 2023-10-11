cb77edbca7b446a95876713602e5b4e8f711b3b0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 23:43:06 -0300

    [:eyes:] | create: controller - emit > post (module/reactive)

--- twitter_clone/src/app/modules/@core/post/controller/reactive/post.controller.ts
+++ twitter_clone/src/app/modules/@core/post/controller/reactive/post.controller.ts
@@ -2,6 +2,8 @@ import { inject, injectable, tagged } from 'inversify';
 import { ReactivePostService } from '../../service/reactive/post.service';
 import { MODULE } from '@/app/modules/app.registry';
 import { SCOPE } from '../../post.tag';
+import { IEmitPostDTO } from '../../DTO/observable/emit/created.dto';
+import { IListenPostDTO } from '../../DTO/observable/listen/created.dto';
 
 @injectable()
 export class ReactivePostController {
@@ -10,4 +12,12 @@ export class ReactivePostController {
     @tagged(SCOPE.TAG, SCOPE.REACTIVE)
     private readonly service: ReactivePostService,
   ) {}
+
+  emitPost(post: IEmitPostDTO) {
+    return this.service.emitPost(post);
+  }
+
+  onPost(post: IListenPostDTO) {
+    return this.service.listenPost(post);
+  }
 }
