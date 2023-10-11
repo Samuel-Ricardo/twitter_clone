46404abf4abaff617379f8ffcaf7c3a63c5047f7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-11 09:32:52 -0300

    [:eyes:] | implement: observable - listen > comment (module/node)

--- twitter_clone/src/app/modules/observable/node/comment/comment.observable.ts
+++ twitter_clone/src/app/modules/observable/node/comment/comment.observable.ts
@@ -1,6 +1,7 @@
 import { ICommentObservable } from '@/app/modules/@core/comment/observable/comment.observable';
 import { NodeObservable } from '../generic/generic.observable';
 import { IEmitCommentDTO } from '@/app/modules/@core/comment/DTO/observable/emit/created.dto';
+import { IListenCommentDTO } from '@/app/modules/@core/comment/DTO/observable/listen/created.dto';
 
 export class NodeCommentObservable
   extends NodeObservable
@@ -8,5 +9,8 @@ export class NodeCommentObservable
 {
   emitComment(comment: IEmitCommentDTO) {
     return this.emit(this.EVENTS.COMMENT.CREATED, comment);
+  }
+  listenComment(scheduled: IListenCommentDTO) {
+    return this.listen(this.EVENTS.COMMENT.CREATED, scheduled.action);
   }
 }
