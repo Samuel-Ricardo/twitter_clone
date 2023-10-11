e191ae52d0c3284dd0f19695083eb6d3e530ea8f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-11 09:58:51 -0300

    [:eyes:] | create: controller - emit > comment (module/reactive)

--- twitter_clone/src/app/modules/@core/comment/controller/reactive/comment.controller.ts
+++ twitter_clone/src/app/modules/@core/comment/controller/reactive/comment.controller.ts
@@ -2,6 +2,7 @@ import { inject, injectable, tagged } from 'inversify';
 import { ReactiveCommentService } from '../../service/reactive/comment.service';
 import { MODULE } from '@/app/modules/app.registry';
 import { SCOPE } from '../../../notification/notification.tag';
+import { IEmitCommentDTO } from '../../DTO/observable/emit/created.dto';
 
 @injectable()
 export class ReactiveCommentController {
@@ -10,4 +11,8 @@ export class ReactiveCommentController {
     @tagged(SCOPE.TAG, SCOPE.REACTIVE)
     private readonly service: ReactiveCommentService,
   ) {}
+
+  async emitComment(comment: IEmitCommentDTO) {
+    return this.service.emitComment(comment);
+  }
 }
