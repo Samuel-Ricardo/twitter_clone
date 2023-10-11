9f509fce3ebf82276b216b2ab7462f8cce8983eb
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 23:11:36 -0300

    [:sparkles:] | update: module - post (module)

--- twitter_clone/src/app/modules/@core/post/post.module.ts
+++ twitter_clone/src/app/modules/@core/post/post.module.ts
@@ -5,11 +5,14 @@ import {
   CreatePostUseCase,
   DeletePostUseCase,
   FindAllPostUseCase,
+  FindPostByIdUseCase,
   FindPostsByAuthorUseCase,
   UpdatePostUseCase,
 } from './use-case';
 import { PostService } from './service';
 import { PostController } from './controller/post.controller';
+import { EmitPostUseCase } from './use-case/observable/emit/created.use-case';
+import { ListenPostUseCase } from './use-case/observable/listen/created.use-case';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -21,11 +24,17 @@ POST_MODULE.bind(POST_REGISTRY.USE_CASE.FIND.ALL).to(FindAllPostUseCase);
 POST_MODULE.bind(POST_REGISTRY.USE_CASE.FIND.BY.AUTHOR.ID).to(
   FindPostsByAuthorUseCase,
 );
-POST_MODULE.bind(POST_REGISTRY.USE_CASE.FIND.BY.ID).to(
-  FindPostsByAuthorUseCase,
-);
+POST_MODULE.bind(POST_REGISTRY.USE_CASE.FIND.BY.ID).to(FindPostByIdUseCase);
 POST_MODULE.bind(POST_REGISTRY.USE_CASE.UPDATE).to(UpdatePostUseCase);
+
+POST_MODULE.bind(POST_REGISTRY.USE_CASE.OBSERVABLE.EMIT.CREATED).to(
+  EmitPostUseCase,
+);
+POST_MODULE.bind(POST_REGISTRY.USE_CASE.OBSERVABLE.LISTEN.CREATED).to(
+  ListenPostUseCase,
+);
 
 POST_MODULE.bind(POST_REGISTRY.SERVICE).to(PostService);
 
 POST_MODULE.bind(POST_REGISTRY.CONTROLLER).to(PostController);
+POST_MODULE.bind(POST_REGISTRY.MAIN).to(PostController);
