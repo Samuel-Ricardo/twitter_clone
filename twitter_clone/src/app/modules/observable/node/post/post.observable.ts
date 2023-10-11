54204f056ad8fb21b69a371429f0c75089137357
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 22:32:14 -0300

    [:eyes:] | implements: observable - emit > post (module/node)

--- twitter_clone/src/app/modules/observable/node/post/post.observable.ts
+++ twitter_clone/src/app/modules/observable/node/post/post.observable.ts
@@ -1,6 +1,12 @@
 import { IPostObservable } from '@/app/modules/@core/post/observable/post.observable';
 import { NodeObservable } from '../generic/generic.observable';
+import { IEmitPostDTO } from '@/app/modules/@core/post/DTO/observable/emit/created.dto';
 
 export class NodePostObservable
   extends NodeObservable
-  implements IPostObservable {}
+  implements IPostObservable
+{
+  emitPost(post: IEmitPostDTO) {
+    return this.emit(this.EVENTS.POST.CREATED, post);
+  }
+}
