4bdaccf8c2719eb4f3ae1c759bb64db82b764ac0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-11 09:32:19 -0300

    [:eyes:] | implement: observable - emit > comment (module/node)

--- twitter_clone/src/app/modules/observable/node/comment/comment.observable.ts
+++ twitter_clone/src/app/modules/observable/node/comment/comment.observable.ts
@@ -1,6 +1,12 @@
 import { ICommentObservable } from '@/app/modules/@core/comment/observable/comment.observable';
 import { NodeObservable } from '../generic/generic.observable';
+import { IEmitCommentDTO } from '@/app/modules/@core/comment/DTO/observable/emit/created.dto';
 
 export class NodeCommentObservable
   extends NodeObservable
-  implements ICommentObservable {}
+  implements ICommentObservable
+{
+  emitComment(comment: IEmitCommentDTO) {
+    return this.emit(this.EVENTS.COMMENT.CREATED, comment);
+  }
+}
