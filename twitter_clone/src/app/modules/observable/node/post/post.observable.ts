8418967cbae75f5e2b327664ce89d1daf9a4dc9d
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 22:33:34 -0300

    [:eyes:] | implements: observable - listen > post (module/node)

--- twitter_clone/src/app/modules/observable/node/post/post.observable.ts
+++ twitter_clone/src/app/modules/observable/node/post/post.observable.ts
@@ -1,6 +1,7 @@
 import { IPostObservable } from '@/app/modules/@core/post/observable/post.observable';
 import { NodeObservable } from '../generic/generic.observable';
 import { IEmitPostDTO } from '@/app/modules/@core/post/DTO/observable/emit/created.dto';
+import { IListenPostDTO } from '@/app/modules/@core/post/DTO/observable/listen/created.dto';
 
 export class NodePostObservable
   extends NodeObservable
@@ -8,5 +9,8 @@ export class NodePostObservable
 {
   emitPost(post: IEmitPostDTO) {
     return this.emit(this.EVENTS.POST.CREATED, post);
+  }
+  listenPost(schedule: IListenPostDTO) {
+    return this.listen(this.EVENTS.POST.CREATED, schedule.action);
   }
 }
