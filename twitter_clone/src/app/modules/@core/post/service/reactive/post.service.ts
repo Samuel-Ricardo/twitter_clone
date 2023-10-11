bdc2b870153d939b56c3a5c7559580b42c26215f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 23:32:23 -0300

    [:eyes:] | create: service - listen > post (module/reactive)

--- twitter_clone/src/app/modules/@core/post/service/reactive/post.service.ts
+++ twitter_clone/src/app/modules/@core/post/service/reactive/post.service.ts
@@ -2,6 +2,8 @@ import { inject, injectable } from 'inversify';
 import { IEmitPostDTO } from '../../DTO/observable/emit/created.dto';
 import { EmitPostUseCase } from '../../use-case/observable/emit/created.use-case';
 import { MODULE } from '@/app/modules/app.registry';
+import { IListenPostDTO } from '../../DTO/observable/listen/created.dto';
+import { ListenPostUseCase } from '../../use-case/observable/listen/created.use-case';
 
 @injectable()
 export class ReactivePostService {
@@ -9,10 +11,14 @@ export class ReactivePostService {
     @inject(MODULE.POST.USE_CASE.OBSERVABLE.EMIT.CREATED)
     private readonly emitPostEvent: EmitPostUseCase,
     @inject(MODULE.POST.USE_CASE.OBSERVABLE.LISTEN.CREATED)
-    private readonly listenPostEvent: EmitPostUseCase,
+    private readonly listenPostEvent: ListenPostUseCase,
   ) {}
 
   async emitPost(post: IEmitPostDTO) {
     return await this.emitPostEvent.executeAsync(post);
+  }
+
+  async listenPost(schedule: IListenPostDTO) {
+    return await this.listenPostEvent.executeAsync(schedule);
   }
 }
