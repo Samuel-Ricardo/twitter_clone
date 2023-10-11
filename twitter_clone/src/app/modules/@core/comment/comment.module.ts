e4f00db7bb3d707bd9848f8711401250d20d9916
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-11 09:54:14 -0300

    [:sparkles:] | update: module - comment (module)

--- twitter_clone/src/app/modules/@core/comment/comment.module.ts
+++ twitter_clone/src/app/modules/@core/comment/comment.module.ts
@@ -13,6 +13,8 @@ import { CommentController } from './controller/comment.controller';
 import { FindCommentByIDUseCase } from './use-case/find_by_id.use-case';
 import { EmitCommentUseCase } from './use-case/observable/emit/created.use-case';
 import { ListenCommentUseCase } from './use-case/observable/listen/created.use-case';
+import { ReactiveCommentService } from './service/reactive/comment.service';
+import { SCOPE } from '../../app.tag';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -39,7 +41,15 @@ COMMENT_MODULE.bind(COMMENT_REGISTRY.USE_CASE.OBSERVABLE.EMIT.CREATED).to(
   EmitCommentUseCase,
 );
 
-COMMENT_MODULE.bind(COMMENT_REGISTRY.SERVICE).to(CommentService);
-COMMENT_MODULE.bind(COMMENT_REGISTRY.CONTROLLER).to(CommentController);
+COMMENT_MODULE.bind(COMMENT_REGISTRY.SERVICE)
+  .to(CommentService)
+  .whenTargetTagged(SCOPE.TAG, SCOPE.MAIN);
+COMMENT_MODULE.bind(COMMENT_REGISTRY.CONTROLLER)
+  .to(CommentController)
+  .whenTargetTagged(SCOPE.TAG, SCOPE.MAIN);
 
 COMMENT_MODULE.bind(COMMENT_REGISTRY.MAIN).to(CommentController);
+
+COMMENT_MODULE.bind(COMMENT_REGISTRY.SERVICE)
+  .to(ReactiveCommentService)
+  .whenTargetTagged(SCOPE.TAG, SCOPE.REACTIVE);
