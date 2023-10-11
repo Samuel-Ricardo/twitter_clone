2bf265cc3c6a89b642018acde73affd4d333e0fa
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-10 21:19:40 -0300

    [:sparkles:] | create: controller - emit > follow (module/reactive)

--- twitter_clone/src/app/modules/@core/follow/controller/reactive/follow.controller.ts
+++ twitter_clone/src/app/modules/@core/follow/controller/reactive/follow.controller.ts
@@ -2,6 +2,7 @@ import { MODULE } from '@/app/modules/app.registry';
 import { inject, injectable, tagged } from 'inversify';
 import { SCOPE } from '../../follow.tag';
 import { ReactiveFollowService } from '../../service/reactive/follow.service';
+import { IEmitFollowDTO } from '../../DTO/observable/emit/created.dto';
 
 @injectable()
 export class ReactiveFollowController {
@@ -10,4 +11,8 @@ export class ReactiveFollowController {
     @tagged(SCOPE.TAG, SCOPE.REACTIVE)
     private readonly service: ReactiveFollowService,
   ) {}
+
+  async emitFollow(follow: IEmitFollowDTO) {
+    await this.service.emitFollow(follow);
+  }
 }
