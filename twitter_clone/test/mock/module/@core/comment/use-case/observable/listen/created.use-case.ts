e3344429477f711468c1c15d0ebc521c0aa035c7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-17 17:03:35 -0300

    [:clown_face:] | mock: use-case - listen > comment (module)

---
+++ twitter_clone/test/mock/module/@core/comment/use-case/observable/listen/created.use-case.ts
@@ -0,0 +1,4 @@
+import { ListenCommentUseCase } from '@/app/modules/@core/comment/use-case/observable/listen/created.use-case';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockListenCommentUseCase = mockDeep<ListenCommentUseCase>;
