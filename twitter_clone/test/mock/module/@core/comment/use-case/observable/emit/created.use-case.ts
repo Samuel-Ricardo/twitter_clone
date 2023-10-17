6ee8bd7ff8047c374be9da66efcab7c5b968acb6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-17 16:52:25 -0300

    [:clown_face:] | mock: use-case - emit > comment (module)

---
+++ twitter_clone/test/mock/module/@core/comment/use-case/observable/emit/created.use-case.ts
@@ -0,0 +1,4 @@
+import { EmitCommentUseCase } from '@/app/modules/@core/comment/use-case/observable/emit/created.use-case';
+import { mockDeep } from 'jest-mock-extended';
+
+export const mockEmitCommentUseCase = mockDeep<EmitCommentUseCase>;
