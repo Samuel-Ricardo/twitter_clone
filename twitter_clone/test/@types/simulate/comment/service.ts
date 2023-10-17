f0553caa6da57fb47a8033fffbda5f3e692fd1d5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-17 17:14:53 -0300

    [:label:] | update: ISimulated - service > comment (test/types)

--- twitter_clone/test/@types/simulate/comment/service.ts
+++ twitter_clone/test/@types/simulate/comment/service.ts
@@ -6,6 +6,9 @@ import {
   FindUserCommentsUseCase,
   UpdateCommentUseCase,
 } from '@/app/modules/@core/comment/use-case';
+import { FindCommentByIDUseCase } from '@/app/modules/@core/comment/use-case/find_by_id.use-case';
+import { EmitCommentUseCase } from '@/app/modules/@core/comment/use-case/observable/emit/created.use-case';
+import { ListenCommentUseCase } from '@/app/modules/@core/comment/use-case/observable/listen/created.use-case';
 import { DeepMockProxy } from 'jest-mock-extended';
 
 export interface ISimulatedCommentService {
@@ -16,8 +19,17 @@ export interface ISimulatedCommentService {
     deleteComment: DeepMockProxy<DeleteCommentUseCase>;
     find: {
       by: {
+        id: DeepMockProxy<FindCommentByIDUseCase>;
         post: DeepMockProxy<FindPostCommentsUseCase>;
         author: DeepMockProxy<FindUserCommentsUseCase>;
+      };
+    };
+    observable: {
+      emit: {
+        created: DeepMockProxy<EmitCommentUseCase>;
+      };
+      Listen: {
+        created: DeepMockProxy<ListenCommentUseCase>;
       };
     };
   };
