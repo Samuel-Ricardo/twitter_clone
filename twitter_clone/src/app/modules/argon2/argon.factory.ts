e66fc3c3d26821bc3d6bb1ea61f1879c40479dfc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-18 16:13:14 -0300

    [:lock_with_ink_pen:] | create: factory - argon (module)

---
+++ twitter_clone/src/app/modules/argon2/argon.factory.ts
@@ -0,0 +1,7 @@
+import argon2 from 'argon2';
+import { ARGON_MODULE } from './argon.module';
+import { ARGON_REGISTRY } from './argon.registry';
+
+export const ARGON_FACTORY = {
+  ARGON2: () => ARGON_MODULE.get<typeof argon2>(ARGON_REGISTRY.ARGON2),
+};
