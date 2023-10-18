477d8316bc84f1d242db3715f140889e1fc46a2c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-18 15:11:13 -0300

    [:lock_with_ink_pen:] | implements: compare - turing (module/security)

--- twitter_clone/src/app/modules/security/cryptography/turing.ts
+++ twitter_clone/src/app/modules/security/cryptography/turing.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'inversify';
 import cryptoLib from 'crypto';
+import bcrypt from 'bcrypt';
 import { ICryptographer } from './cryptography.contract';
 import { MODULE } from '../../app.registry';
 
@@ -8,13 +9,23 @@ export class Turing implements ICryptographer {
   constructor(
     @inject(MODULE.CRYPTO)
     private readonly _crypto: typeof cryptoLib,
+    @inject(MODULE.BCRYPT)
+    private readonly _bcrypt: typeof bcrypt,
   ) {}
 
   get crypto() {
     return this._crypto;
   }
 
-  async hash(data: string): Promise<string> {
-    return this._crypto.createHash('sha256').update(data).digest('hex');
+  get bcrypt() {
+    return this._bcrypt;
+  }
+
+  hash(data: string) {
+    return this.bcrypt.hashSync(data, 32);
+  }
+
+  compare(word: string, hash: string) {
+    return this.bcrypt.compareSync(word, hash);
   }
 }
