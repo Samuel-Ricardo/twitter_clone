afb4f6dce514b9f916d9921dfa03f48f01629478
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-19 22:08:43 -0300

    [:closed_lock_with_key:] | update: config - ENV (module)

--- twitter_clone/src/app/modules/config/env/app.env.ts
+++ twitter_clone/src/app/modules/config/env/app.env.ts
@@ -3,11 +3,27 @@ export const ENV = {
 
   API: {
     URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3004',
+    SOCKET: {
+      URL: process.env.NEXT_PUBLIC_API_SOCKET_URL || 'http://localhost:3004',
+    },
+  },
+
+  JWT: {
+    SECRET: process.env.NEXT_PUBLIC_JWT_SECRET || '',
   },
 
   NEXT: {
     API: {
       URL: process.env.NEXT_PUBLIC_NEXT_API_URL || 'http://localhost:3000',
+    },
+    AUTH: {
+      SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET || '',
+    },
+  },
+  SECURITY: {
+    CYPHER: {
+      IV: process.env.NEXT_PUBLIC_SECURITY_CYPHER_IV || '',
+      KEY: process.env.NEXT_PUBLIC_SECURITY_CYPHER_KEY || '',
     },
   },
 };
