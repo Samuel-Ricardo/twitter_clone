63f624788a372e066eed85c8f0ce4f6412824fbc
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-19 22:37:37 -0300

    [:lock_with_ink_pen:] | implements: encrypt - turing (module/security)

--- twitter_clone/src/app/modules/security/cryptography/turing.ts
+++ twitter_clone/src/app/modules/security/cryptography/turing.ts
@@ -1,18 +1,46 @@
 import { inject, injectable } from 'inversify';
 import cryptoLib from 'crypto';
-import bcrypt from 'bcrypt';
 import { ICryptographer } from './cryptography.contract';
 import { MODULE } from '../../app.registry';
 import argon2 from 'argon2';
+import { injectConfig } from '../../config/config.module';
 
 @injectable()
 export class Turing implements ICryptographer {
+  @injectConfig(MODULE.CONFIG.SECURITY.CYPHER.ALGORITHMS.AES[256].GCM)
+  private readonly _algorithm: string;
+  @injectConfig(MODULE.CONFIG.SECURITY.CYPHER.IV)
+  private readonly _iv: string;
+  @injectConfig(MODULE.CONFIG.SECURITY.CYPHER.KEY)
+  private readonly _key: string;
+
   constructor(
     @inject(MODULE.CRYPTO)
     private readonly _crypto: typeof cryptoLib,
     @inject(MODULE.ARGON2)
     private readonly _argon: typeof argon2,
   ) {}
+
+  hash(data: string) {
+    return this.argon.hash(data, { type: argon2.argon2id });
+  }
+
+  compare(word: string, hash: string) {
+    return this.argon.verify(word, hash, { type: argon2.argon2id });
+  }
+
+  encrypt(word: string) {
+    return this.cypheriv.update(word, 'utf8', 'hex');
+  }
+
+  decrypt(word: string) {
+    return this.cypheriv.update(word, 'hex', 'utf8');
+  }
+
+  get cypheriv() {
+    const { algorithm, key, iv } = this;
+    return this.crypto.createCipheriv(algorithm, key, iv);
+  }
 
   get crypto() {
     return this._crypto;
@@ -22,11 +50,15 @@ export class Turing implements ICryptographer {
     return this._argon;
   }
 
-  hash(data: string) {
-    return this.argon.hash(data, { type: argon2.argon2id });
+  get algorithm() {
+    return this._algorithm;
   }
 
-  compare(word: string, hash: string) {
-    return this.argon.verify(word, hash, { type: argon2.argon2id });
+  get iv() {
+    return this._iv;
+  }
+
+  get key() {
+    return this._key;
   }
 }
