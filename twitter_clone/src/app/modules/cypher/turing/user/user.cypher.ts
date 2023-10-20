97788aa7013ae92edce74d115425df7a238c7b95
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-20 10:08:43 -0300

    [:lock_with_ink_pen:] | implements: IUserCypher - turing > hash (module)

--- twitter_clone/src/app/modules/cypher/turing/user/user.cypher.ts
+++ twitter_clone/src/app/modules/cypher/turing/user/user.cypher.ts
@@ -1,11 +1,19 @@
 import { IUserCypher } from '@/app/modules/@core/user/cypher/user.cypher';
+import { MODULE } from '@/app/modules/app.registry';
 import { ICryptographer } from '@/app/modules/security/cryptography/cryptography.contract';
 import { Turing } from '@/app/modules/security/cryptography/turing';
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
 
 @injectable()
 export class TuringUserCypher implements IUserCypher {
-  constructor(private readonly _turing: Turing | ICryptographer) {}
+  constructor(
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    private readonly _turing: Turing | ICryptographer,
+  ) {}
+
+  hashPassword(password: string) {
+    return this.turing.hash(password);
+  }
 
   get turing() {
     return this._turing;
