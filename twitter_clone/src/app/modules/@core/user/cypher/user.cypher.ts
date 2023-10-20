be48e4b1b781a9a1909c1012e974c73ed3c925be
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-20 10:27:16 -0300

    [:lock_with_ink_pen:] | update: cypher - user (module)

--- twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
+++ twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
@@ -1,12 +1,13 @@
+import { IEncriptedIv } from '@/app/@types/security/cryptographer/encriptedIv';
 import { IUserDTO } from '../DTO';
 
 export interface IUserCypher {
-  hashPassword(password: string): Promise<string>;
-  comparePassword(password: string, hash: string): Promise<boolean>;
+  hashPassword(password: string): string | Promise<string>;
+  comparePassword(password: string, hash: string): string | Promise<boolean>;
 
-  encryptUserId(id: string): Promise<string>;
-  decryptUserId(id: string): Promise<string>;
+  encryptIvUserId(id: string): IEncriptedIv | Promise<IEncriptedIv>;
+  decryptIvUserId(secret: IEncriptedIv): string | Promise<string>;
 
-  encryptUser(user: IUserDTO): Promise<IUserDTO>;
-  decryptUser(user: IUserDTO): Promise<IUserDTO>;
+  encryptIvUser(user: IUserDTO): IEncriptedIv | Promise<IEncriptedIv>;
+  decryptIvUser(user: IEncriptedIv): IUserDTO | Promise<IUserDTO>;
 }
