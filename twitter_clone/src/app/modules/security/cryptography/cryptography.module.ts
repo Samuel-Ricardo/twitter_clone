0e3c01ab9adbd7deb037035932d17c6a3f1fe107
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-20 00:36:29 -0300

    [:lock_with_ink_pen:] | create: module - cryptography (module)

---
+++ twitter_clone/src/app/modules/security/cryptography/cryptography.module.ts
@@ -0,0 +1,19 @@
+import { Container } from 'inversify';
+import { CRYPTO_MODULE } from '../../crypto/crypto.module';
+import { ARGON_MODULE } from '../../argon2/argon.module';
+import { CRYPTOGRAPHY_REGISTRY } from './cryptography.registry';
+import { Turing } from './turing';
+import getDecorators from 'inversify-inject-decorators';
+
+const MODULE = new Container({ autoBindInjectable: true });
+
+export const CRYPTOGRAPHY_MODULE = Container.merge(
+  MODULE,
+  CRYPTO_MODULE,
+  ARGON_MODULE,
+);
+
+CRYPTOGRAPHY_MODULE.bind(CRYPTOGRAPHY_REGISTRY.TURING).to(Turing);
+
+export const { lazyInject: injectCryptographer } =
+  getDecorators(CRYPTOGRAPHY_MODULE);
