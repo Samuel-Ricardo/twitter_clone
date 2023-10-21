58bd4b8495cfbd84dd5335525f4a9203137c501a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-20 22:04:49 -0300

    [:lock_with_ink_pen:] | update: cypher - user (module)

--- twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
+++ twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
@@ -1,13 +1,43 @@
 import { IEncriptedIv } from '@/app/@types/security/cryptographer/encriptedIv';
-import { IUserDTO } from '../DTO';
+import {
+  ICreateUserDTO,
+  IDeleteuserDTO,
+  ISelectUserByIdDTO,
+  IUpdateUserDTO,
+  IUserDTO,
+} from '../DTO';
+import { ISelectUserByEmailDTO } from '../DTO/select_by_email.dto';
+import { ISelectUserByCredentialsDTO } from '../DTO/select_by_credentials.dto';
 
 export interface IUserCypher {
   hashPassword(password: string): string | Promise<string>;
   comparePassword(password: string, hash: string): string | Promise<boolean>;
 
-  encryptIvUserId(id: string): IEncriptedIv | Promise<IEncriptedIv>;
-  decryptIvUserId(secret: IEncriptedIv): string | Promise<string>;
+  encryptIvUserId(id: string): IEncriptedIv;
+  decryptIvUserId(secret: IEncriptedIv): string;
 
-  encryptIvUser(user: IUserDTO): IEncriptedIv | Promise<IEncriptedIv>;
-  decryptIvUser(user: IEncriptedIv): IUserDTO | Promise<IUserDTO>;
+  encryptIvUser(user: IUserDTO): IEncriptedIv;
+  decryptIvUser(user: IEncriptedIv): IUserDTO;
+
+  enecryptIvCreateDTO(user: ICreateUserDTO): IEncriptedIv;
+  decryptIvCreateDTO(user: IEncriptedIv): ICreateUserDTO;
+
+  enecryptIvUpdateDTO(user: IUpdateUserDTO): IEncriptedIv;
+  decryptIvUpdateDTO(user: IEncriptedIv): IUpdateUserDTO;
+
+  encryptIvDeleteDTO(user: IDeleteuserDTO): IEncriptedIv;
+  decryptIvDeleteDTO(user: IEncriptedIv): IDeleteuserDTO;
+
+  encryptIvSelectByIdDTO(user: ISelectUserByIdDTO): IEncriptedIv;
+  decryptIvSelectByIdDTO(user: IEncriptedIv): ISelectUserByIdDTO;
+
+  encryptIvSelectByEmailDTO(user: ISelectUserByEmailDTO): IEncriptedIv;
+  decryptIvSelectByEmailDTO(user: IEncriptedIv): ISelectUserByEmailDTO;
+
+  encryptIvSelectByCredentialsDTO(
+    user: ISelectUserByCredentialsDTO,
+  ): IEncriptedIv;
+  decryptIvSelectByCredentialsDTO(
+    user: IEncriptedIv,
+  ): ISelectUserByCredentialsDTO;
 }
