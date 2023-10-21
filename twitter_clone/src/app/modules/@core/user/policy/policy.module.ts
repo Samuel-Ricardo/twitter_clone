bf9bfad2129401146715c9692456c4b32354a002
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-20 23:48:35 -0300

    [:scroll:] | update: module - policy (module/user)

--- twitter_clone/src/app/modules/@core/user/policy/policy.module.ts
+++ twitter_clone/src/app/modules/@core/user/policy/policy.module.ts
@@ -1,16 +1,27 @@
 import { Container } from 'inversify';
 import { USER_POLICY_REGISTRY } from './policy.registry';
-import { EncryptUserDataPolicy } from './security/encrypt/data.policy';
-import { DecryptUserDataPolicy } from './security/decrypt/data.policy';
+import { EncryptUserDataPolicy } from './security/encrypt/user.policy';
+import { DecryptUserDataPolicy } from './security/decrypt/user.policy';
 import getDecorators from 'inversify-inject-decorators';
+import { EncryptCreateUserDataPolicy } from './security/encrypt/create.policy';
+import { DecryptCreateUserDataPolicy } from './security/decrypt/create.policy';
 
 export const USER_POLICY_MODULE = new Container({ autoBindInjectable: true });
 
 USER_POLICY_MODULE.bind(USER_POLICY_REGISTRY.SECURITY.ENCRYPT.DATA).to(
   EncryptUserDataPolicy,
 );
+
+USER_POLICY_MODULE.bind(USER_POLICY_REGISTRY.SECURITY.ENCRYPT.CREATE).to(
+  EncryptCreateUserDataPolicy,
+);
+
 USER_POLICY_MODULE.bind(USER_POLICY_REGISTRY.SECURITY.DECRYPT.DATA).to(
   DecryptUserDataPolicy,
+);
+
+USER_POLICY_MODULE.bind(USER_POLICY_REGISTRY.SECURITY.DECRYPT.CREATE).to(
+  DecryptCreateUserDataPolicy,
 );
 
 export const { lazyInject: injectUserPolicy } =
--- twitter_clone/src/app/modules/@core/user/policy/security/decrypt/create.policy.ts
+++ twitter_clone/src/app/modules/@core/user/policy/security/decrypt/create.policy.ts
@@ -1,7 +1,7 @@
 import { IEncriptedIv } from '@/app/@types/security/cryptographer/encriptedIv';
 import { UserCypherAccess } from '../../../cypher/cypher.access';
 
-export class DecryptUserDataPolicy extends UserCypherAccess {
+export class DecryptCreateUserDataPolicy extends UserCypherAccess {
   execute(user: IEncriptedIv) {
     return this._cypher.decryptIvCreateDTO(user);
   }
