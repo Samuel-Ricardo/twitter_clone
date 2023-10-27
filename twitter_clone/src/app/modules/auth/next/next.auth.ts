c0e7a08782a26fd0dd015dd5e16a4a35d732934f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-10-27 10:38:23 -0300

    [:passport_control:] | create: module - auth > next (module)

--- twitter_clone/src/app/modules/auth/next/next.auth.ts
+++ twitter_clone/src/app/modules/auth/next/next.auth.ts
@@ -1,39 +1,45 @@
 import { AuthOptions } from 'next-auth';
 import CredetialProvider from 'next-auth/providers/credentials';
-import { MODULES } from '../..';
 import { ENV } from '../../config/env/app.env';
+import { interfaces } from 'inversify';
+import { MODULE } from '../../app.registry';
+import { UserController } from '../../@core/user/controller';
 
-export const authOptions: AuthOptions = {
-  providers: [
-    CredetialProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password)
-          throw new Error('Invalid credentials');
+export const authOptions = ({ container }: interfaces.Context): AuthOptions => {
+  const USER = container.get<UserController>(MODULE.USER.MAIN);
 
-        const result = await MODULES.USER.MAIN().selectByCredentials({
-          email: credentials.email,
-          password: credentials.password,
-        });
+  return {
+    providers: [
+      CredetialProvider({
+        name: 'Credentials',
+        credentials: {
+          email: { label: 'email', type: 'text' },
+          password: { label: 'password', type: 'password' },
+        },
+        async authorize(credentials) {
+          if (!credentials?.email || !credentials?.password)
+            throw new Error('Invalid credentials');
 
-        if (!result) throw new Error('Invalid credentials');
+          const result = await USER.selectByCredentials({
+            email: credentials.email,
+            password: credentials.password,
+          });
 
-        return result.user;
-      },
-    }),
-  ],
-  debug: ENV.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  jwt: {
-    secret: ENV.JWT.SECRET,
-  },
-  secret: ENV.NEXT.AUTH.SECRET,
+          if (!result) throw new Error('Invalid credentials');
+
+          return result.user;
+        },
+      }),
+    ],
+    debug: ENV.NODE_ENV === 'development',
+    session: {
+      strategy: 'jwt',
+    },
+    jwt: {
+      secret: ENV.JWT.SECRET,
+    },
+    secret: ENV.NEXT.AUTH.SECRET,
+  };
 };
 
 //export const NEXT_AUTH_HANDLER = NextAuth(authOptions);
