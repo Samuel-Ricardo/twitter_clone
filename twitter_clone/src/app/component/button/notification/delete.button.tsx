caabd4eda9ab0b060fca012dac59312b83fd66b0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:38:34 -0300

    [:art:] | update: button - delete > notification (app)

--- twitter_clone/src/app/component/button/notification/delete.button.tsx
+++ twitter_clone/src/app/component/button/notification/delete.button.tsx
@@ -1,3 +1,4 @@
+import { MODULES } from '@/app/modules';
 import { IoMdClose } from 'react-icons/io';
 
 export const DeleteNotificationButton = (
@@ -6,8 +7,9 @@ export const DeleteNotificationButton = (
   return (
     <div
       {...props}
+      onClick={handleClick}
       className={
-        'rounded-full h-14 w-14 p-4 flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out hover:bg-red-300 ' +
+        'rounded-full h-14 w-14 p-4 flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-200 ease-in-out hover:bg-red-300 hover:mt-6 sm:hover:mt-auto ' +
         props.className
       }
     >
