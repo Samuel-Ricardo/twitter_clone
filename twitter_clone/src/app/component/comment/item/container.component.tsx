3871feb22f911e5b8e5395802ca01cb163951817
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:42:37 -0300

    [:art:] | update: style - comment > item (app)

--- twitter_clone/src/app/component/comment/item/container.component.tsx
+++ twitter_clone/src/app/component/comment/item/container.component.tsx
@@ -3,8 +3,8 @@ export const CommentItemContainer = ({
 }: {
   children: React.ReactNode;
 }) => (
-  <div className="flex flex-col flex-1  bg-gradient-to-br from-gray-100/80 to-gray-400/40 transition-all duration-1000 ease-in-out hover:opacity-90 hover:scale-110 hover:my-8 hover:from-gray-400/40 hover:to-gray-100/80 rounded-xl">
-    <div className=" rounded-xl flex flex-row flex-1 bg-gray-300/70 transparent transition-all duration-1000 ease-in-out hover:bg-transparent p-2">
+  <div className="flex flex-col flex-1  bg-gradient-to-tl from-gray-100/80 to-gray-400/40 transition-all duration-1000 ease-in-out hover:opacity-90 hover:scale-110 hover:my-8 hover:bg-gradient-to-br rounded-xl">
+    <div className=" rounded-xl flex flex-row flex-1 bg-gray-300/60 transparent transition-all duration-1000 ease-in-out hover:bg-transparent p-2">
       {children}
     </div>
   </div>
