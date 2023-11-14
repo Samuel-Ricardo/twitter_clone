20ad4f80aacc7bfcb623f0df97c429b9fe4481cf
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 11:52:32 -0300

    [:clown_face:] | simulate: gateway - axios > follow (mock/module)

--- twitter_clone/test/mock/module/gateway/axios/follow/follow.gateway.ts
+++ twitter_clone/test/mock/module/gateway/axios/follow/follow.gateway.ts
@@ -1,3 +1,4 @@
+import { MODULES } from '@/app/modules';
 import { AxiosFollowGateway } from '@/app/modules/gateway/axios/follow/follow.gateway';
 import { ISimulatedFollowGateway } from '@test/@types/simulate/follow/gateway';
 import { MODULE_MOCK } from '@test/mock/module/app.registry';
@@ -16,7 +17,10 @@ export const simulateAxiosFollowGateway = ({
   const client = container.get<any>(MODULE_MOCK.AXIOS.MOCK);
 
   return {
-    gateway: new AxiosFollowGateway('http://localhost:3004', client),
+    gateway: new AxiosFollowGateway(
+      client,
+      MODULES.SECURITY.CRYPTOGRAPHY.TURING(),
+    ),
     client,
   };
 };
