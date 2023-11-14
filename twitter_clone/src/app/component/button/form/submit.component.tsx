23de43e70708d320a744d633c1ceb91e846a92b2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 11:26:57 -0300

    [:sparkles:] | create: button - submit (app)

---
+++ twitter_clone/src/app/component/button/form/submit.component.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { ISubmitIconProps } from '@/app/@types/props/submit/icon';
+import { useRef } from 'react';
+import { BiExit } from 'react-icons/bi';
+
+export const SubmitIcon = (props: ISubmitIconProps) => {
+  const submitRef = useRef<HTMLInputElement>(null);
+  return (
+    <>
+      <BiExit
+        {...props}
+        size={30}
+        onClick={() => submitRef.current?.click()}
+        className={
+          'text-black self-end mr-4 cursor-pointer transition hover:text-cyan-300 hover:scale-105 ' +
+          props.className
+        }
+      />
+
+      <input type="submit" ref={submitRef} hidden />
+    </>
+  );
+};
