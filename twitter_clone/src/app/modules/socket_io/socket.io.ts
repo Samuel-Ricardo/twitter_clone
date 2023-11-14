2d8d7777d9520a3b1b024a21e1d4ae8264a7a779
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 14:59:40 -0300

    [:electric_plug:] | update: global - socket.io (module)

--- twitter_clone/src/app/modules/socket_io/socket.io.ts
+++ twitter_clone/src/app/modules/socket_io/socket.io.ts
@@ -5,6 +5,7 @@ const globalSocketIO = globalThis as unknown as {
   socketIO: Socket | undefined;
 };
 
-export const socketIO = globalSocketIO.socketIO || io(ENV.API.SOCKET.URL);
+export const socketIO =
+  globalSocketIO.socketIO || io(ENV.API.SOCKET.URL, { autoConnect: false });
 
 globalSocketIO.socketIO = socketIO;
