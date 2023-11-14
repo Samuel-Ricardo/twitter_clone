8010cf0aa540cd6949d4389a078f4811819c8eb0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:59:47 -0300

    [:lock_with_ink_pen:] | encrpyt: gateway - http (module/axios)

--- twitter_clone/src/app/modules/gateway/axios/generic/http.gateway.ts
+++ twitter_clone/src/app/modules/gateway/axios/generic/http.gateway.ts
@@ -4,14 +4,19 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { ISWRSupport } from '../../support/swr.support';
 import swr from 'swr';
 import { MODULE } from '@/app/modules/app.registry';
+import { CONFIG } from '@/app/modules/config/app.config';
+import { GlobalUser } from '@/app/global/user.global';
+import { type ICryptographer } from '@/app/modules/security/cryptography/cryptography.contract';
 
 @injectable()
 export class AxiosHTTPGateway implements IHTTPGateway, ISWRSupport {
   constructor(
-    @inject(MODULE.CONFIG.API.URL)
-    public readonly URL: string,
     @inject(MODULE.AXIOS)
     protected readonly client: typeof axios,
+    @inject(MODULE.SECURITY.CRYPTOGRAPHY.TURING)
+    protected readonly cypher: ICryptographer,
+    //@inject(MODULE.CONFIG.API.URL)
+    public readonly URL: string = CONFIG.API.URL,
     protected readonly useSWR = swr,
   ) {
     this.setup();
@@ -19,26 +24,55 @@ export class AxiosHTTPGateway implements IHTTPGateway, ISWRSupport {
 
   async setup() {
     this.client.defaults.baseURL = this.URL;
+    this.client.defaults.headers.authorization = `Bearer ${this.userToken}`;
   }
 
   fetcher = async (url: string) => {
-    return (await this.client.get(url)).data;
+    let { data } = await this.client.get(url);
+
+    return await this.decrypt(data);
   };
 
   async get<T>(path: string, config?: AxiosRequestConfig) {
-    return await this.client.get<T>(`${this.URL}/${path}`, config);
+    const response = await this.client.get<T>(`${this.URL}/${path}`, config);
+    response.data = await this.decrypt(response.data);
+
+    return response;
   }
 
   async post<T>(path: string, body: any, config?: AxiosRequestConfig) {
-    return await this.client.post<T>(`${this.URL}/${path}`, body, config);
+    const response = await this.client.post<T>(
+      `${this.URL}/${path}`,
+      this.encrypt(body),
+      config,
+    );
+    response.data = await this.decrypt(response.data);
+
+    console.log({ response });
+
+    return response;
   }
 
   async put<T>(path: string, body: any, config?: AxiosRequestConfig) {
-    return await this.client.put<T>(`${this.URL}/${path}`, body, config);
+    const response = await this.client.put<T>(
+      `${this.URL}/${path}`,
+      body,
+      config,
+    );
+    response.data = await this.decrypt(response.data);
+
+    return response;
   }
 
   async patch<T>(path: string, body: any, config?: AxiosRequestConfig) {
-    return await this.client.patch<T>(`${this.URL}/${path}`, body, config);
+    const response = await this.client.patch<T>(
+      `${this.URL}/${path}`,
+      body,
+      config,
+    );
+    response.data = await this.decrypt(response.data);
+
+    return response;
   }
 
   async delete<T>(path: string, config?: AxiosRequestConfig) {
