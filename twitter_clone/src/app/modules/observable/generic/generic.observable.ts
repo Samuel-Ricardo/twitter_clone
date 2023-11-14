770e6859021888279cfa8a55507f69578f92e0d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:40:04 -0300

    [:eyes:] | update: interface - observable (module)

--- twitter_clone/src/app/modules/observable/generic/generic.observable.ts
+++ twitter_clone/src/app/modules/observable/generic/generic.observable.ts
@@ -1,5 +1,7 @@
+import { GenericAction } from '@/app/@types/event/action/generic';
+
 export interface IObservable {
-  listen(event: string, action: (...data: any) => any): any | void;
-  stopListening(event: string): any | void;
+  listen(event: string, action: GenericAction): any | void;
+  stopListening(event: string, action?: GenericAction): any | void;
   emit(event: string, data: any): any | void;
 }
