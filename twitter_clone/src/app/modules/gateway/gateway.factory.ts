9b1f6b7bfed28099109ecf95c608aa3aa71eb769
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:55:09 -0300

    [:electric_plug:] | update: factory - gateway (module)

--- twitter_clone/src/app/modules/gateway/gateway.factory.ts
+++ twitter_clone/src/app/modules/gateway/gateway.factory.ts
@@ -1,5 +1,7 @@
 import { AxiosGatewayFactory as AXIOS } from './axios/axios.factory';
+import { REACTIVE_GATEWAY_FACTORY as REACTIVE } from './reactive/reactive.factory';
 
 export const GatewayFactory = {
   AXIOS,
+  REACTIVE,
 };
