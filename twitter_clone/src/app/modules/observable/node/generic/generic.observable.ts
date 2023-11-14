58ad69f60bdc15341a4d756b6cd6e9d75ce5527a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:01:49 -0300

    [:eyes:] | update: observable - generic (module)

--- twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
+++ twitter_clone/src/app/modules/observable/node/generic/generic.observable.ts
@@ -2,25 +2,34 @@ import { inject, injectable } from 'inversify';
 import { IObservable } from '../../generic/generic.observable';
 import { MODULE } from '@/app/modules/app.registry';
 import { GenericAction } from '@/app/@types/event/action/generic';
+import { EVENT } from '@/app/modules/config/event';
 
 @injectable()
 export class NodeObservable implements IObservable {
   constructor(
     @inject(MODULE.EVENT.EMITER[2])
     protected readonly _events: EventEmitter2,
+    @inject(MODULE.CONFIG.EVENT)
+    protected readonly _EVENTS: typeof EVENT,
   ) {}
 
   listen(event: string, action: GenericAction) {
     this._events.on(event, action);
+    console.log({ LISTEN: this.events }, { EVENT: event }, { ACTION: action });
   }
   stopListening(event: string, action: GenericAction) {
     this._events.off(event, action);
   }
   emit(event: string, data: any) {
     this._events.emit(event, data);
+    console.log({ EMIT: this.events }, { EVENT: event }, { DATA: data });
   }
 
   get events() {
     return this._events;
+  }
+
+  get EVENTS() {
+    return this._EVENTS;
   }
 }
