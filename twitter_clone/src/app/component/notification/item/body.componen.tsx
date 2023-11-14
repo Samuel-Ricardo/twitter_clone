fdfbe6d21d2f706175a0fbc5f29da664f9d5ff15
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 11:23:34 -0300

    [:sparkles:] | create: component - notification > item (app)

---
+++ twitter_clone/src/app/component/notification/item/body.componen.tsx
@@ -0,0 +1,28 @@
+import { INotificationItemBodyProps } from '@/app/@types/props/notification/item_body';
+import { FOLLOW, POST } from '@/app/modules/@core/notification/entity';
+import { format } from 'date-fns';
+import { useRouter } from 'next/navigation';
+import { useCallback, useMemo } from 'react';
+import { BsCheck2All } from 'react-icons/bs';
+
+export const NotificationItemBody = ({
+  notification,
+  createdAt,
+  visualizedAt,
+}: INotificationItemBodyProps) => {
+  return (
+    <div onClick={onClick} className="flex flex-1">
+      <p className="truncate">{notification?.body}</p>
+
+      <div className="flex flex-row gap-2 flex-1 items-end justify-end -mb-8">
+        <p className="text-neutral-400 text-sm">{date}</p>
+        <BsCheck2All
+          size={24}
+          className={`self-end ${
+            visualizedAt ? 'text-cyan-300' : 'text-gray-400'
+          }`}
+        />
+      </div>
+    </div>
+  );
+};
