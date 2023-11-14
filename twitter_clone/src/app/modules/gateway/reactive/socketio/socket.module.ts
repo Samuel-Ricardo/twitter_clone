5572e97455acad1306927f2a54a3a69fac087201
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:43:27 -0300

    [:sparkles:] | update: module - gateway > reactive (module)

--- twitter_clone/src/app/modules/gateway/reactive/socketio/socket.module.ts
+++ twitter_clone/src/app/modules/gateway/reactive/socketio/socket.module.ts
@@ -1,18 +1,16 @@
-import { ConfigModule } from '@/app/modules/config/config.module';
-import { SOCKET_MODULE } from '@/app/modules/socket_io/socket.module';
 import { Container } from 'inversify';
 import { REACTIVE_SOCKET_GATEWAY_REGISTRY } from './socket.registry';
 import { SocketIONotificationGateway } from './notification/notification.gateway';
+import { REACTIVE_SOCKET_MODULE } from '@/app/modules/reactive/socket/socket.module';
 
 const MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-const REACTIVE_SOCKET_GATEWAY_MODULE = Container.merge(
+export const REACTIVE_SOCKET_GATEWAY_MODULE = Container.merge(
   MODULE,
-  SOCKET_MODULE,
-  ConfigModule,
+  REACTIVE_SOCKET_MODULE,
 );
 
 REACTIVE_SOCKET_GATEWAY_MODULE.bind(
