8c3c3571a69515868745f56c7bdd7b7cfa804323
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:54:02 -0300

    [:electric_plug:] | update: interface - WebsocketGateway (module/gateway)

--- twitter_clone/src/app/modules/gateway/generic/websocket.gateway.ts
+++ twitter_clone/src/app/modules/gateway/generic/websocket.gateway.ts
@@ -2,12 +2,9 @@ import { ISubscribeSocketData } from '@/app/@types/gateway/reactive/websocket/li
 import { IPublishSocketData } from '@/app/@types/gateway/reactive/websocket/publish';
 
 export interface WebSocketGateway<S> {
-  readonly _URL: string;
-  readonly _socket: S;
-
   get URL(): string;
   get socket(): S;
 
-  publish(payload: IPublishSocketData): any | Promise<any>;
+  publish<D>(payload: IPublishSocketData<D>): any | Promise<any>;
   subscribe(scheduled: ISubscribeSocketData): any | Promise<any>;
 }
