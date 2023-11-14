4ad67924cc06aa50357219cc2126fd727f8b01c9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 15:54:40 -0300

    [:lock_with_ink_pen:] | update: module - cryptography (module/security)

--- twitter_clone/src/app/modules/security/cryptography/cryptography.module.ts
+++ twitter_clone/src/app/modules/security/cryptography/cryptography.module.ts
@@ -1,16 +1,18 @@
 import { Container } from 'inversify';
 import { CRYPTO_MODULE } from '../../crypto/crypto.module';
-import { ARGON_MODULE } from '../../argon2/argon.module';
+//import { ARGON_MODULE } from '../../argon2/argon.module';
 import { CRYPTOGRAPHY_REGISTRY } from './cryptography.registry';
 import { Turing } from './turing';
 import getDecorators from 'inversify-inject-decorators';
+import { BCRYPT_MODULE } from '../../bcrypt/bcrypt.module';
 
 const MODULE = new Container({ autoBindInjectable: true });
 
 export const CRYPTOGRAPHY_MODULE = Container.merge(
   MODULE,
   CRYPTO_MODULE,
-  ARGON_MODULE,
+  BCRYPT_MODULE,
+  //  ARGON_MODULE,
 );
 
 CRYPTOGRAPHY_MODULE.bind(CRYPTOGRAPHY_REGISTRY.TURING).to(Turing);
