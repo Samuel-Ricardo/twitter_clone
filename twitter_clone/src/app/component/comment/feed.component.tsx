6b6580ccb2d2dd50c71344edf4cbaa3970f0f2b2
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:41:31 -0300

    [:recycle:] | refact: clean

--- twitter_clone/src/app/component/comment/feed.component.tsx
+++ twitter_clone/src/app/component/comment/feed.component.tsx
@@ -1,19 +1,9 @@
 import { ICommentFeedProps } from '@/app/@types/props/comment/feed';
 import { useTweetComments } from '@/app/hooks/comment/post.hook';
 import { CommentItem } from './item.component';
-import { useCurrentUser } from '@/app/hooks/user/current.hook';
 
 export const CommentFeed = ({ tweetId }: ICommentFeedProps) => {
   const { comments } = useTweetComments({ postId: tweetId });
-
-  comments.push({
-    id: '1',
-    body: 'test',
-    postId: tweetId,
-    authorId: useCurrentUser().currentUser?.id || '',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
 
   return (
     <div className="flex flex-col flex-1 gap-4 mt-10 mb-36">
