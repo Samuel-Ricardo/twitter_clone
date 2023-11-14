3d793e420388776ef0acb72c5d80b5c643dacfd9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:10:16 -0300

    [:triangular_ruler:] | update: layout - root (app)

--- twitter_clone/src/app/layout.tsx
+++ twitter_clone/src/app/layout.tsx
@@ -1,10 +1,14 @@
+import 'reflect-metadata';
+
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { BaseLayout } from './page/layout/base.layout';
 import { Sidebar } from './component/section/sidebar/sidebar.component';
 import { FollowBar } from './component/section/followbar/followbar.component';
-import { MODULES } from './modules';
+import { NotificationModule } from './component/notification/module.component';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +22,6 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  //  MODULES.NOTIFICATION.REACTIVE();
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -28,9 +31,9 @@ export default async function RootLayout({
           {/* </div> */}
 
           {/* <div className="h-full ml-[18vw] w-full mr-[30vw] z-50"> */}
-          {children}
+          <NotificationModule />
+          <Suspense fallback={<Loading />}>{children}</Suspense>
           {/* </div> */}
-
           {/* <div className="fixed right-0 top-0 h-screen w-fit"> */}
           <FollowBar />
           {/* </div>  */}
