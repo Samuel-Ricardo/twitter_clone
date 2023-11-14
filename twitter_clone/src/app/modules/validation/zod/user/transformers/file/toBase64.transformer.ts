1af943bb764b39ca2d3deaa9592eae564363ab78
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 00:30:04 -0300

    [:safety_vest:] | create: transformer - toBase64 > zod (module/validation)

---
+++ twitter_clone/src/app/modules/validation/zod/user/transformers/file/toBase64.transformer.ts
@@ -0,0 +1,12 @@
+export const toBase64 = (file?: File | null | string) => {
+  return new Promise((resolve, reject) => {
+    if (!file || typeof file === 'string') return resolve(null);
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => resolve(event.target?.result);
+    reader.onerror = (error) => reject(error);
+
+    reader.readAsDataURL(file);
+  });
+};
