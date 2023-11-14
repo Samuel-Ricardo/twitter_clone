841ca157f0c77082a00dabbfb768cded363ac2b1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:34:23 -0300

    [:recycle:] | refact: router - auth (api)

--- twitter_clone/src/app/api/auth/[...nextauth]/route.ts
+++ twitter_clone/src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,6 @@
 import { MODULES } from '@/app/modules';
-import { ENV } from '@/app/modules/config/env/app.env';
-import NextAuth, { AuthOptions } from 'next-auth';
-import CredetialProvider from 'next-auth/providers/credentials';
+import NextAuth from 'next-auth';
 
-export const authOptions: AuthOptions = {
-  providers: [
-    CredetialProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password)
-          throw new Error('Invalid credentials');
+const handler = NextAuth(MODULES.AUTH.NEXT.OPTIONS());
 
-        const result = await MODULES.USER.MAIN().selectByCredentials({
-          email: credentials.email,
-          password: credentials.password,
-        });
-
-        if (!result) throw new Error('Invalid credentials');
-
-        return result.user;
-      },
-    }),
-  ],
-  debug: ENV.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  jwt: {
-    secret: ENV.JWT.SECRET,
-  },
-  secret: ENV.NEXT.AUTH.SECRET,
-};
-
-export default NextAuth(authOptions);
+export { handler as GET, handler as POST };
