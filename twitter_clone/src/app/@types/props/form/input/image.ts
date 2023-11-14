906540e0cbcc61bb996d33cf3ecb515a8b294606
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:29:31 -0300

    [:label:] | update: props - image upload (@types)

--- twitter_clone/src/app/@types/props/form/input/image.ts
+++ twitter_clone/src/app/@types/props/form/input/image.ts
@@ -1,7 +1,10 @@
-import { IInputProps } from '../../input';
-
-export interface IImageUploadProps extends IInputProps {
-  onChange: (data: { file: File; base64: string }) => void;
+export interface IImageUploadProps {
+  onChange: (data: { file?: File | null; base64: string }) => void;
+  label: string;
+  className?: string;
   image: string;
   disabled?: boolean;
+  large?: boolean;
+  reactHook: { registry: () => void | any };
+  errors?: string;
 }
