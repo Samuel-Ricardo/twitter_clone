b7e7f9a054ad51db1daefe51cbb022439eee784f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 16:54:40 -0300

    [:electric_plug:] | update: module - gateway (module)

--- twitter_clone/src/app/modules/gateway/gateway.module.ts
+++ twitter_clone/src/app/modules/gateway/gateway.module.ts
@@ -1,8 +1,7 @@
 import { Container } from 'inversify';
 import { AxiosGatewayModule } from './axios/axios.module';
 import { REACTIVE_GATEWAY_MODULE } from './reactive/reactive.module';
-import { ConfigRegistry } from '../config/config.registry';
-import { CONFIG } from '../config/app.config';
+import getDecorators from 'inversify-inject-decorators';
 
 const Module = new Container({ autoBindInjectable: true });
 
@@ -12,4 +11,6 @@ export const GatewayModule = Container.merge(
   REACTIVE_GATEWAY_MODULE,
 );
 
-GatewayModule.rebind(ConfigRegistry.API.URL).toConstantValue(CONFIG.API.URL);
+//GatewayModule.rebind(ConfigRegistry.API.URL).toConstantValue(CONFIG.API.URL);
+
+export const { lazyInject: injectGateway } = getDecorators(GatewayModule);
