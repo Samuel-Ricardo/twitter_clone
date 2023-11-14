9e3f79d2c302cd871412127b49995fa26cfef9c9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:13:28 -0300

    [:label:] | update: props - image uploader > container (@types)

--- twitter_clone/src/app/@types/props/form/input/image.container.ts
+++ twitter_clone/src/app/@types/props/form/input/image.container.ts
@@ -1,3 +1,6 @@
+import { DropzoneRootProps } from 'react-dropzone';
 import { IInputContainerProps } from '../../inputContainer';
 
-export interface IImageUploadContainerProps extends IInputContainerProps {}
+export interface IImageUploadContainerProps extends IInputContainerProps {
+  dropzone: <T extends DropzoneRootProps>(props: T) => any;
+}
