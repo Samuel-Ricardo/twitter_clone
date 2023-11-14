83edc3ffaaf7ef3beb7b154eae331ef4a5058269
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-14 17:35:32 -0300

    [:art:] | update: component - input (app)

--- twitter_clone/src/app/component/input.component.tsx
+++ twitter_clone/src/app/component/input.component.tsx
@@ -2,15 +2,14 @@ import { IInputProps } from '../@types/props/input';
 import { InputContainer } from './inputContainer.component';
 
 export const Input = (props: IInputProps) => {
-  const { label, core, errors } = props;
+  const { label, core, errors, reactForms, light } = props;
 
-  return (
-    <InputContainer label={label} errors={errors}>
-      <input
-        {...core}
-        className=" 
+  const darkTheme = ` 
         w-full 
-        p-1 
+        h-14
+        p-1
+        mx-1
+        px-8
         rounded-lg 
         text-lg 
         bg-black 
@@ -19,12 +18,25 @@ export const Input = (props: IInputProps) => {
         outline-none 
         text-white 
         focus:border-sky-500 
-        foxus:border-3 
+        focus:border-3 
         transition 
         disabled:bg-neutral-900 
         disabled:opacity-70 
-        disabled:cursor-not-allowed
-        "
+        disabled:cursor-not-allowed 
+        ${core?.className}
+        `;
+
+  return (
+    <InputContainer label={label} errors={errors} className={props.className}>
+      <input
+        {...props}
+        {...core}
+        className={
+          !light
+            ? darkTheme
+            : ` outline-none  flex flex-1 w-full h-fit flex-col rounded-md text-black my-4 mx-2 bg-[rgba(255,255,255,0.35)] py-2 pl-2 ${core?.className}`
+        }
+        {...reactForms?.register(reactForms.name)}
       />
     </InputContainer>
   );
