583024afc369426edf16ddc3b03d5ac1cda1d850
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:05:50 -0300

    [:fire:] | delete (file)

--- twitter_clone/src/app/component/text/comment.component.tsx
+++
@@ -1,9 +0,0 @@
-import { IFooterTextProps } from '@/app/@types/props/text/footer';
-
-export const FooterText = (props: IFooterTextProps) => {
-  return (
-    <p className="text-neutral-400">
-      {props.children ? props.children : props.text}
-    </p>
-  );
-};
