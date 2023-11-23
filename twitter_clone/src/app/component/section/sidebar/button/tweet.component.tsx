a425ab2bd75c73b706dd0ace9e68200524728ce6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:03:00 -0300

    [:mailbox_with_mail:] | sync: events - notification (component/sidebar)

--- twitter_clone/src/app/component/section/sidebar/button/tweet.component.tsx
+++ twitter_clone/src/app/component/section/sidebar/button/tweet.component.tsx
@@ -14,7 +14,7 @@ export const SidebarTweetButton = ({ className }: { className?: string }) => {
       <div className="flex mt-6 bg-sky-500 hover:bg-opacity-60 hover:scale-110 transition-all duration-300 ease-in-out lg:hidden rounded-full h-14 w-14 p-4 items-center justify-center cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
-      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-70 cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out ">
+      <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-70 cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
           Tweet
         </p>
