81df13644dbc2de0096ae852ecc959b2de09b5a0
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 12:08:31 -0300

    [:sparkles:] | create: use-case - emit > like (module/core)

--- twitter_clone/src/app/modules/@core/like/use-case/observable/emit/create.use-case.ts
+++ twitter_clone/src/app/modules/@core/like/use-case/observable/emit/create.use-case.ts
@@ -4,7 +4,11 @@ import { IEmitLikeDTO } from '../../../DTO/observable/emit/like.dto';
 
 @injectable()
 export class EmitLikeCreatedUseCase extends LikeObservableSupport {
-  execute(like: IEmitLikeDTO) {
-    this.observable.emitLike(like);
+  execute(like: IEmitLikeDTO, isComment?: boolean) {
+    console.log({ isComment });
+    isComment
+      ? this.observable.emitCommentLike(like)
+      : this.observable.emitPostLike(like);
+    //this.observable.emitLike(like);
   }
 }
