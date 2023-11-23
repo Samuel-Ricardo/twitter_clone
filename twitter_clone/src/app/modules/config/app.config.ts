15a1c8942effa6da13e48dfab4e352c3ceb20357
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:32:43 -0300

    [:gear:] | update: config (module)

--- twitter_clone/src/app/modules/config/app.config.ts
+++ twitter_clone/src/app/modules/config/app.config.ts
@@ -1,5 +1,11 @@
 import { ENV } from './env/app.env';
+import { EVENT } from './event/event.config';
+import { CYPHER_CONFIG } from './security/cypher.config';
 
 export const CONFIG = {
   ...ENV,
+  EVENT,
+  SECURITY: {
+    CYPHER: { ...CYPHER_CONFIG },
+  },
 };
