f9d555258814ccd0fa6b5733d2d9d84f62cf2bb1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:41:35 -0300

    [:art:] | update: style - item > container (component/notification)

--- twitter_clone/src/app/component/notification/item/container.component.tsx
+++ twitter_clone/src/app/component/notification/item/container.component.tsx
@@ -1,11 +1,16 @@
 export const NotificationItemContainer = ({
   children,
+  onClick,
 }: {
   children: React.ReactNode;
+  onClick?: any;
 }) => {
   return (
-    <div className="flex flex-1 bg-gradient-to-tl from-gray-100/80 to-gray-400/40 transition-all duration-1000 ease-in-out hover:scale-110 hover:my-4 hover:bg-gradient-to-br rounded-full">
-      <div className="flex flex-row flex-1 items-center gap-4 p-4 bg-gray-300/60 transition-all duration-1000 ease-in-out hover:bg-transparent rounded-full">
+    <div
+      onClick={onClick}
+      className=" hover:cursor-pointer z-50 rounded-xl flex h-fit w-full bg-gradient-to-tl from-gray-100/80 to-gray-400/40 transition-all duration-1000 ease-in-out hover:scale-105 hover:mr-auto hover:my-4 hover:bg-gradient-to-br sm:rounded-full "
+    >
+      <div className="flex flex-col flex-1 rounded-xl sm:flex-row items-center gap-4 p-4 bg-gray-300/50 transition-all duration-1000 ease-in-out hover:bg-transparent sm:rounded-full">
         {children}
       </div>
     </div>
