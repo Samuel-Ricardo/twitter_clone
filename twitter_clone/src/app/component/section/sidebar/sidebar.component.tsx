35fa1b44c0cf165e84f19da7d7f093d8476d1905
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:59:28 -0300

    [:art:] | update: style - section > sidebar (component)

--- twitter_clone/src/app/component/section/sidebar/sidebar.component.tsx
+++ twitter_clone/src/app/component/section/sidebar/sidebar.component.tsx
@@ -8,6 +8,9 @@ import { signOut } from 'next-auth/react';
 import { BiLogOut } from 'react-icons/bi';
 import { BsBellFill, BsHouseFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+import { useNotifications } from '@/app/hooks/notification/find_by_user.hook';
+import { useEffect, useMemo, useState } from 'react';
+import { useNotificationEvents } from '@/app/hooks/notification/observable/all.hook';
 
 export const Sidebar = () => {
   const {
@@ -15,16 +18,17 @@ export const Sidebar = () => {
   } = useCurrentUser();
 
   return (
-    <div className="col-span-1 h-full px-4 md:pr-6 pt-2">
-      <div className="flex flex-col items-end h-full">
+    <div className="col-span-1 h-screen px-3 md:pr-6 pt-2 mr-2 bg-gradient-to-r from-gray-300/40 to-gray-100/30 rounded-e-lg ">
+      <div className="flex flex-col items-end h-full w-fit">
         <div className="space-y-6 lg:w-auto h-full items-center justify-center px-auto">
-          <SidebarLogo className="mb-16" />
+          <SidebarLogo className="mb-16 mt-2" />
 
           <SideBarItem icon={BsHouseFill} label="Home" href="/" />
           <SideBarItem
             icon={BsBellFill}
             label="Notifications"
             href="/notifications"
+            alert={hasNotifications}
           />
           <SideBarItem
             icon={FaUser}
@@ -35,9 +39,9 @@ export const Sidebar = () => {
           {data?.user && (
             <SideBarItem onClick={signOut} icon={BiLogOut} label="Logout" />
           )}
-          <div className="w-auto pt-[100%] mx-4">
-            <SidebarTweetButton />
-          </div>
+        </div>
+        <div className=" self-center mb-10 w-full mx-0 sm:px-3 ">
+          <SidebarTweetButton />
         </div>
       </div>
     </div>
