9442989d7c60d4e84af55d9039edefb2db05dba6
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:22:10 -0300

    [:recycle:] | refact

--- twitter_clone/src/app/modules/@core/user/policy/security/decrypt/create.policy.ts
+++ twitter_clone/src/app/modules/@core/user/policy/security/decrypt/create.policy.ts
@@ -2,6 +2,6 @@ import { UserCypherAccess } from '../../../cypher/cypher.access';
 
 export class DecryptCreateUserDataPolicy extends UserCypherAccess {
   execute(user: string) {
-    return this._cypher.decryptIvCreateDTO(user);
+    //    return this._cypher.decryptIvCreateDTO(user);
   }
 }
