1eda73b7e0af4ee72ea13cad36d50a59b9cb74e3
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 15:45:22 -0300

    [:closed_lock_with_key:] | load: env - test (module)

--- twitter_clone/src/app/modules/config/security/cypher.config.ts
+++ twitter_clone/src/app/modules/config/security/cypher.config.ts
@@ -1,5 +1,7 @@
 import { ENV } from '../env/app.env';
 
+console.log({ KKKKEEEEYYYY: ENV.SECURITY.CYPHER.KEY });
+
 export const CYPHER_CONFIG = {
   KEY: ENV.SECURITY.CYPHER.KEY,
   BREAKER: ENV.SECURITY.CYPHER.BREAKER,
