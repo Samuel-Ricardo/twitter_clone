d0b96c1aa6837d622bb912ce6df722f1b77bdd19
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:42:12 -0300

    [:art:] | update: style - tweets > feed (component)

--- twitter_clone/src/app/component/post/feed.component.tsx
+++ twitter_clone/src/app/component/post/feed.component.tsx
@@ -4,18 +4,28 @@ import { usePosts } from '@/app/hooks/post/all.hook';
 import { PostItem } from './item.component';
 import { useCurrentUser } from '@/app/hooks/user/current.hook';
 
-export const PostFeed = () => {
+//import { io } from 'socket.io-client';
+
+export const TweetFeed = () => {
   const { data } = usePosts();
   const {
     result: { data: fetch },
   } = useCurrentUser();
 
+  //const socket = io('http://localhost:3004');
+
+  //  socket.on('connect', () => {
+  //    console.log('connected');
+  //  });
+
+  //  socket.emit('HANDSHAKE', { RAPAZ: 'TOMI' });
+
   return (
-    <>
+    <div className="flex flex-col gap-3 flex-1 mt-auto mr-2">
       {data &&
         data.posts.map((post) => (
           <PostItem key={post.id} post={post} currentUser={fetch?.user} />
         ))}
-    </>
+    </div>
   );
 };
