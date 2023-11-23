4d57c764822133cbbd4663d2dc7f7557341735af
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:36:17 -0300

    [:clipboard:] | update: registry - config > API (module)

--- twitter_clone/src/app/modules/config/config.registry.ts
+++ twitter_clone/src/app/modules/config/config.registry.ts
@@ -5,4 +5,12 @@ export const ConfigRegistry = {
   CONFIG: Symbol.for('app.module.config'),
   EVENT: Symbol.for('app.module.event'),
   SOCKET_EVENT: Symbol.for('app.module.socket.event'),
+  API: {
+    ...CONFIG.API,
+    URL: Symbol.for('app.module.api.url'),
+    SOCKET: {
+      ...CONFIG.API.SOCKET,
+      URL: Symbol.for('app.module.socket.url'),
+    },
+  },
 };
