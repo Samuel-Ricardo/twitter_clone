9eda76eae0df4d1363c05ad8feafa0ff1d260a56
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:44:14 -0300

    [:art:] | update: style - post > item (component)

--- twitter_clone/src/app/component/post/item/container.component.tsx
+++ twitter_clone/src/app/component/post/item/container.component.tsx
@@ -9,8 +9,8 @@ export const PostItemContainer = ({
 }) => (
   <div
     onClick={onClick}
-    className="border-b-[1px] border-neutral-800 border-opacity-90 p-5 cursor-pointer hover:bg-neutral-900 hover:scale-110 transition-all duration-300 ease-in-out"
+    className="border-b-[1px] border-neutral-800 border-opacity-90 p-5 cursor-pointer hover:rounded-md hover:bg-gradient-to-br from-gray-400/40 to-gray-100/80 hover:scale-105 transition-all duration-500 ease-in-out hover:my-8 hover:shadow-[0px_0px_30px_-15px] hover:border-none"
   >
-    <div className="flex flex-row items-start gap-3">{children}</div>
+    <div className="flex flex-col items-start gap-3">{children}</div>
   </div>
 );
