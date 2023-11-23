87beb1f4ef7a354827ed4178a0c3f6ff7a66c177
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:15:08 -0300

    [:lock_with_ink_pen:] | update: cypher - user (module)

--- twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
+++ twitter_clone/src/app/modules/@core/user/cypher/user.cypher.ts
@@ -1,12 +1,4 @@
-import {
-  ICreateUserDTO,
-  IDeleteuserDTO,
-  ISelectUserByIdDTO,
-  IUpdateUserDTO,
-  IUserDTO,
-} from '../DTO';
-import { ISelectUserByEmailDTO } from '../DTO/select_by_email.dto';
-import { ISelectUserByCredentialsDTO } from '../DTO/select_by_credentials.dto';
+import { IUserDTO } from '../DTO';
 
 export interface IUserCypher {
   hashPassword(password: string): string | Promise<string>;
@@ -18,21 +10,6 @@ export interface IUserCypher {
   encryptIvUser(user: IUserDTO): string;
   decryptIvUser(user: string): IUserDTO;
 
-  encryptIvCreateDTO(user: ICreateUserDTO): string;
-  decryptIvCreateDTO(user: string): ICreateUserDTO;
-
-  enecryptIvUpdateDTO(user: IUpdateUserDTO): string;
-  decryptIvUpdateDTO(user: string): IUpdateUserDTO;
-
-  encryptIvDeleteDTO(user: IDeleteuserDTO): string;
-  decryptIvDeleteDTO(user: string): IDeleteuserDTO;
-
-  encryptIvSelectByIdDTO(user: ISelectUserByIdDTO): string;
-  decryptIvSelectByIdDTO(user: string): ISelectUserByIdDTO;
-
-  encryptIvSelectByEmailDTO(user: ISelectUserByEmailDTO): string;
-  decryptIvSelectByEmailDTO(user: string): ISelectUserByEmailDTO;
-
-  encryptIvSelectByCredentialsDTO(user: ISelectUserByCredentialsDTO): string;
-  decryptIvSelectByCredentialsDTO(user: string): ISelectUserByCredentialsDTO;
+  encryptIvUserList(users: IUserDTO[]): string;
+  decryptIvUserList(users: string): IUserDTO[];
 }
