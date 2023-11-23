f1c15cb6f703fe6b6030630c5bbe20bc9903d7d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:06:46 -0300

    [:art:] | update: style - link (component)

--- twitter_clone/src/app/component/text/link.component.tsx
+++ twitter_clone/src/app/component/text/link.component.tsx
@@ -3,7 +3,7 @@ import { ILinkProps } from '@/app/@types/props/text/link';
 export const Linked = (props: ILinkProps) => {
   return (
     <span
-      className="text-blue-500 font-semibold cursor-pointer hover:underline hover:text-blue-600"
+      className="text-cyan-500 font-semibold cursor-pointer hover:underline hover:text-cyan-400 transition"
       onClick={props.onClick}
     >
       {props.children ? props.children : props.text}
