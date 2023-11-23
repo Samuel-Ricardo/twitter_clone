40a5ebbd18f5365fd36e818b88be1863156e7d25
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:52:21 -0300

    [:art:] | update: style - item > header (component/post)

--- twitter_clone/src/app/component/post/item/header.component.tsx
+++ twitter_clone/src/app/component/post/item/header.component.tsx
@@ -1,24 +1,29 @@
 import { IUserDTO } from '@/app/modules/@core/user/DTO';
 import { Avatar } from '../../user/avatar.component';
+import { DeleteTweetButton } from '../../button/tweet/delete.button';
 
 export const PostItemHeader = ({
   user,
   createdAt,
   body,
+  id,
+  deletable,
 }: {
   user?: IUserDTO;
   createdAt: string;
   body: string;
+  id: string;
+  deletable?: boolean;
 }) => (
-  <div className="flex flex-row items-start gap-3">
+  <div className="flex flex-row items-start gap-3 w-full">
     <div className="min-w-fit">
       <Avatar userId={user?.id} image={user?.profileImage} />
     </div>
 
     <div>
       <div className="flex flex-col items-start gap-2">
-        <div className="flex flex-row items-center gap-2">
-          <p className="hover:underline">{user?.name || ''}</p>
+        <div className="flex flex-row items-center gap-2 w-full">
+          <p className="hover:underline text-lg ">{user?.name || ''}</p>
           <span className=" text-neutral-500 hover:underline">
             @{user?.username || ''}
           </span>
@@ -28,9 +33,9 @@ export const PostItemHeader = ({
       </div>
 
       <div className="my-4 mx-2 bg-[rgba(255,255,255,0.35)] p-2 rounded-md overflow-clip break-all hidden md:block ">
-        {body +
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+        {body}
       </div>
     </div>
+    {deletable && <DeleteTweetButton id={id} className="mr-2 ml-auto" />}
   </div>
 );
