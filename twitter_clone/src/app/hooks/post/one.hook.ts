13ceecd2e3ddf619065b947216fd58ff783ca29a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:21:31 -0300

    [:recycle:] | refact: hook - usePost (hook/use)

--- twitter_clone/src/app/hooks/post/one.hook.ts
+++ twitter_clone/src/app/hooks/post/one.hook.ts
@@ -3,7 +3,7 @@ import { IFindPostByIdDTO } from '@/app/modules/@core/post';
 import { useMemo } from 'react';
 
 export const usePost = (data: IFindPostByIdDTO) => {
-  const result = MODULES.POST.MAIN().findById(data).post;
-  const post = useMemo(() => result?.data?.post, [result]);
+  const result = MODULES.POST.MAIN().findById(data);
+  const post = useMemo(() => result?.post.data?.post, [result]);
   return { ...result, post };
 };
