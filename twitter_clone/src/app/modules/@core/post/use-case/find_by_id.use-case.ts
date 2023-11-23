a10362eb4ebbbf1d8904a931fbae3e1bb059c8c8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:12:53 -0300

    [:sparkles:] | create: use-case - find by [id] > post [async] (module/core)

--- twitter_clone/src/app/modules/@core/post/use-case/find_by_id.use-case.ts
+++ twitter_clone/src/app/modules/@core/post/use-case/find_by_id.use-case.ts
@@ -4,6 +4,9 @@ import { PostGatewayAccess } from '../gateway';
 
 @injectable()
 export class FindPostByIdUseCase extends PostGatewayAccess {
+  async executeAsync(id: IFindPostByIdDTO) {
+    return this.gateway.findById(id);
+  }
   execute(data: IFindPostByIdDTO) {
     return this.gateway.swrFindById(data);
   }
