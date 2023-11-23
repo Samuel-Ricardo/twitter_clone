6b3353e76f5115c191e798053d038bf86141295a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:27:45 -0300

    [:recycle:] | refact: footer - props > type (component/modal)

--- twitter_clone/src/app/component/modal/generic/footer.component.tsx
+++ twitter_clone/src/app/component/modal/generic/footer.component.tsx
@@ -1,3 +1,3 @@
-export const ModalFooter = ({ children }: { children: JSX.Element }) => (
+export const ModalFooter = ({ children }: { children: React.ReactNode }) => (
   <footer className="flex flex-col gap-2 p-10">{children}</footer>
 );
