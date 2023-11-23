e4d89c1c66324fa0e976752af1d2e4b0079e7e82
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:41:13 -0300

    [:label:] | replicate: interface - File > to nextjs side (@types)

--- twitter_clone/src/app/modules/validation/zod/user/edit.validation.ts
+++ twitter_clone/src/app/modules/validation/zod/user/edit.validation.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { maxFileSize } from './rules/file/size.rule';
 import { toBase64 } from './transformers/file/toBase64.transformer';
 import { UppercaseFirstLetters } from './transformers/name.transformer';
+import { File } from '../../../../@types/file';
 
 export const userEditSchema = z.object({
   profileImage: z
