ed5b69869dcc3cfa7f1a0da434c48936924a99d9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:35:49 -0300

    [:clipboard:] | update: registry - config > API (module)

--- twitter_clone/src/app/modules/config/config.module.ts
+++ twitter_clone/src/app/modules/config/config.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { ConfigRegistry } from './config.registry';
 import { CONFIG } from './app.config';
+import getDecorators from 'inversify-inject-decorators';
 
 export const ConfigModule = new Container({
   autoBindInjectable: true,
@@ -10,6 +11,50 @@ export const ConfigModule = new Container({
 ConfigModule.bind(ConfigRegistry.CONFIG).toConstantValue(CONFIG);
 
 ConfigModule.bind(ConfigRegistry.API.URL).toConstantValue(CONFIG.API.URL);
+ConfigModule.bind(ConfigRegistry.API.SOCKET.URL).toConstantValue(
+  CONFIG.API.SOCKET.URL,
+);
+
 ConfigModule.bind(ConfigRegistry.NEXT.API.URL).toConstantValue(
   CONFIG.NEXT.API.URL,
 );
+
+ConfigModule.bind(ConfigRegistry.EVENT).toConstantValue(CONFIG.EVENT);
+
+ConfigModule.bind(ConfigRegistry.SOCKET_EVENT).toConstantValue(
+  CONFIG.EVENT.SOCKET,
+);
+
+ConfigModule.bind(
+  ConfigRegistry.SECURITY.CYPHER.ALGORITHMS.AES[128].CBC,
+).toConstantValue(CONFIG.SECURITY.CYPHER.ALGORITHMS.AES[128].CBC);
+
+ConfigModule.bind(
+  ConfigRegistry.SECURITY.CYPHER.ALGORITHMS.AES[256].CBC,
+).toConstantValue(CONFIG.SECURITY.CYPHER.ALGORITHMS.AES[256].CBC);
+
+ConfigModule.bind(
+  ConfigRegistry.SECURITY.CYPHER.ALGORITHMS.AES[128].GCM,
+).toConstantValue(CONFIG.SECURITY.CYPHER.ALGORITHMS.AES[128].GCM);
+
+ConfigModule.bind(
+  ConfigRegistry.SECURITY.CYPHER.ALGORITHMS.AES[256].GCM,
+).toConstantValue(CONFIG.SECURITY.CYPHER.ALGORITHMS.AES[256].GCM);
+
+ConfigModule.bind(ConfigRegistry.SECURITY.CYPHER.KEY).toConstantValue(
+  CONFIG.SECURITY.CYPHER.KEY,
+);
+
+ConfigModule.bind(ConfigRegistry.SECURITY.CYPHER.BREAKER).toConstantValue(
+  CONFIG.SECURITY.CYPHER.BREAKER,
+);
+
+ConfigModule.bind(ConfigRegistry.SECURITY.CYPHER.AUTH.BREAKER).toConstantValue(
+  CONFIG.SECURITY.CYPHER.AUTH.BREAKER,
+);
+
+ConfigModule.bind(ConfigRegistry.SECURITY.CYPHER.IV.BREAKER).toConstantValue(
+  CONFIG.SECURITY.CYPHER.IV.BREAKER,
+);
+
+export const { lazyInject: injectConfig } = getDecorators(ConfigModule);
