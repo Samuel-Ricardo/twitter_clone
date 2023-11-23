a0ff9a55a2498bf6ac23813ee9e5262dafe7c52a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:17:19 -0300

    [:syringe:] | update: injection - lazy (module/core)

--- twitter_clone/src/app/modules/@core/user/gateway/gateway_access.ts
+++ twitter_clone/src/app/modules/@core/user/gateway/gateway_access.ts
@@ -1,11 +1,10 @@
 import { inject, injectable } from 'inversify';
 import type { IUserGateway } from './user.gateway';
 import { MODULE } from '@/app/modules/app.registry';
+import { injectGateway } from '@/app/modules/gateway/gateway.module';
 
 @injectable()
 export abstract class UserGatewayAccess {
-  constructor(
-    @inject(MODULE.GATEWAY.AXIOS.USER)
-    protected readonly gateway: IUserGateway,
-  ) {}
+  @injectGateway(MODULE.GATEWAY.AXIOS.USER)
+  protected readonly gateway: IUserGateway;
 }
