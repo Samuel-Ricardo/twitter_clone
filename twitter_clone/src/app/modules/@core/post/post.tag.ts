e4fa16ff5afff647ed5a27df936383083672c0fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:06:59 -0300

    [:fire:] | delete (file)

--- twitter_clone/src/app/modules/@core/post/post.tag.ts
+++
@@ -1,9 +0,0 @@
-export const POST_TAG = {
-  SCOPE: {
-    TAG: 'TAG:POST.SCOPE',
-    MAIN: 'TAG:POST.SCOPE.MAIN',
-    REACTIVE: 'TAG:POST.SCOPE.REACTIVE',
-  },
-};
-
-export const { SCOPE } = POST_TAG;
