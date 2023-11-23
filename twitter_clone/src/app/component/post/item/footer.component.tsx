cd114a38af1afab53bc595388b2a58944c6a8605
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:51:06 -0300

    [:recycle:] | refact: button - like (component/post)

--- twitter_clone/src/app/component/post/item/footer.component.tsx
+++ twitter_clone/src/app/component/post/item/footer.component.tsx
@@ -1,30 +1,13 @@
 import { IPostItemFooterProps } from '@/app/@types/props/post/item_footer';
 import { useTweetComments } from '@/app/hooks/comment/post.hook';
-import { useTweetLikes } from '@/app/hooks/like/post.hook';
-import { useToggleLike } from '@/app/hooks/like/post/mutate/toggle.hook';
-import { useCallback } from 'react';
-import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
+import { AiOutlineMessage } from 'react-icons/ai';
+import { LikeButton } from '../../button/like.component';
 
 export const PostItemFooter = ({
   postId,
-  onLikeClick,
   onCommentClick,
 }: IPostItemFooterProps) => {
-  const { likes, mutate: syncLikes } = useTweetLikes({ likedId: postId });
   const { comments } = useTweetComments({ postId });
-
-  const { toggle: toggleLike, hasLiked } = useToggleLike({ likedId: postId });
-
-  const handleLike = useCallback(
-    (event: React.MouseEvent<any>) => {
-      event.stopPropagation();
-
-      if (onLikeClick) onLikeClick(event);
-
-      toggleLike();
-    },
-    [onLikeClick, toggleLike],
-  );
 
   return (
     <div className="flex flex-row items-center mt-3 gap-10">
@@ -35,18 +18,7 @@ export const PostItemFooter = ({
         <AiOutlineMessage size={28} />
         <p>{comments.length}</p>
       </div>
-      <div
-        onClick={handleLike}
-        onMouseLeave={syncLikes}
-        className="flex flex-row items-center gap-2 cursor-pointer transition-all hover:scale-105 duration-300 ease-in-out hover:bg-red-200 hover:rounded-full hover:p-2 "
-      >
-        {hasLiked() ? (
-          <AiFillHeart size={28} color="red" />
-        ) : (
-          <AiOutlineHeart size={28} />
-        )}
-        <p>{likes?.length || 0}</p>
-      </div>
+      <LikeButton likedId={postId} />
     </div>
   );
 };
