ee842902d7fde3ed87e7346debd577ac4356d0d7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:22:26 -0300

    [:fire:] | delete (file)

--- twitter_clone/src/app/modules/@core/user/policy/security/decrypt/data.policy.ts
+++
@@ -1,8 +0,0 @@
-import { IEncriptedIv } from '@/app/@types/security/cryptographer/encriptedIv';
-import { UserCypherAccess } from '../../../cypher/cypher.access';
-
-export class DecryptUserDataPolicy extends UserCypherAccess {
-  execute(user: IEncriptedIv) {
-    return this._cypher.decryptIvUser(user);
-  }
-}
