f60e0bfa12c32ccc2fa806f8581b44e6fe547b56
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 10:22:32 -0300

    [:art:] | update: component - form > ImageUploader (app)

--- twitter_clone/src/app/component/form/input/image/container.component.tsx
+++ twitter_clone/src/app/component/form/input/image/container.component.tsx
@@ -1,17 +1,19 @@
 import { IImageUploadContainerProps } from '@/app/@types/props/form/input/image.container';
 
 export const ImageUploadContainer = ({
-  className,
+  className: style,
   errors,
-  label,
   children,
+  dropzone,
 }: IImageUploadContainerProps) => (
-  <div className={`flex flex-col w-full ${className}`}>
-    {label && (
-      <label className="text-lg text-white font-semibold mb-2">{label}</label>
-    )}
-
-    {children}
+  <div className="flex flex-col flex-1 gap-6 text-center justify-center items-center">
+    <div
+      {...dropzone({
+        className: `flex justify-center itens-center w-full p-4 text-white text-center border-[3px] border-dotted rounded-md border-white ${style}`,
+      })}
+    >
+      {children}
+    </div>
 
     {errors && (
       <p className="text-red-500 font-semibold text-sm">{errors.message}</p>
