df30dbd2c56ef9790a9eb156cfa04028f99e2884
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 13:51:38 -0300

    [:pencil2:] | fix: typo

--- twitter_clone/src/app/modules/@core/like/use-case/observable/listen/post/deleted.use-case.ts
+++ twitter_clone/src/app/modules/@core/like/use-case/observable/listen/post/deleted.use-case.ts
@@ -3,7 +3,7 @@ import { IListenDislikeDTO } from '../../../../DTO/observable/listen/dislike.dto
 import { LikeObservableSupport } from '../../../../observable/observable.support';
 
 @injectable()
-export class ListenDislikePostUseCase extends LikeObservableSupport {
+export class ListenPostDislikeUseCase extends LikeObservableSupport {
   execute(schedule: IListenDislikeDTO) {
     this.observable.listenPostDislike(schedule);
   }
