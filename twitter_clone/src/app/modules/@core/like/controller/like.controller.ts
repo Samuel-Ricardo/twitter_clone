9d87c0f3f4279a37573be4c8b4fd77c942a996c1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 12:00:30 -0300

    [:syringe:] | update: controller - like > scope (module/core)

--- twitter_clone/src/app/modules/@core/like/controller/like.controller.ts
+++ twitter_clone/src/app/modules/@core/like/controller/like.controller.ts
@@ -1,4 +1,4 @@
-import { inject, injectable } from 'inversify';
+import { inject, injectable, tagged } from 'inversify';
 import { LikeService } from '../service';
 import { MODULE } from '@/app/modules/app.registry';
 import {
@@ -8,15 +8,18 @@ import {
   IFindPostLikesDTO,
   IFindUserLikesDTO,
 } from '../DTO';
+import { SCOPE } from '@/app/modules/app.tag';
 
 @injectable()
 export class LikeController {
   constructor(
     @inject(MODULE.LIKE.SERVICE)
+    @tagged(SCOPE.TAG, SCOPE.MAIN)
     private readonly service: LikeService,
   ) {}
 
   async create(like: ICreateLikeDTO) {
+    console.log({ LIKEcontrolller: like });
     return { like: (await this.service.create(like)).toStruct() };
   }
 
