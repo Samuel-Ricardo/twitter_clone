e0186668ea3241358fde70b81458f9a594431156
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:25:37 -0300

    [:sparkles:] | create: use-case - find by [id] > user (module/core)

--- twitter_clone/src/app/modules/@core/user/use-case/select_by_id.use-case.ts
+++ twitter_clone/src/app/modules/@core/user/use-case/select_by_id.use-case.ts
@@ -2,6 +2,9 @@ import { ISelectUserByIdDTO } from '../DTO';
 import { UserGatewayAccess } from '../gateway';
 
 export class SelectUserByIdUseCase extends UserGatewayAccess {
+  async executeAsync(user: ISelectUserByIdDTO) {
+    return await this.gateway.selectById(user);
+  }
   execute(user: ISelectUserByIdDTO) {
     return this.gateway.swrSelectById(user);
   }
