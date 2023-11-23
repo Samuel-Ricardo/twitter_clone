a5169d0d642b29974c33a6d850a1d16638beb930
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:21:45 -0300

    [:clipboard:] | update: registry - user > policy (module/core)

--- twitter_clone/src/app/modules/@core/user/policy/policy.registry.ts
+++ twitter_clone/src/app/modules/@core/user/policy/policy.registry.ts
@@ -2,9 +2,11 @@ export const USER_POLICY_REGISTRY = {
   SECURITY: {
     ENCRYPT: {
       DATA: Symbol.for('user.policy.encrypt.data'),
+      CREATE: Symbol.for('user.policy.encrypt.create'),
     },
     DECRYPT: {
       DATA: Symbol.for('user.policy.decrypt.data'),
+      CREATE: Symbol.for('user.policy.decrypt.create'),
     },
   },
 };
