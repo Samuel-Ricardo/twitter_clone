0b476bb510ccf7ef5fd6c4d72d08b256f745de53
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:25:14 -0300

    [:link:] | update: hook - useUser (hook)

--- twitter_clone/src/app/hooks/user/one.hook.ts
+++ twitter_clone/src/app/hooks/user/one.hook.ts
@@ -1,5 +1,10 @@
 import { MODULES } from '@/app/modules';
 import { ISelectUserByIdDTO } from '@/app/modules/@core/user/DTO';
+import { useMemo } from 'react';
 
-export const useUser = (user: ISelectUserByIdDTO) =>
-  MODULES.USER.MAIN().selectById(user).user;
+export const useUser = (data: ISelectUserByIdDTO) => {
+  const result = MODULES.USER.MAIN().selectById(data).user;
+  const user = useMemo(() => result.data?.user, [result]);
+
+  return { ...result, user };
+};
