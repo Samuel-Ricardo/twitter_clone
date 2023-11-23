6fffc10b7fdd5ebeb82e34f8da6734b33ffbf21c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:06:30 -0300

    [:syringe:] | import: modules - post (module/core)

--- twitter_clone/src/app/modules/@core/post/post.module.ts
+++ twitter_clone/src/app/modules/@core/post/post.module.ts
@@ -13,13 +13,20 @@ import { PostService } from './service';
 import { PostController } from './controller/post.controller';
 import { EmitPostUseCase } from './use-case/observable/emit/created.use-case';
 import { ListenPostUseCase } from './use-case/observable/listen/created.use-case';
-import { SCOPE } from './post.tag';
+import { SCOPE } from '../../app.tag';
 import { ReactivePostService } from './service/reactive/post.service';
 import { ReactivePostController } from './controller/reactive/post.controller';
 
+import getDecorators from 'inversify-inject-decorators';
+import { OBSERVABLE_MODULE } from '../../observable/observable.module';
+
 const MODULE = new Container({ autoBindInjectable: true });
 
-export const POST_MODULE = Container.merge(MODULE, GatewayModule);
+export const POST_MODULE = Container.merge(
+  MODULE,
+  OBSERVABLE_MODULE,
+  GatewayModule,
+);
 
 POST_MODULE.bind(POST_REGISTRY.USE_CASE.CREATE).to(CreatePostUseCase);
 POST_MODULE.bind(POST_REGISTRY.USE_CASE.DELETE).to(DeletePostUseCase);
@@ -56,3 +63,5 @@ POST_MODULE.bind(POST_REGISTRY.CONTROLLER)
   .whenTargetTagged(SCOPE.TAG, SCOPE.REACTIVE);
 
 POST_MODULE.bind(POST_REGISTRY.REACTIVE).to(ReactivePostController);
+
+export const { lazyInject: postInject } = getDecorators(POST_MODULE);
