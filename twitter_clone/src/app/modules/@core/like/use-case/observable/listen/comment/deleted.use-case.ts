0a2f9cc66f5140ac65d8f8a10fb26a5bf850238f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 13:50:36 -0300

    [:adhesive_bandage:] | fix: injection (module/core)

--- twitter_clone/src/app/modules/@core/like/use-case/observable/listen/comment/deleted.use-case.ts
+++ twitter_clone/src/app/modules/@core/like/use-case/observable/listen/comment/deleted.use-case.ts
@@ -1,6 +1,8 @@
+import { injectable } from 'inversify';
 import { IListenDislikeDTO } from '../../../../DTO/observable/listen/dislike.dto';
 import { LikeObservableSupport } from '../../../../observable/observable.support';
 
+@injectable()
 export class ListenCommentDislikeUseCase extends LikeObservableSupport {
   execute(like: IListenDislikeDTO) {
     this.observable.listenCommentDislike(like);
