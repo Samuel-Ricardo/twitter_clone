9924288811fdb7c05125d65802cbabb2d6b1f616
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:30:56 -0300

    [:syringe:] | update: module - auth (module/core)

--- twitter_clone/src/app/modules/auth/auth.module.ts
+++ twitter_clone/src/app/modules/auth/auth.module.ts
@@ -1,11 +1,16 @@
 import { Container } from 'inversify';
 import { AUTH_REGISTRY } from './auth.registry';
-import { NEXT_AUTH_HANDLER, authOptions } from './next/next.auth';
+import { authOptions } from './next/next.auth';
+import { UserModule } from '../@core/user/user.module';
 
-export const AUTH_MODULE = new Container({
+const MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-AUTH_MODULE.bind(AUTH_REGISTRY.NEXT.OPTIONS).toConstantValue(authOptions);
-AUTH_MODULE.bind(AUTH_REGISTRY.NEXT.HANDLER).toConstantValue(NEXT_AUTH_HANDLER);
+export const AUTH_MODULE = Container.merge(MODULE, UserModule);
+
+AUTH_MODULE.bind(AUTH_REGISTRY.NEXT.OPTIONS)
+  .toDynamicValue(authOptions)
+  .inSingletonScope();
+//AUTH_MODULE.bind(AUTH_REGISTRY.NEXT.HANDLER).toConstantValue(NEXT_AUTH_HANDLER);
