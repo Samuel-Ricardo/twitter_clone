8970d9282fe63f8d9da6c27c113dcb8d3fa15792
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:35:22 -0300

    [:clipboard:] | update: registry - config > SOCKET_EVENT (module)

--- twitter_clone/src/app/modules/config/config.registry.ts
+++ twitter_clone/src/app/modules/config/config.registry.ts
@@ -4,4 +4,5 @@ export const ConfigRegistry = {
   ...CONFIG,
   CONFIG: Symbol.for('app.module.config'),
   EVENT: Symbol.for('app.module.event'),
+  SOCKET_EVENT: Symbol.for('app.module.socket.event'),
 };
