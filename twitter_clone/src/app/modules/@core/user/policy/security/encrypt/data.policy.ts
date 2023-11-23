14616aabe6fbad05b03e2d2c1c8fdffd61cb4831
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 14:22:35 -0300

    [:fire:] | delete (file)

--- twitter_clone/src/app/modules/@core/user/policy/security/encrypt/data.policy.ts
+++
@@ -1,8 +0,0 @@
-import { IUserDTO } from '../../../DTO';
-import { UserCypherAccess } from '../../../cypher/cypher.access';
-
-export class EncryptUserDataPolicy extends UserCypherAccess {
-  execute(user: IUserDTO) {
-    return this._cypher.encryptIvUser(user);
-  }
-}
