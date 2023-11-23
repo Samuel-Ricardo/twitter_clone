e41e7a0a1bdccf6459c959c3b9cbba6a2d2142ac
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:56:27 -0300

    [:sparkles:] | create: use-case - find > followers (module)

--- twitter_clone/src/app/modules/@core/follow/use-case/get_followers.use-case.ts
+++ twitter_clone/src/app/modules/@core/follow/use-case/get_followers.use-case.ts
@@ -4,6 +4,9 @@ import { FollowGatewayAccess } from '../gateway/gateway.access';
 
 @injectable()
 export class GetFollowersUseCase extends FollowGatewayAccess {
+  async executeAsync(data: IGetFollowersDTO) {
+    return await this.gateway.getFollowers(data);
+  }
   execute(user: IGetFollowersDTO) {
     return this.gateway.swrGetFollowers(user);
   }
