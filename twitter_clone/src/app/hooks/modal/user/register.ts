41c2ee3767a385a123093174c21ad2a32c81f237
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:19:40 -0300

    [:fire:] | delete (file)

--- twitter_clone/src/app/hooks/modal/user/register.ts
+++
@@ -1,8 +0,0 @@
-import { IUserRegisterModalState } from '@/app/@types/state/modal/user/register';
-import { create } from 'zustand';
-
-export const useRegisterModal = create<IUserRegisterModalState>((set) => ({
-  isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
-}));
