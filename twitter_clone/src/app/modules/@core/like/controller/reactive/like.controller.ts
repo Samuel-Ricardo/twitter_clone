77b9f38f2a2b788078a0bc9dcf7c1cbaae5d8b00
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 12:00:52 -0300

    [:recycle:] | refact: imports

--- twitter_clone/src/app/modules/@core/like/controller/reactive/like.controller.ts
+++ twitter_clone/src/app/modules/@core/like/controller/reactive/like.controller.ts
@@ -1,9 +1,9 @@
 import { inject, injectable, tagged } from 'inversify';
-import { SCOPE } from '../../like.tag';
 import { ReactiveLikeService } from '../../service/reactive/like.service';
 import { MODULE } from '@/app/modules/app.registry';
 import { IListenLikeDTO } from '../../DTO/observable/listen/like.dto';
 import { IListenDislikeDTO } from '../../DTO/observable/listen/dislike.dto';
+import { SCOPE } from '@/app/modules/app.tag';
 
 @injectable()
 export class ReactiveLikeController {
@@ -14,6 +14,7 @@ export class ReactiveLikeController {
   ) {}
 
   onTweetLike(schedule: IListenLikeDTO) {
+    console.log({ TWITERRRRRLIKEEEEEEEEE: schedule });
     this.service.onTweetLike(schedule);
   }
 
