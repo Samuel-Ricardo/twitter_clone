7a151a6203ab0712d157ba2981d6e7298681d617
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:20:12 -0300

    [:link:] | update: hook - useNotificationEvents (hook/events)

--- twitter_clone/src/app/hooks/notification/observable/all.hook.ts
+++ twitter_clone/src/app/hooks/notification/observable/all.hook.ts
@@ -2,8 +2,6 @@ import { MODULES } from '@/app/modules';
 
 export const useNotificationEvents = () => {
   return {
-    onCreate: MODULES.NOTIFICATION.REACTIVE().onCreate,
-    onView: MODULES.NOTIFICATION.REACTIVE().onView,
-    onDelete: MODULES.NOTIFICATION.REACTIVE().onDelete,
+    notification: MODULES.NOTIFICATION.REACTIVE(),
   };
 };
