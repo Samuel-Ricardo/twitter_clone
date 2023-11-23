a1743d6fa7baa9d02b122d4dfce1db6b771699c4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:15:11 -0300

    [:link:] | update: hook - useFollowers (hook)

--- twitter_clone/src/app/hooks/follow/followers.hook.ts
+++ twitter_clone/src/app/hooks/follow/followers.hook.ts
@@ -1,5 +1,9 @@
 import { MODULES } from '@/app/modules';
 import { IGetFollowersDTO } from '@/app/modules/@core/follow/DTO';
+import { useMemo } from 'react';
 
-export const useFollowers = (user: IGetFollowersDTO) =>
-  MODULES.FOLLOW.MAIN().followersOf(user).followers;
+export const useFollowers = (user: IGetFollowersDTO) => {
+  const result = MODULES.FOLLOW.MAIN().followersOf(user).followers;
+  const followers = useMemo(() => result.data?.followers, [result.data]);
+  return { ...result, followers };
+};
