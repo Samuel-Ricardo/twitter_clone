f10611404e1c62a56113448e99afb8583f31966a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-23 11:08:19 -0300

    [:art:] | update: style - perfil > cover (component)

--- twitter_clone/src/app/component/user/cover.component.tsx
+++ twitter_clone/src/app/component/user/cover.component.tsx
@@ -9,18 +9,25 @@ export const ProfileCover = ({
   onAvatarClick,
 }: ICoverProps) => {
   return (
-    <div>
-      <div className="bg-neutral-700 h-44 relative">
+    <div className="mt-1 z-10">
+      <div className="bg-gradient-to-br from-gray-100/70 to-gray-400/40   min-h-fit h-[35vh] relative rounded-md">
         {cover && (
           <Image
             src={cover}
             fill
             alt="cover image"
             style={{ objectFit: 'cover' }}
+            className="rounded-md"
           />
         )}
         <div className="absolute -bottom-16 left-4">
-          <Avatar userId={userId} image={avatar} onClick={onAvatarClick} />
+          <Avatar
+            userId={userId}
+            image={avatar}
+            onClick={onAvatarClick}
+            large
+            bordered
+          />
         </div>
       </div>
     </div>
