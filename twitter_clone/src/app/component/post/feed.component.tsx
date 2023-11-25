907793f7f7edc97a5d4ebe675363d17480d8069e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-25 09:06:24 -0300

    [:bulb:] | refact: clean - comments

--- twitter_clone/src/app/component/post/feed.component.tsx
+++ twitter_clone/src/app/component/post/feed.component.tsx
@@ -4,21 +4,11 @@ import { usePosts } from '@/app/hooks/post/all.hook';
 import { PostItem } from './item.component';
 import { useCurrentUser } from '@/app/hooks/user/current.hook';
 
-//import { io } from 'socket.io-client';
-
 export const TweetFeed = () => {
   const { data } = usePosts();
   const {
     result: { data: fetch },
   } = useCurrentUser();
-
-  //const socket = io('http://localhost:3004');
-
-  //  socket.on('connect', () => {
-  //    console.log('connected');
-  //  });
-
-  //  socket.emit('HANDSHAKE', { RAPAZ: 'TOMI' });
 
   return (
     <div className="flex flex-col gap-3 flex-1 mt-auto mr-2">
