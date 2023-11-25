1aae2812acc98ea7e4345a111fb6b74f7918e231
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-24 22:59:07 -0300

    [:mute:] | remove: logs - global > user (module)

--- twitter_clone/src/app/global/user.global.ts
+++ twitter_clone/src/app/global/user.global.ts
@@ -4,12 +4,10 @@ export class GlobalUser {
   private static _globalUser: IUserDTO | undefined = undefined;
 
   static get user(): IUserDTO | undefined {
-    console.log({ GLOBAL_GET: this._globalUser });
     return this._globalUser;
   }
 
   static set user(data: IUserDTO | undefined) {
     this._globalUser = data;
-    console.log({ GLOBAL_SET: this._globalUser });
   }
 }
