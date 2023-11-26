3d6686e0de1e2cf3df079e039d14662f8bbcf907
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-26 09:47:26 -0300

    [:safety_vest:] | apply: rule - file > zod (module/validation)

--- twitter_clone/src/app/modules/validation/zod/user/edit.validation.ts
+++ twitter_clone/src/app/modules/validation/zod/user/edit.validation.ts
@@ -2,12 +2,17 @@ import { z } from 'zod';
 import { maxFileSize } from './rules/file/size.rule';
 import { toBase64 } from './transformers/file/toBase64.transformer';
 import { UppercaseFirstLetters } from './transformers/name.transformer';
-import { File } from '../../../../@types/file';
+import { File } from '@/app/@types/file';
+import { mustBeValidFile } from './rules/file/type.rule';
 
 export const userEditSchema = z.object({
   profileImage: z
     .string()
-    .or(z.instanceof(File, { message: 'Avatar must be an image file' }))
+    .or(
+      z.custom<File>(mustBeValidFile, {
+        message: 'Avatar must be an image file',
+      }),
+    )
     .nullish()
     .refine(maxFileSize(5 * 1024 * 1024), 'File size must be less than 5MB')
     .transform(toBase64),
