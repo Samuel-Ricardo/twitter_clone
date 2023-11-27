8ab2932af5b0f2414cdc561a32e6993908817dca
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-27 10:01:07 -0300

    [:white_check_mark:] | test: gateway - create > user (integration)

--- twitter_clone/test/app/module/gateway/axios/integration/user.gateway.spec.ts
+++ twitter_clone/test/app/module/gateway/axios/integration/user.gateway.spec.ts
@@ -8,6 +8,7 @@ import { MODULES } from '@/app/modules';
 import { AxiosUserGateway } from '@/app/modules/gateway/axios/user/user.gateway';
 import { expect } from '@jest/globals';
 import { User } from '@/app/modules/@core/user/entity/user.entity';
+import { CREATE_USER_DATA } from '@test/mock/data/user';
 
 describe('[GATEWAY] | AXIOS => [USER] ', () => {
   let gateway: AxiosUserGateway;
@@ -16,6 +17,13 @@ describe('[GATEWAY] | AXIOS => [USER] ', () => {
     gateway = MODULES.GATEWAY.AXIOS.USER();
 
     expect(gateway).toBeInstanceOf(AxiosUserGateway);
+  });
+
+  it('[INTEGRATION] | Should: create [USER]', async () => {
+    const result = await gateway.create(CREATE_USER_DATA);
+
+    expect(result).toBeInstanceOf(User);
+    expect(result.id).toBeDefined();
   });
 
   it('[INTEGRATION] | Should: list [all] => [USER]', async () => {
