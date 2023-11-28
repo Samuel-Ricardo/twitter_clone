f43e167084e158cfd5bba400cdf69ce25da771d8
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-28 11:42:47 -0300

    [:clown_face:] | update: factory - user > session (mock:module/hook)

--- twitter_clone/test/mock/module/hooks/user/user.factory.ts
+++ twitter_clone/test/mock/module/hooks/user/user.factory.ts
@@ -1,8 +1,10 @@
-import { DeepMockProxy } from 'jest-mock-extended';
+import { CalledWithMock, DeepMockProxy } from 'jest-mock-extended';
 import { USER_HOOKS_MODULE_MOCK } from './user.module';
 import { useUsers } from '@/app/hooks/user/all.hook';
 import { USER_HOOKS_REGISTRY_MOCK } from './user.registry';
 import { ISimulatedUseUsersHook } from '@test/@types/simulate/user/hooks/all';
+import { useSession } from 'next-auth/react';
+import { MockedUseSession } from './session.hook';
 
 export const USER_HOOKS_FACTORY_MOCK = {
   ALL: {
@@ -13,6 +15,12 @@ export const USER_HOOKS_FACTORY_MOCK = {
     SIMULATE: () =>
       USER_HOOKS_MODULE_MOCK.get<ISimulatedUseUsersHook>(
         USER_HOOKS_REGISTRY_MOCK.ALL.SIMULATE,
+      ),
+  },
+  SESSION: {
+    MOCK: () =>
+      USER_HOOKS_MODULE_MOCK.get<MockedUseSession>(
+        USER_HOOKS_REGISTRY_MOCK.SESSION.MOCK,
       ),
   },
 };
