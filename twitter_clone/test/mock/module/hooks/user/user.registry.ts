8bfde40a1d9636f238e96db8d222694426432d32
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-11-28 11:31:41 -0300

    [:clown_face:] | update: registry - user > session (mock:module/hook)

--- twitter_clone/test/mock/module/hooks/user/user.registry.ts
+++ twitter_clone/test/mock/module/hooks/user/user.registry.ts
@@ -3,4 +3,8 @@ export const USER_HOOKS_REGISTRY_MOCK = {
     MOCK: Symbol.for('mock:user.hooks.all'),
     SIMULATE: Symbol.for('simulate:user.hooks.all'),
   },
+  SESSION: {
+    MOCK: Symbol.for('mock:user.hooks.session'),
+    SIMULATE: Symbol.for('simulate:user.hooks.session'),
+  },
 };
