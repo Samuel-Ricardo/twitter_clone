ceffbcc58593c00557a6f45a3f8fa3ffd86293c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-01 11:28:06 -0300

    [:label:] | create: ISimulated - hooks > user (test::types)

--- twitter_clone/test/@types/simulate/user/hooks/index.ts
+++ twitter_clone/test/@types/simulate/user/hooks/index.ts
@@ -1,5 +1,12 @@
-import { ISimulatedUseUsersHook } from './all';
+import { DeepMockProxy } from 'jest-mock-extended';
+import { UserController } from '@/app/modules/@core/user/controller';
+import { useUsers } from '@/app/hooks/user/all.hook';
+import { useCurrentUser } from '@/app/hooks/user/current.hook';
+import { MockedUseSession } from '@test/@types/hooks/user/session';
 
 export interface ISimulatedUserHooks {
-  all: ISimulatedUseUsersHook;
+  all: typeof useUsers;
+  current: typeof useCurrentUser;
+  session: MockedUseSession;
+  controller: DeepMockProxy<UserController>;
 }
