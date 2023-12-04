08c6b6ce92e5195aac32cf83a7438600cd0d7775
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-04 15:59:59 -0300

    [:label:] | update: ISimulated - hooks > user (test::types)

--- twitter_clone/test/@types/simulate/user/hooks/index.ts
+++ twitter_clone/test/@types/simulate/user/hooks/index.ts
@@ -3,10 +3,12 @@ import { UserController } from '@/app/modules/@core/user/controller';
 import { useUsers } from '@/app/hooks/user/all.hook';
 import { useCurrentUser } from '@/app/hooks/user/current.hook';
 import { MockedUseSession } from '@test/@types/hooks/user/session';
+import { useUser } from '@/app/hooks/user/one.hook';
 
 export interface ISimulatedUserHooks {
   all: typeof useUsers;
   current: typeof useCurrentUser;
+  one: typeof useUser;
   session: MockedUseSession;
   controller: DeepMockProxy<UserController>;
 }
