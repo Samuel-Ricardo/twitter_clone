c4717c96b12d3f4986a2b12d69dcf41720b0f131
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-04 10:36:13 -0300

    [:label:] | create: type - useCurrentUserMock (test::types)

--- twitter_clone/test/@types/hooks/user/current.ts
+++ twitter_clone/test/@types/hooks/user/current.ts
@@ -1,3 +1,9 @@
+import { useCurrentUser } from '@/app/hooks/user/current.hook';
+import { mockFn } from 'jest-mock-extended';
+
 export interface IUseCurrentUserConfig {
   displayLogin?: boolean;
 }
+
+const a = mockFn<typeof useCurrentUser>();
+export type UseCurrentUserMock = typeof a;
