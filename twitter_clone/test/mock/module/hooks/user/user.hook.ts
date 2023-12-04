8d990d9bb4aa54baa34281cc11464a53c8a73f38
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-04 15:58:11 -0300

    [:recycle:] | refact: imports

--- twitter_clone/test/mock/module/hooks/user/user.hook.ts
+++ twitter_clone/test/mock/module/hooks/user/user.hook.ts
@@ -1,6 +1,6 @@
+import { useSession } from './session.hook';
 import { useCurrentUser } from '@/app/hooks/user/current.hook';
 import { useUsers } from '@/app/hooks/user/all.hook';
-import { useSession } from './session.hook';
 
 import { interfaces } from 'inversify';
 import { MODULES } from '@/app/modules';
@@ -10,7 +10,7 @@ import { ISimulatedUserHooks } from '@test/@types/simulate/user/hooks';
 import { MockedUseSession } from '@test/@types/hooks/user/session';
 
 jest.mock('../../../../../src/app/modules/@core/user/user.factory.ts');
-export { MODULES };
+export { MODULES, useSession, useUsers, useCurrentUser };
 
 export const simulateUserHooks = ({
   container,
