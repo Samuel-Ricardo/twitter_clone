ed0ddcd0b77dae56fa8b5e637cdc5459f718e010
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-10 23:56:33 -0300

    [ :white_check_mark: ] | test: navigate - modal > register (test:E2E)

--- twitter_clone/cypress/e2e/register.cy.ts
+++ twitter_clone/cypress/e2e/register.cy.ts
@@ -1,5 +1,5 @@
 describe('[E2E] | Register => [USER]', () => {
-  it('[1/3] | should render login', () => {
+  it('[1/3] | should: render [modal] => [LOGIN]', () => {
     cy.visit('http://localhost:3000');
     cy.get('div').should(
       'have.class',
@@ -7,5 +7,21 @@ describe('[E2E] | Register => [USER]', () => {
     );
     cy.get('div').get('h3').contains('Login');
     cy.get('div').get('input').should('have.length', 3);
+  });
+
+  it('[2/3] | should: navigate to [modal] => [REGISTER]', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('div').get('h3').contains('Login');
+
+    cy.get('div')
+      .get('span')
+      .should(
+        'have.class',
+        'text-cyan-500 font-semibold cursor-pointer hover:underline hover:text-cyan-400 transition',
+      )
+      .contains('Register Now! :D')
+      .click();
+
+    cy.get('div').get('h3').contains('Register');
   });
 });
